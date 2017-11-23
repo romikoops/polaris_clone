0162695b6e966df021c9cfb60b45af2bdd7f5021
Francisco P Coutinho <francisco.lgpc@gmail.com>
2017-11-23 19:56:47 +0100

    improved layouting of LandingTop

--- client/app/components/LandingTop/LandingTop.js
+++ client/app/components/LandingTop/LandingTop.js
@@ -17,21 +17,25 @@ export class LandingTop extends Component {
         }
         return (
             <div className="landing_top layout-row flex-100 layout-align-center">
-              <div className="layout-row flex-75 layout-wrap">
+              <div className="layout-row flex-100 layout-wrap">
                 <div className="top_row flex-100 layout-row">
-                  <div className="logo_row flex-50 layout-row layout-align-start-center">
-                    <img className="tenant_logo" src={logo} />
+                  <div className="logo_row flex-50 layout-row layout-align-start-center layout_elem">
+                    <div className="wrapper_tenant_logo">
+                      <img className="tenant_logo" src={logo} />
+                    </div>
                   </div>
-                  <div className="sign_in_row flex-50 layout-row layout-align-end-center">
+                  <div className="sign_in_row flex-50 layout-row layout-align-end-center layout_elem">
                   </div>
                 </div>
-                <div className="flex-100 flex-gt-sm-50">
+                <div className="flex-100 flex-gt-sm-50 layout_elem">
                   <LoginPage theme={theme} />
                 </div>
-                <div className="flex-100 flex-gt-sm-50 sign_up">
-                  <h2>Never spend precious time on trasportation again LCL shipping made simple</h2>
-                  <h3>Enjoy the most advanced and easy to use booking system in the market</h3>
-                  <RoundButton text="sign up" theme={theme} active/>
+                <div className="flex-100 flex-gt-sm-50 layout_elem">
+                  <div className="sign_up">
+                    <h2>Never spend precious time on trasportation again LCL shipping made simple</h2>
+                    <h3>Enjoy the most advanced and easy to use booking system in the market</h3>
+                    <RoundButton text="sign up" theme={theme} active/>
+                  </div>
                 </div>
               </div>
             </div>
