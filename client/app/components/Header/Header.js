53128f90394e20721746d4acf38694faba26a5a1
Henry Perschk <henry.perschk@gmail.com>
2017-11-23 12:55:01 +0100

    css working

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -5,15 +5,14 @@ import logo from '../../assets/images/logos/logo_black.png';
 export class Header extends Component {
     render() {
         return (
-            <div className={`${styles.header} layout-row flex-100 layout-wrap`}>
-                <div className="flex-100 button_row layout-row layout-align-end-center">
-                    <div className="flex-50 buttons layout-row layout-align-end-center">
-                        <div className="flex-25 layout-row layout-align-center-center">
-                            <h4 className="flex-none">About Us</h4>
-                            <img src={logo} alt=""/>
-                        </div>
+            <div className={`${styles.header} layout-row flex-100 layout-wrap layout-align-center`}>
+                <div className="content-width layout-row flex-none">
+                    <div className="layout-row flex-50 layout-align-start-center">
+                        <img src={logo} alt="" />
                     </div>
-                    <div className="flex-20" />
+                    <div className="layout-row flex-50 layout-align-end-center">
+                        <span>hey</span>
+                    </div>
                 </div>
             </div>
         );
