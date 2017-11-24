0c1ffca19e781f3eb221b15f73a1d31d68181252
Henry Perschk <henry.perschk@gmail.com>
2017-11-24 11:38:23 +0100

    load logo from theme

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,49 +18,50 @@ end
 tenant = Tenant.create(
   theme: {
     colors: {
-        primary: "#00E503",
-        secondary: "#00ACE5"
+      primary: "#00E503",
+      secondary: "#00ACE5"
     },
-    logo: "https://s3.eu-central-1.amazonaws.com/imcdev/assets/images/logos/logo_black_small.png"
-    },
-    address: "Torgny Segerstedtsgatan 80 426 77 Västra Frölunda",
-    phone: "+46 31-85 32 00",
-    emails: {
-    },
-    subdomain: "greencarrier"
+    logoLarge: "https://s3.eu-central-1.amazonaws.com/imcdev/assets/images/logos/logo_black.png",
+    logoSmall: "https://s3.eu-central-1.amazonaws.com/imcdev/assets/images/logos/logo_black_small.png"
+  },
+  address: "Torgny Segerstedtsgatan 80 426 77 Västra Frölunda",
+  phone: "+46 31-85 32 00",
+  emails: {
+  },
+  subdomain: "greencarrier"
 )
 
 admin = tenant.users.new(
-        role: Role.find_by_name('admin'),
+  role: Role.find_by_name('admin'),
 
-        company_name: "Greencarrier",
-        first_name: "Admin",
-        last_name: "Admin",
-        phone: "123456789",
+  company_name: "Greencarrier",
+  first_name: "Admin",
+  last_name: "Admin",
+  phone: "123456789",
 
-        email: "admin@greencarrier.com",
-        password: "demo123456789",
-        password_confirmation: "demo123456789",
-        
-        confirmed_at: DateTime.new(2017, 1, 20)
-    )
+  email: "admin@greencarrier.com",
+  password: "demo123456789",
+  password_confirmation: "demo123456789",
+
+  confirmed_at: DateTime.new(2017, 1, 20)
+)
 admin.skip_confirmation!
 admin.save!
 
 shipper = tenant.users.new(
-        role: Role.find_by_name('shipper'),
+  role: Role.find_by_name('shipper'),
 
-        company_name: "Example Shipper Company",
-        first_name: "John",
-        last_name: "Smith",
-        phone: "123456789",
+  company_name: "Example Shipper Company",
+  first_name: "John",
+  last_name: "Smith",
+  phone: "123456789",
 
-        email: "demo@greencarrier.com",
-        password: "demo123456789",
-        password_confirmation: "demo123456789",
-        
-        confirmed_at: DateTime.new(2017, 1, 20)
-    )
+  email: "demo@greencarrier.com",
+  password: "demo123456789",
+  password_confirmation: "demo123456789",
+
+  confirmed_at: DateTime.new(2017, 1, 20)
+)
 shipper.skip_confirmation!
 shipper.save!
 
