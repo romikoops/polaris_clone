a713eab5f64b8a0f814b3966da37702dc6285e23
Francisco P Coutinho <francisco.lgpc@gmail.com>
2017-12-04 00:20:50 +0100

    updated datepicker shipmentDetails

--- client/app/components/ShipmentDetails/ShipmentDetails.js
+++ client/app/components/ShipmentDetails/ShipmentDetails.js
@@ -206,10 +206,6 @@ export class ShipmentDetails extends Component {
     }
 
     render() {
-        // const textStyle = {
-        //     background: theme && theme.colors ? '-webkit-linear-gradient(left, ' + theme.colors.primary + ',' + theme.colors.secondary + ')' : 'black'
-        // }
-
         const { theme, messages, shipmentData } = this.props;
         let cargoDetails;
         if (shipmentData.data) {
@@ -260,9 +256,6 @@ export class ShipmentDetails extends Component {
         const value = this.state.selectedDay
             ? moment(this.state.selectedDay).format('DD/MM/YYYY')
             : '';
-        const textStyle = {
-            background: theme && theme.colors ? '-webkit-linear-gradient(left, ' + theme.colors.primary + ',' + theme.colors.secondary + ')' : 'black'
-        };
         const flash = messages ? <FlashMessages messages={messages} /> : '';
         const future = {
             after: new Date(),
@@ -283,7 +276,7 @@ export class ShipmentDetails extends Component {
                             </p>
                             <div className={'flex-none layout-row ' + styles.dpb}>
                                 <div className={'flex-none layout-row layout-align-center-center ' + styles.dpb_icon}>
-                                    <i className="flex-none fa fa-calendar" style={textStyle}></i>
+                                    <i className="flex-none fa fa-calendar"></i>
                                 </div>
                                 <DayPickerInput
                                     name="birthday"
