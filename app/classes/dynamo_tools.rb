331035b5ca4f7aaa646aa6524ff034a90f10ab86
Henry Perschk <henry.perschk@gmail.com>
2017-12-08 13:55:35 +0100

    .

--- app/classes/dynamo_tools.rb
+++ app/classes/dynamo_tools.rb
@@ -3,11 +3,12 @@ module DynamoTools
     client = init
     value["#{key}"] = keyName
     resp = client.put_item({
-      item: value, 
-        return_consumed_capacity: "TOTAL", 
-        table_name: "#{table}", 
+                             item: value,
+                             return_consumed_capacity: "TOTAL",
+                             table_name: "#{table}",
     })
   end
+
   def update_item(table, key, keyName, value)
     client = init
     update_str = 'set '
@@ -21,57 +22,61 @@ module DynamoTools
     end
 
     resp = client.update_item({
-      key: {
-        "#{key}" => keyName
-      },
-      update_expression: update_str,
-      expression_attribute_values: upd_val, 
-        return_consumed_capacity: "TOTAL", 
-        table_name: "#{table}", 
+                                key: {
+                                  "#{key}" => keyName
+                                },
+                                update_expression: update_str,
+                                expression_attribute_values: upd_val,
+                                return_consumed_capacity: "TOTAL",
+                                table_name: "#{table}",
     })
   end
+
   def get_item(table, keyName, key)
     client = init
     resp = client.get_item({
-    key: {
-      "#{keyName}" => "#{key}"
-    }, 
-    table_name: "#{table}", 
-  })
+                             key: {
+                               "#{keyName}" => "#{key}"
+                             },
+                             table_name: "#{table}",
+    })
   end
+
   def seed_init_table(table_name, primary_key)
     client = init
     params = {
-        table_name: table_name,
-        key_schema: [
-            {
-                attribute_name: "#{primary_key}",
-                key_type: 'HASH'  #Partition key
-            }
-        ],
-        attribute_definitions: [
-            {
-                attribute_name: "#{primary_key}",
-                attribute_type: 'S'
-            }
-        ],
-        provisioned_throughput: {
-            read_capacity_units: 5,
-            write_capacity_units: 5
+      table_name: table_name,
+      key_schema: [
+        {
+          attribute_name: "#{primary_key}",
+          key_type: 'HASH'  #Partition key
+        }
+      ],
+      attribute_definitions: [
+        {
+          attribute_name: "#{primary_key}",
+          attribute_type: 'S'
+        }
+      ],
+      provisioned_throughput: {
+        read_capacity_units: 5,
+        write_capacity_units: 5
       }
     }
 
     begin
-        result = client.create_table(params)
+      result = client.create_table(params)
 
-        puts 'Created table. Status: ' +
-            result.table_description.table_status;
+      puts 'Created table. Status: ' +
+        result.table_description.table_status;
     rescue  Aws::DynamoDB::Errors::ServiceError => error
-        puts 'Unable to create table:'
-        puts error.message
+      puts 'Unable to create table:'
+      puts error.message
     end
   end
+
   private
+
   def init
     #dynamodb = Aws::DynamoDB::Client.new(
     #   region: 'eu-central-1',
@@ -81,5 +86,4 @@ module DynamoTools
     dynamodb = Aws::DynamoDB::Client.new(endpoint:'http://localhost:8000')
     return dynamodb
   end
-
 end
