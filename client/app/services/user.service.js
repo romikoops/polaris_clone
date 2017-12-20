6c4429a081243f0d8e09c59ca87e86cf03ad7503
DaddyWozBucks <wkbeamish@gmail.com>
2017-12-20 14:50:50 +0100

    merge 3

--- client/app/services/user.service.js
+++ client/app/services/user.service.js
@@ -46,7 +46,7 @@ function makePrimary(userId, locationId) {
     ).then(handleResponse);
 }
 
-<<<<<<< HEAD
+
 function getShipments(id) {
     const requestOptions = {
         method: 'GET',
@@ -56,85 +56,7 @@ function getShipments(id) {
     return fetch(BASE_URL + '/users/' + id + '/shipments', requestOptions).then(handleResponse);
 }
 
-function login(username, password) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: username, password })
-    };
-    return fetch(BASE_URL + '/auth/sign_in', requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(response.statusText);
-            }
-            if (response.headers.get('access-token')) {
-                const accessToken = response.headers.get('access-token');
-                const client = response.headers.get('client');
-                const expiry = response.headers.get('expiry');
-                const tokenType = response.headers.get('token-type');
-                const uid = response.headers.get('uid');
-                const aHeader = {
-                    client: client,
-                    expiry: expiry,
-                    uid: uid,
-                    'access-token': accessToken,
-                    'token-type': tokenType
-                };
-                localStorage.setItem('authHeader', JSON.stringify(aHeader));
-            }
-            return response.json();
-        })
-        .then(user => {
-            // login successful if there's a jwt token in the response
-            if (user) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-            }
 
-            return user;
-        });
-}
-
-function anonymousLogin() {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-    };
-    return fetch(BASE_URL + '/auth/guest_sign_in', requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(response.statusText);
-            }
-            if (response.headers.get('access-token')) {
-                const accessToken = response.headers.get('access-token');
-                const client = response.headers.get('client');
-                const expiry = response.headers.get('expiry');
-                const tokenType = response.headers.get('token-type');
-                const uid = response.headers.get('uid');
-                const aHeader = {
-                    client: client,
-                    expiry: expiry,
-                    uid: uid,
-                    'access-token': accessToken,
-                    'token-type': tokenType
-                };
-                localStorage.setItem('authHeader', JSON.stringify(aHeader));
-            }
-            return response.json();
-        })
-        .then(user => {
-            // login successful if there's a jwt token in the response
-            if (user) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-            }
-
-            return user;
-        });
-}
-
-=======
->>>>>>> 113becd58bc8b08da7f3a5ff99d22cb9f80c035a
 function getStoredUser() {
     const sortedUser = JSON.parse(localStorage.getItem('user'));
     return sortedUser ? sortedUser : {};
@@ -207,7 +129,6 @@ export const userService = {
     makePrimary,
     getShipment,
     getShipments,
-    anonymousLogin,
     getHubs,
     getAll,
     getById,
