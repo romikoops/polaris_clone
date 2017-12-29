92f737231d5331b73ab08f89e099bf19d7bf6113
Francisco P Coutinho <francisco.lgpc@gmail.com>
2017-12-29 10:57:26 +0100

    updated private routes

--- client/app/routes/PrivateRoute.js
+++ client/app/routes/PrivateRoute.js
@@ -32,9 +32,9 @@
 
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-const user = localStorage.getItem('user');
-export const PrivateRoute = ({ component: Component, loggedIn, theme, ...rest }) => (
-    <Route {...rest} render={props => (
+
+export const PrivateRoute = ({ component: Component, user, loggedIn, theme, ...rest }) => (
+    <Route {...rest} render={ props => (
         user && loggedIn
             ? <Component theme={theme} {...props} />
             : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
