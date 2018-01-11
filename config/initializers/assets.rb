a495b68d93976a889556619629d27222171324fd
DaddyWozBucks <wkbeamish@gmail.com>
2018-01-11 17:55:39 +0100

    precompile added
    Former-commit-id: b3b1079b739eece52e30a16ddcc90fe09571ca69

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.precompile += %w( foundation_emails.css )
+Rails.application.config.assets.precompile += %w( foundation_emails.css pdfs.scss.erb )
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 Rails.application.config.assets.precompile += %w(.svg .eot .woff .ttf .otf)
