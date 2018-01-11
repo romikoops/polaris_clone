9b8ebc9786cb2c02c2fd1065f5d00d63079043c4
DaddyWozBucks <wkbeamish@gmail.com>
2018-01-11 17:53:41 +0100

    precompile added
    Former-commit-id: 99c52a14a3f251ad181d3347ce260936a57e1d1d

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module Imcr
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.assets.precompile = true
+    config.assets.digest = true
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, key: '_imc_platform_session'
