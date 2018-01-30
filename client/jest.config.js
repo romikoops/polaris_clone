6d78037c19de70079806881e6cfbb050f9a3e491
Ch. König <criss@paars.de>
2018-01-30 14:47:30 +0100

    jest base setup
    Former-commit-id: 8a7bba17c9253feaf07b0df4dfa9cca169289fed

---
+++ client/jest.config.js
@@ -0,0 +1,17 @@
+const Enzyme = require('enzyme')
+const Adapter = require('enzyme-adapter-react-16')
+
+Enzyme.configure({ adapter: new Adapter() })
+
+module.exports = {
+  // if true stops test after first failure
+  bail: true,
+  verbose: true,
+  setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  transformIgnorePatterns: ['/node_modules/'],
+  unmockedModulePathPatterns: [],
+  moduleNameMapper: {
+    '\\.s?css$': 'identity-obj-proxy'
+  }
+}
