815968ed616ffa8bb1be9dca85ff83fe0a283e07
Ch. König <criss@paars.de>
2018-01-31 14:34:56 +0100

    Added testfiles
    Former-commit-id: ba69a1ada2e4a1bb2228356bf8da0c90731cdd90

--- client/app/reducers/authentication.reducer.js
+++ client/app/reducers/authentication.reducer.js
@@ -1,74 +1,74 @@
-import { authenticationConstants } from '../constants';
-import { getSubdomain } from '../helpers/subdomain';
-const subdomainKey = getSubdomain();
-const cookieKey = subdomainKey + '_user';
-const user = JSON.parse(localStorage.getItem(cookieKey));
-const initialState = user ? { loggedIn: true, user } : {};
+import { authenticationConstants } from '../constants'
+import { getSubdomain } from '../helpers/subdomain'
 
-export function authentication(state = initialState, action) {
-    switch (action.type) {
-        case authenticationConstants.LOGIN_REQUEST:
-            return {
-                ...state,
-                loginAttempt: false,
-                loggingIn: true,
-            };
-        case authenticationConstants.LOGIN_SUCCESS:
-            return {
-                user: action.user,
-                loggedIn: true
-            };
-        case authenticationConstants.LOGIN_FAILURE:
-            const newState = action.loginFailure.persistState ? state : {};
-            return {
-                ...newState,
-                error: action.loginFailure.error,
-                loginAttempt: true,
-                loggingIn: false
-            };
+const subdomainKey = getSubdomain()
+const cookieKey = `${subdomainKey}_user`
 
-        case authenticationConstants.UPDATE_USER_REQUEST:
-            return {
-                ...state,
-                loggedIn: true,
-                registering: true
-            };
-        case authenticationConstants.UPDATE_USER_SUCCESS:
-            return {
-                loggedIn: true,
-                registered: true,
-                user: action.user
-            };
-        case authenticationConstants.UPDATE_USER_FAILURE:
-            return {
-                ...state,
-                registering: false,
-                registrationAttempt: true
-            };
+const localStorage = window.localStorage || { getItem (key) { return null } }
+const user = JSON.parse(localStorage.getItem(cookieKey))
 
-        case authenticationConstants.REGISTRATION_REQUEST:
-            return {
-                loading: action.user.guest,
-                registering: true
-            };
-        case authenticationConstants.REGISTRATION_SUCCESS:
-            return {
-                loggedIn: true,
-                registered: true,
-                user: action.user
-            };
-        case authenticationConstants.REGISTRATION_FAILURE:
-            return {
-                registrationAttempt: true
-            };
-        case authenticationConstants.LOGOUT:
-            return {};
-         case authenticationConstants.SET_USER:
-            return {
-                ...state,
-                user: action.user
-            };
-        default:
-            return state;
-    }
+const initialState = user ? { loggedIn: true, user } : {}
+
+export default function (state = initialState, action) {
+  if (typeof state !== 'object') {
+    throw new Error('invalid state')
+  }
+
+  if (!action || !action.type) {
+    return state
+  }
+
+  switch (action.type) {
+    case authenticationConstants.LOGIN_REQUEST:
+      return {
+        loggingIn: true,
+        user: action.user
+      }
+    case authenticationConstants.LOGIN_SUCCESS:
+      return {
+        loggedIn: true,
+        user: action.user
+      }
+    case authenticationConstants.LOGIN_FAILURE:
+      return {}
+
+    case authenticationConstants.UPDATE_USER_REQUEST:
+      return {
+        loggedIn: true,
+        registering: true,
+        user: action.user
+      }
+    case authenticationConstants.UPDATE_USER_SUCCESS:
+      return {
+        loggedIn: true,
+        registered: true,
+        user: action.user
+      }
+    case authenticationConstants.UPDATE_USER_FAILURE:
+      return {}
+
+    case authenticationConstants.REGISTRATION_REQUEST:
+      return {
+        loading: true,
+        registering: true,
+        user: action.user
+      }
+    case authenticationConstants.REGISTRATION_SUCCESS:
+      return {
+        loggedIn: true,
+        registered: true,
+        user: action.user
+      }
+    case authenticationConstants.REGISTRATION_FAILURE:
+      return {}
+    case authenticationConstants.LOGOUT:
+      return {}
+    case authenticationConstants.SET_USER:
+      return {
+        ...state,
+        user: action.user
+      }
+    default:
+      return state
+  }
 }
--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -1,42 +1,41 @@
-import { routerReducer as routing } from 'react-router-redux';
-import { combineReducers } from 'redux';
+import { routerReducer as routing } from 'react-router-redux'
+import { combineReducers } from 'redux'
 // import { authStateReducer } from 'redux-auth';
-import * as types from '../actions/types';
-import { authentication } from './authentication.reducer';
-import { users } from './user.reducer';
-import { registration } from './registration.reducer';
-import { shipment } from './shipment.reducer';
-import { alert } from './alert.reducer';
-import { admin } from './admin.reducer';
-import { app } from './app.reducer';
-import { nexus } from './nexus.reducer';
-import { messaging } from './messaging.reducer';
-import { tenant, selectedSubdomain } from './tenant.reducer';
+import * as types from '../actions/types'
+import authentication from './authentication.reducer'
+import { users } from './user.reducer'
+import { registration } from './registration.reducer'
+import { shipment } from './shipment.reducer'
+import { alert } from './alert.reducer'
+import { admin } from './admin.reducer'
+import { app } from './app.reducer'
+import { nexus } from './nexus.reducer'
+import { messaging } from './messaging.reducer'
+import { tenant, selectedSubdomain } from './tenant.reducer'
 
 const filter = (state = '', action) => {
-    switch (action.type) {
-        case types.FILTER:
-            return action.filter;
-        default:
-            return state;
-    }
-};
+  switch (action.type) {
+    case types.FILTER:
+      return action.filter
+    default:
+      return state
+  }
+}
 
 const rootReducer = combineReducers({
-    authentication,
-    registration,
-    users,
-    alert,
-    filter,
-    bookingData: shipment,
-    selectedSubdomain,
-    tenant,
-    admin,
-    app,
-    routing,
-    nexus,
-    messaging
-});
+  authentication,
+  registration,
+  users,
+  alert,
+  filter,
+  bookingData: shipment,
+  selectedSubdomain,
+  tenant,
+  admin,
+  app,
+  routing,
+  nexus,
+  messaging
+})
 
-export default rootReducer;
-
+export default rootReducer
