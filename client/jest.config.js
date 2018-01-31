f80175d1cd493f4ab6c5b8d71ff30295ec81bef1
Ch. König <criss@paars.de>
2018-01-31 12:15:09 +0100

    Adjusted dependencies and settings for testing
    Former-commit-id: 2d59b8719d6eb6aadc80959c567fd72ef0180d75

--- client/jest.config.js
+++ client/jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   // if true stops test after first failure
   bail: true,
   verbose: true,
+  setupFiles: ['./jest.init.js'],
   setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
   snapshotSerializers: ['enzyme-to-json/serializer'],
   transformIgnorePatterns: ['/node_modules/'],
---
+++ client/jest.init.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { toBeType } from 'jest-tobetype'
+import expect from 'expect'
+/* eslint-enable import/no-extraneous-dependencies */
+
+expect.extend({
+  toBeType
+})
