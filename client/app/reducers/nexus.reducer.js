8872f7728911745d73ef9b08e0807162b03113ed
Christopher König <criss@paars.de>
2018-02-05 18:40:45 +0100

    eslint nexus.reducer.js
    Former-commit-id: b76cf5628e09419852aff0cd99322b93f061acea

--- client/app/reducers/nexus.reducer.js
+++ client/app/reducers/nexus.reducer.js
@@ -1,18 +1,22 @@
-import { nexusConstants } from '../constants';
-import merge from 'lodash/merge';
+import merge from 'lodash/merge'
+import { nexusConstants } from '../constants'
 
-export function nexus(state = {}, action) {
-    switch (action.type) {
-        case nexusConstants.GET_AVAILABLE_DESTINATIONS_REQUEST:
-            return {};
-        case nexusConstants.GET_AVAILABLE_DESTINATIONS_SUCCESS:
-        		return { availableDestinations: action.data.available_destinations};
-        case nexusConstants.GET_AVAILABLE_DESTINATIONS_FAILURE:
-            const errG = merge({}, state, {
-                error: { get: [ action.error ] }
-            });
-            return errG;
-        default:
-            return state;
+export default function nexus (state = {}, action) {
+  switch (action.type) {
+    case nexusConstants.GET_AVAILABLE_DESTINATIONS_REQUEST:
+      return {}
+
+    case nexusConstants.GET_AVAILABLE_DESTINATIONS_SUCCESS:
+      return { availableDestinations: action.data.available_destinations }
+
+    case nexusConstants.GET_AVAILABLE_DESTINATIONS_FAILURE: {
+      const errG = merge({}, state, {
+        error: { get: [action.error] }
+      })
+      return errG
     }
+
+    default:
+      return state
+  }
 }
