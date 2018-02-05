3d36c6e585af9938372da4f3a8c3dc43fe1d9198
Christopher König <criss@paars.de>
2018-02-05 18:41:32 +0100

    messaging.reducer.js
    Former-commit-id: 39df2284422b8b1c6bb4d6893d536560d18361c0

--- client/app/reducers/messaging.reducer.js
+++ client/app/reducers/messaging.reducer.js
@@ -1,112 +1,95 @@
-import { messagingConstants } from '../constants';
-export function messaging(state = {}, action) {
-    switch (action.type) {
-        case messagingConstants.GET_USER_MESSAGES_SUCCESS:
-            return {
-                ...state,
-                ...action.payload,
-                loading: false
-            };
-        case messagingConstants.GET_USER_MESSAGES_ERROR:
-            return {
-                ...state,
-                error: action.payload,
-                loading: false
-            };
-        case messagingConstants.GET_USER_MESSAGES_REQUEST:
-            return {
-                ...state,
-                loading: true
-            };
+import { messagingConstants } from '../constants'
 
-        case messagingConstants.GET_ADMIN_MESSAGES_SUCCESS:
-            return {
-                ...state,
-                ...action.payload,
-                loading: false
-            };
-        case messagingConstants.GET_ADMIN_MESSAGES_ERROR:
-            return {
-                ...state,
-                error: action.payload,
-                loading: false
-            };
-        case messagingConstants.GET_ADMIN_MESSAGES_REQUEST:
-            return {
-                ...state,
-                loading: true,
-                sending: true
-            };
+export default function messaging (state = {}, action) {
+  switch (action.type) {
+    case messagingConstants.GET_USER_MESSAGES_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        loading: false
+      }
+    case messagingConstants.GET_USER_MESSAGES_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      }
+    case messagingConstants.GET_USER_MESSAGES_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
 
-        case messagingConstants.SEND_USER_MESSAGE_SUCCESS:
-            const newMessages = state.conversations[action.payload.shipmentRef].messages;
-            newMessages.push(action.payload);
-            return {
-                ...state,
-                conversations: {
-                    ...state.conversations,
-                    [action.payload.shipmentRef]: {
-                        ...state.conversations[action.payload.shipmentRef],
-                        messages: newMessages
-                    }
-                },
-                loading: false,
-                sending: false
-            };
-        case messagingConstants.SEND_USER_MESSAGE_ERROR:
-            return {
-                ...state,
-                error: action.payload,
-                loading: false,
-                sending: false
-            };
-        case messagingConstants.SEND_USER_MESSAGE_REQUEST:
-            return {
-                ...state,
-                loading: true,
-                sending: true
-            };
-
-        case messagingConstants.GET_SHIPMENT_DATA_SUCCESS:
-            return {
-                ...state,
-                shipment: action.payload,
-                loading: false
-            };
-        case messagingConstants.GET_SHIPMENT_DATA_ERROR:
-            return {
-                ...state,
-                error: action.payload,
-                loading: false
-            };
-        case messagingConstants.GET_SHIPMENT_DATA_REQUEST:
-            return {
-                ...state,
-                loading: true
-            };
-        case messagingConstants.MARK_AS_READ_SUCCESS:
-            return {
-                ...state,
-                conversations: action.payload.conversations,
-                loading: false
-            };
-        case messagingConstants.MARK_AS_READ_ERROR:
-            return {
-                ...state,
-                error: action.payload,
-                loading: false
-            };
-        case messagingConstants.MARK_AS_READ_REQUEST:
-            return {
-                ...state,
-                loading: true
-            };
-        case messagingConstants.SHOW_MESSAGE_CENTER:
-            return {
-                ...state,
-                showMessages: !state.showMessages
-            };
-        default:
-            return state;
+    case messagingConstants.SEND_USER_MESSAGE_SUCCESS: {
+      const newMessages = state.conversations[action.payload.shipmentRef].messages
+      newMessages.push(action.payload)
+      return {
+        ...state,
+        conversations: {
+          ...state.conversations,
+          [action.payload.shipmentRef]: {
+            ...state.conversations[action.payload.shipmentRef],
+            messages: newMessages
+          }
+        },
+        loading: false,
+        sending: false
+      }
     }
+    case messagingConstants.SEND_USER_MESSAGE_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+        sending: false
+      }
+    case messagingConstants.SEND_USER_MESSAGE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        sending: true
+      }
+
+    case messagingConstants.GET_SHIPMENT_DATA_SUCCESS:
+      return {
+        ...state,
+        shipment: action.payload,
+        loading: false
+      }
+    case messagingConstants.GET_SHIPMENT_DATA_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      }
+    case messagingConstants.GET_SHIPMENT_DATA_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case messagingConstants.MARK_AS_READ_SUCCESS:
+      return {
+        ...state,
+        conversations: action.payload.conversations,
+        loading: false
+      }
+    case messagingConstants.MARK_AS_READ_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      }
+    case messagingConstants.MARK_AS_READ_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case messagingConstants.SHOW_MESSAGE_CENTER:
+      return {
+        ...state,
+        showMessages: !state.showMessages
+      }
+    default:
+      return state
+  }
 }
