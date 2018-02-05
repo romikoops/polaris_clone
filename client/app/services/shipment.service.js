3c730ee40254c20456779bb672fdb19d6d1d6c07
Christopher König <criss@paars.de>
2018-02-05 16:54:29 +0100

    eslint app/services/shipment.service.js
    Former-commit-id: d50644cc01d2f2e8125dfdc5de9aba78ecf7cf71

--- client/app/services/shipment.service.js
+++ client/app/services/shipment.service.js
@@ -1,120 +1,123 @@
-import { authHeader } from '../helpers';
-import { Promise } from 'es6-promise-promise';
-import { BASE_URL } from '../constants';
+import { Promise } from 'es6-promise-promise'
+import { BASE_URL } from '../constants'
+import { authHeader } from '../helpers'
 
-function handleResponse(response) {
-    const promise = Promise;
-    const respJSON = response.json();
-    if (!response.ok) {
-        return promise.reject(respJSON);
-    }
+const { fetch, localStorage, FormData } = window
 
-    return respJSON;
+function handleResponse (response) {
+  const promise = Promise
+  const respJSON = response.json()
+  if (!response.ok) {
+    return promise.reject(respJSON)
+  }
+
+  return respJSON
 }
 
-function getStoredShipment() {
-    const storedShipment = JSON.parse(localStorage.getItem('shipment'));
-    return storedShipment ? storedShipment : {};
+function getStoredShipment () {
+  const storedShipment = JSON.parse(localStorage.getItem('shipment'))
+  return storedShipment || {}
 }
 
-function getAll() {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
+function getAll () {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  }
 
-    return fetch(BASE_URL + '/shipments', requestOptions).then(handleResponse);
+  return fetch(`${BASE_URL}/shipments`, requestOptions).then(handleResponse)
 }
 
-function getShipment(id) {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
+function getShipment (id) {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  }
 
-    return fetch(BASE_URL + '/shipments/' + id, requestOptions).then(
-        handleResponse
-    );
+  return fetch(`${BASE_URL}/shipments/${id}`, requestOptions).then(handleResponse)
 }
 
-function newShipment(loadType) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify({ loadType })
-    };
-    const url = BASE_URL + '/shipments';
-    return fetch(url, requestOptions).then(handleResponse);
+function newShipment (loadType) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify({ loadType })
+  }
+  const url = `${BASE_URL}/shipments`
+  return fetch(url, requestOptions).then(handleResponse)
 }
 
-function setShipmentDetails(data) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    };
-    const url = BASE_URL + '/shipments/' + data.shipment.id + '/get_offer';
-    return fetch(url, requestOptions).then(handleResponse);
+function setShipmentDetails (data) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }
+  const url = `${BASE_URL}/shipments/${data.shipment.id}/get_offer`
+  return fetch(url, requestOptions).then(handleResponse)
 }
 
-function setShipmentRoute(data) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    };
-    const url = BASE_URL + '/shipments/' + data.shipment.id + '/finish_booking';
-    return fetch(url, requestOptions).then(handleResponse);
+function setShipmentRoute (data) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }
+  const url = `${BASE_URL}/shipments/${data.shipment.id}/finish_booking`
+  return fetch(url, requestOptions).then(handleResponse)
 }
 
-function setShipmentContacts(data) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    };
-    const url = BASE_URL + '/shipments/' + data.shipment.id + '/update';
-    return fetch(url, requestOptions).then(handleResponse);
-}
-function acceptShipment(id) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' }
-    };
-    const url = BASE_URL + '/shipments/' + id + '/confirm_shipment';
-    return fetch(url, requestOptions).then(handleResponse);
+function setShipmentContacts (data) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  }
+  const url = `${BASE_URL}/shipments/${data.shipment.id}/update`
+  return fetch(url, requestOptions).then(handleResponse)
 }
 
-function uploadDocument(doc, type, url) {
-    const formData = new FormData();
-    formData.append('file', doc);
-    formData.append('type', type);
-    const requestOptions = {
-        method: 'POST',
-        headers: authHeader(),
-        body: formData
-    };
+function uploadDocument (doc, type, url) {
+  const formData = new FormData()
+  formData.append('file', doc)
+  formData.append('type', type)
+  const requestOptions = {
+    method: 'POST',
+    headers: authHeader(),
+    body: formData
+  }
 
-    return fetch(BASE_URL + url, requestOptions).then(handleResponse);
+  return fetch(BASE_URL + url, requestOptions).then(handleResponse)
 }
-function deleteDocument(documentId) {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
 
-    return fetch(BASE_URL + '/documents/delete/' + documentId, requestOptions).then(handleResponse);
+function acceptShipment (id) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' }
+  }
+  const url = `${BASE_URL}/shipments/${id}/confirm_shipment`
+  return fetch(url, requestOptions).then(handleResponse)
+}
+
+function deleteDocument (documentId) {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  }
+  return fetch(`${BASE_URL}/documents/delete/${documentId}`, requestOptions).then(handleResponse)
 }
 
 export const shipmentService = {
-    newShipment,
-    getAll,
-    getShipment,
-    setShipmentRoute,
-    deleteDocument,
-    setShipmentDetails,
-    getStoredShipment,
-    setShipmentContacts,
-    uploadDocument,
-    acceptShipment
-};
+  newShipment,
+  getAll,
+  getShipment,
+  setShipmentRoute,
+  deleteDocument,
+  setShipmentDetails,
+  getStoredShipment,
+  setShipmentContacts,
+  uploadDocument,
+  acceptShipment
+}
+
+export default shipmentService
