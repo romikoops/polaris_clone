947796723d3c3f540ac099655db63379ed367b4b
Christopher König <criss@paars.de>
2018-02-05 17:00:14 +0100

    eslint app/service/authentication.service.js
    Former-commit-id: 22993edd95789f61fda51eddf87ebe6547e115c3

--- client/app/services/authentication.service.js
+++ client/app/services/authentication.service.js
@@ -1,129 +1,135 @@
-import { Promise } from 'es6-promise-promise';
-import { BASE_URL } from '../constants';
-import { authHeader, getSubdomain } from '../helpers';
-const subdomainKey = getSubdomain();
-const cookieKey = subdomainKey + '_user';
-function logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem(cookieKey);
+import { Promise } from 'es6-promise-promise'
+import { BASE_URL } from '../constants'
+import { authHeader, getSubdomain } from '../helpers'
+
+const { fetch, localStorage } = window
+
+const subdomainKey = getSubdomain()
+const cookieKey = `${subdomainKey}_user`
+function logout () {
+  // remove user from local storage to log user out
+  localStorage.removeItem(cookieKey)
 }
 
-function login(data) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email: data.email, password: data.password })
-    };
-    return fetch(BASE_URL + '/auth/sign_in', requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(response.json());
-            }
-            if (response.headers.get('access-token')) {
-                const accessToken = response.headers.get('access-token');
-                const client = response.headers.get('client');
-                const expiry = response.headers.get('expiry');
-                const tokenType = response.headers.get('token-type');
-                const uid = response.headers.get('uid');
-                const aHeader = {
-                    client: client,
-                    expiry: expiry,
-                    uid: uid,
-                    'access-token': accessToken,
-                    'token-type': tokenType
-                };
-                localStorage.setItem('authHeader', JSON.stringify(aHeader));
-            }
-            return response.json();
-        })
-        .then(response => {
-            // login successful if there's a jwt token in the response
-            if (response) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem(cookieKey, JSON.stringify(response.data));
-            }
+function login (data) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email: data.email, password: data.password })
+  }
+  return fetch(`${BASE_URL}/auth/sign_in`, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject(response.json())
+      }
+      if (response.headers.get('access-token')) {
+        const accessToken = response.headers.get('access-token')
+        const client = response.headers.get('client')
+        const expiry = response.headers.get('expiry')
+        const tokenType = response.headers.get('token-type')
+        const uid = response.headers.get('uid')
+        const aHeader = {
+          client,
+          expiry,
+          uid,
+          'access-token': accessToken,
+          'token-type': tokenType
+        }
+        localStorage.setItem('authHeader', JSON.stringify(aHeader))
+      }
+      return response.json()
+    })
+    .then((response) => {
+      // login successful if there's a jwt token in the response
+      if (response) {
+        // store user details and jwt token in local storage to keep
+        // user logged in between page refreshes
+        localStorage.setItem(cookieKey, JSON.stringify(response.data))
+      }
 
-            return response;
-        });
+      return response
+    })
 }
 
-function getStoredUser() {
-    const sortedUser = JSON.parse(localStorage.getItem(cookieKey));
-    return sortedUser ? sortedUser : {};
+function getStoredUser () {
+  const sortedUser = JSON.parse(localStorage.getItem(cookieKey))
+  return sortedUser || {}
 }
 
-function register(user) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    };
+function register (user) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user)
+  }
 
-    return fetch(BASE_URL + '/auth/', requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(response.statusText);
-            }
-            if (response.headers.get('access-token')) {
-                const accessToken = response.headers.get('access-token');
-                const client = response.headers.get('client');
-                const expiry = response.headers.get('expiry');
-                const tokenType = response.headers.get('token-type');
-                const uid = response.headers.get('uid');
-                const aHeader = {
-                    client: client,
-                    expiry: expiry,
-                    uid: uid,
-                    'access-token': accessToken,
-                    'token-type': tokenType
-                };
-                localStorage.setItem('authHeader', JSON.stringify(aHeader));
-            }
-            return response.json();
-        })
-        .then(response => {
-            // login successful if there's a jwt token in the response
-            if (response) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem(cookieKey, JSON.stringify(response.data));
-                const user2 = localStorage.getItem(cookieKey);
-                console.log(user2);
-            }
-            return response;
-        });
+  return fetch(`${BASE_URL}/auth/`, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject(response.statusText)
+      }
+      if (response.headers.get('access-token')) {
+        const accessToken = response.headers.get('access-token')
+        const client = response.headers.get('client')
+        const expiry = response.headers.get('expiry')
+        const tokenType = response.headers.get('token-type')
+        const uid = response.headers.get('uid')
+        const aHeader = {
+          client,
+          expiry,
+          uid,
+          'access-token': accessToken,
+          'token-type': tokenType
+        }
+        localStorage.setItem('authHeader', JSON.stringify(aHeader))
+      }
+      return response.json()
+    })
+    .then((response) => {
+      // login successful if there's a jwt token in the response
+      if (response) {
+        // store user details and jwt token in local storage to keep
+        // user logged in between page refreshes
+        localStorage.setItem(cookieKey, JSON.stringify(response.data))
+      }
+      return response
+    })
 }
 
-function updateUser(user, req) {
-    const requestOptions = {
-        method: 'PUT',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify({update: req})
-    };
+function updateUser (user, req) {
+  const requestOptions = {
+    method: 'PUT',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify({ update: req })
+  }
 
-    return fetch(BASE_URL + '/users/' + user.id + '/update', requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(response.statusText);
-            }
-            return response.json();
-        })
-        .then(response => {
-            // login successful if there's a jwt token in the response
-            if (response) {
-                if (response.data.headers) {
-                    localStorage.setItem('authHeader', JSON.stringify(response.data.headers));
-                }
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem(cookieKey, JSON.stringify(response.data));
-            }
-            return response;
-        });
+  return fetch(`${BASE_URL}/users/${user.id}/update`, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject(response.statusText)
+      }
+      return response.json()
+    })
+    .then((response) => {
+      // login successful if there's a jwt token in the response
+      if (response) {
+        if (response.data.headers) {
+          localStorage.setItem('authHeader', JSON.stringify(response.data.headers))
+        }
+        // store user details and jwt token in local storage to keep
+        // user logged in between page refreshes
+        localStorage.setItem(cookieKey, JSON.stringify(response.data))
+      }
+      return response
+    })
 }
 
-export const authenticationService = {
-    login,
-    logout,
-    register,
-    getStoredUser,
-    updateUser
-};
+const authenticationService = {
+  login,
+  logout,
+  register,
+  getStoredUser,
+  updateUser
+}
+
+export default authenticationService
