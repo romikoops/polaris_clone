b094e4f278e583c4c5f26b2a0421b564b5481d6f
Christopher König <criss@paars.de>
2018-02-05 16:57:18 +0100

    app/services/messaging.service.js
    Former-commit-id: de77d3e6a6dacf674d0526b9693a2bbadbfdf107

--- client/app/services/messaging.service.js
+++ client/app/services/messaging.service.js
@@ -1,67 +1,71 @@
-import { Promise } from 'es6-promise-promise';
-import { BASE_URL } from '../constants';
-import { authHeader } from '../helpers';
+import { Promise } from 'es6-promise-promise'
+import { BASE_URL } from '../constants'
+import { authHeader } from '../helpers'
 
-function handleResponse(response) {
-    const promise = Promise;
-    const respJSON = response.json();
-    if (!response.ok) {
-        return promise.reject(respJSON);
-    }
+const { fetch } = window
 
-    return respJSON;
+function handleResponse (response) {
+  const promise = Promise
+  const respJSON = response.json()
+  if (!response.ok) {
+    return promise.reject(respJSON)
+  }
+
+  return respJSON
 }
 
-function getUserConversations() {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
-    return fetch(BASE_URL + '/messaging/get', requestOptions).then(handleResponse);
+function getUserConversations () {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  }
+  return fetch(`${BASE_URL}/messaging/get`, requestOptions).then(handleResponse)
 }
 
-function getAdminConversations() {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
-    return fetch(BASE_URL + '/messaging/get_admin', requestOptions).then(handleResponse);
+function getAdminConversations () {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader()
+  }
+  return fetch(`${BASE_URL}/messaging/get_admin`, requestOptions).then(handleResponse)
 }
 
-function sendUserMessage(message) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify({ message })
-    };
-    const url = BASE_URL + '/messaging/send';
-    return fetch(url, requestOptions).then(handleResponse);
+function sendUserMessage (message) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify({ message })
+  }
+  const url = `${BASE_URL}/messaging/send`
+  return fetch(url, requestOptions).then(handleResponse)
 }
 
-function getShipmentData(ref) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify({ ref })
-    };
-    const url = BASE_URL + '/messaging/data';
-    return fetch(url, requestOptions).then(handleResponse);
+function getShipmentData (ref) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ref })
+  }
+  const url = `${BASE_URL}/messaging/data`
+  return fetch(url, requestOptions).then(handleResponse)
 }
 
-function markAsRead(shipmentRef) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { ...authHeader(), 'Content-Type': 'application/json' },
-        body: JSON.stringify({ shipmentRef })
-    };
-    const url = BASE_URL + '/messaging/mark';
-    return fetch(url, requestOptions).then(handleResponse);
+function markAsRead (shipmentRef) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { ...authHeader(), 'Content-Type': 'application/json' },
+    body: JSON.stringify({ shipmentRef })
+  }
+  const url = `${BASE_URL}/messaging/mark`
+  return fetch(url, requestOptions).then(handleResponse)
 }
 
-export const messagingService = {
-    getUserConversations,
-    sendUserMessage,
-    getShipmentData,
-    markAsRead,
-    getAdminConversations
-};
+const messagingService = {
+  getUserConversations,
+  sendUserMessage,
+  getShipmentData,
+  markAsRead,
+  getAdminConversations
+}
+
+export default messagingService
