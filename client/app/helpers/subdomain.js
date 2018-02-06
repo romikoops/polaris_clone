0ae6001e36541268765a3aaa3fae28a400239c0f
Christopher König <criss@paars.de>
2018-02-06 14:16:46 +0100

    helper/subdomain
    Former-commit-id: fa1bb3a8a195367f328528b7335403288bdd06ff

--- client/app/helpers/subdomain.js
+++ client/app/helpers/subdomain.js
@@ -1,12 +1,16 @@
-export const getSubdomain = () => {
-    const host = window.location.host;
-    if (host.indexOf('.') < 0) {
-        return 'demo';
-        // return 'isa';
-        // return 'demo';
-    }
-    if (host.split('.')[0] === 'www' || host.split('.')[0] === 'react' || host.includes('localhost')) {
-        return 'demo';
-    }
-    return host.split('.')[0];
-};
+export function getSubdomain () {
+  const { host } = window.location
+  if (host.indexOf('.') < 0) {
+    return 'greencarrier'
+  }
+  if (
+    host.split('.')[0] === 'www' ||
+    host.split('.')[0] === 'react' ||
+    host.includes('localhost')
+  ) {
+    return 'demo'
+  }
+  return host.split('.')[0]
+}
+
+export default getSubdomain
