c0806fa505493d830034881d1a0782b7aa0e7a8f
Christopher König <criss@paars.de>
2018-02-06 14:20:41 +0100

    helpers/store
    Former-commit-id: 94b746cbebcf9d6615eafba098e393e6442f7eb7

--- client/app/helpers/store.js
+++ client/app/helpers/store.js
@@ -1,13 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import rootReducer from '../reducers';
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import rootReducer from '../reducers'
 
-const loggerMiddleware = createLogger();
-export const store = createStore(
-    rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
-);
+const loggerMiddleware = createLogger()
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware))
+export default store
