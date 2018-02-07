232b917d3e48bc71a36165240031ca9ecc655aeb
Christopher König <criss@paars.de>
2018-02-07 17:10:23 +0100

    added new prop types, extended existing ones
    Former-commit-id: e9fdb5919db277b2d8fc59b75912c1d1314032f9

--- client/app/prop-types.js
+++ client/app/prop-types.js
@@ -7,4 +7,26 @@ PropTypes.theme = PropTypes.shape({
   })
 })
 
+PropTypes.user = PropTypes.shape({
+  guest: PropTypes.bool
+})
+
+PropTypes.history = PropTypes.shape({
+  push: PropTypes.func
+})
+
+PropTypes.tenant = PropTypes.shape({
+  data: PropTypes.shape({
+    id: PropTypes.number,
+    theme: PropTypes.theme,
+    subdomain: PropTypes.string
+  })
+})
+
+PropTypes.req = PropTypes.shape({
+  schedule: PropTypes.arrayOf(PropTypes.object),
+  total: PropTypes.number,
+  shipment: PropTypes.object
+})
+
 export default PropTypes
