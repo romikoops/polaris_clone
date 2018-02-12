f5d5a25162f4f400b91897125330fc5bea0e9ee2
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-02-12 11:33:26 +0100

    contact setter body is now collapsable
    Former-commit-id: ae2c9d1c19a929077cae9fd81f81bf192f5cfca1

--- client/app/components/ContactSetter/ContactSetter.js
+++ client/app/components/ContactSetter/ContactSetter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import styles from './ContactSetter.scss';
+import styles from './ContactSetter.scss';
 import defs from '../../styles/default_classes.scss';
 import { ShipmentContactForm } from '../ShipmentContactForm/ShipmentContactForm';
 import { AddressBook } from '../AddressBook/AddressBook';
@@ -34,7 +34,8 @@ export class ContactSetter extends Component {
     	contactData: {
       	type: 'shipper',
     		...this.newContactData
-    	}
+    	},
+      showBody: false
     };
     this.contactTypes = ['shipper', 'consignee', 'notifyee'];
     this.stages = ['shipper', 'consignee', 'notifyees'];
@@ -42,6 +43,7 @@ export class ContactSetter extends Component {
     this.setContact = this.setContact.bind(this);
     this.setStage = this.setStage.bind(this);
     this.setContactForEdit = this.setContactForEdit.bind(this);
+    this.toggleShowBody = this.toggleShowBody.bind(this);
   }
 
   setContactForEdit(contactData) {
@@ -114,47 +116,59 @@ export class ContactSetter extends Component {
     ));
   }
 
+  toggleShowBody() {
+    this.setState({ showBody: !this.state.showBody });
+  }
   render() {
     const { theme, shipper, consignee, notifyees } = this.props;
-    const { contactData } = this.state;
+    const { contactData, showBody } = this.state;
     const stageIndex = this.contactTypes.indexOf(contactData.type);
-    console.log('!!', contactData);
+    const showBodyIconStyles = { padding: '10px' };
+    if (showBody) showBodyIconStyles.transform = 'rotate(90deg)';
+
     return (
       <div className="flex-100 layout-row layout-wrap layout-align-center-start">
         <div className={`
         	flex-none ${defs.content_width} layout-row layout-wrap
         `}>
-	        <div className="flex-100 layout-row layout-align-center-center">
-	          <h1> Set Contact Details</h1>
-	        </div>
-
-          <div className="flex-100 layout-row layout-align-center-center">
-	          <StageTimeline
-              theme={theme}
-              currentStageIndex={stageIndex}
-              stages={this.stages}
-              setStage={this.setStage}
-	          />
-          </div>
-          <div
-          	className="flex-100 layout-row layout-align-center-center"
-          	style={{ marginBottom: '50px', height: '493px', boxShadow: 'rgba(0, 0, 0, 0.05) 2px 2px 1px' }}
+	        <div
+            className="flex-100 layout-row layout-align-center-center pointy"
+            onClick={this.toggleShowBody}
           >
-	          <div className="flex-50" style={{height: '100%'}}>
-	            <ShipmentContactForm
-	              contactData={contactData}
-	              theme={theme}
-	              setContact={this.setContact}
-	            />
-	          </div>
+	          <h1> Set Contact Details</h1>
+            <i style={showBodyIconStyles} className="fa fa-chevron-right"></i>
+	        </div>
+          <div className={`
+            flex-100 layout-row layout-wrap ${styles.body} ${showBody ? '' : styles.hidden}
+          `}>
+            <div className="flex-100 layout-row layout-align-center-center">
+  	          <StageTimeline
+                theme={theme}
+                currentStageIndex={stageIndex}
+                stages={this.stages}
+                setStage={this.setStage}
+  	          />
+            </div>
+            <div
+            	className="flex-100 layout-row layout-align-center-center"
+            	style={{ marginBottom: '50px', height: '493px', boxShadow: 'rgba(0, 0, 0, 0.05) 2px 2px 1px' }}
+            >
+  	          <div className="flex-50" style={{height: '100%'}}>
+  	            <ShipmentContactForm
+  	              contactData={contactData}
+  	              theme={theme}
+  	              setContact={this.setContact}
+  	            />
+  	          </div>
 
-	          <div className="flex-50" style={{ height: '100%' }}>
-							<AddressBook
-								contacts={this.availableContacts()}
-								autofillContact={this.autofillContact}
-								theme={theme}
-							/>
-	          </div>
+  	          <div className="flex-50" style={{ height: '100%' }}>
+  							<AddressBook
+  								contacts={this.availableContacts()}
+  								autofillContact={this.autofillContact}
+  								theme={theme}
+  							/>
+  	          </div>
+            </div>
           </div>
           <div className="flex-100 layout-row layout-align-center-center">
 	          <ShipmentContactsBox
