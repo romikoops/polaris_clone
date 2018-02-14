4b1fac0dd9983ec6f7418067406c3bb28e88db1c
Christopher König <criss@paars.de>
2018-02-14 20:53:03 +0100

    CardLinkRow
    Former-commit-id: 274f23fdcea8ec9941097499dac72e6251bbfeaa

---
+++ client/app/components/CardLinkRow/CardLinkRow.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from '../../prop-types'
+import { CardLink } from '../CardLink/CardLink'
+import styles from './CardLinkRow.scss'
+
+export function CardLinkRow ({ cardArray, handleClick, theme }) {
+  const cards = cardArray.map(card => (
+    <CardLink
+      key={card.name}
+      text={card.name}
+      img={card.img}
+      path={card.url}
+      handleClick={card.handleClick ? card.handleClick : handleClick}
+      theme={theme}
+      options={card.options}
+    />
+  ))
+  return <div className={`${styles.card_link_row} layout-row flex-100`}>{cards}</div>
+}
+
+CardLinkRow.propTypes = {
+  theme: PropTypes.theme,
+  cardArray: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    img: PropTypes.string,
+    url: PropTypes.string,
+    handleClick: PropTypes.func,
+    options: PropTypes.object
+  })).isRequired,
+  handleClick: PropTypes.func.isRequired
+}
+
+CardLinkRow.defaultProps = {
+  theme: null
+}
+
+export default CardLinkRow
