ee9527adcfcf49da5ca3c018b5f51e6ff0b2dd4f
Christopher König <criss@paars.de>
2018-02-14 21:16:59 +0100

    ChooseShipment
    Former-commit-id: 59cbe418504ec037a4e58746dceab854aae2600d

---
+++ client/app/components/ChooseShipment/ChooseShipment.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import PropTypes from '../../prop-types'
+import styles from './ChooseShipment.scss'
+import { FlashMessages } from '../FlashMessages/FlashMessages'
+import defs from '../../styles/default_classes.scss'
+import { CardLinkRow } from '../CardLinkRow/CardLinkRow'
+import { LOAD_TYPES } from '../../constants'
+import { Tooltip } from '../Tooltip/Tooltip'
+import { TextHeading } from '../TextHeading/TextHeading'
+
+export class ChooseShipment extends Component {
+  constructor (props) {
+    super(props)
+    const cards = LOAD_TYPES.map(loadType => ({
+      name: loadType.name,
+      img: loadType.img,
+      options: { contained: true },
+      handleClick: () => this.props.selectLoadType(loadType.code)
+    }))
+    this.state = { cards }
+  }
+  render () {
+    const { theme, messages } = this.props
+    const flash = messages && messages.length > 0 ? <FlashMessages messages={messages} /> : ''
+
+    return (
+      <div className={`${styles.card_link_row} layout-row flex-100 layout-align-center`}>
+        {flash}
+        <div
+          className={`flex-none ${
+            defs.content_width
+          } layout-row layout-align-start-center layout-wrap`}
+        >
+          <div className={` ${styles.header} flex-100 layout-row layout-align-start-center`}>
+            <div className="flex-none">
+              <TextHeading theme={theme} size={1} text="Choose your shipment type:   " />
+            </div>
+            <Tooltip theme={theme} icon="fa-info-circle" text="shipment_mots" />
+          </div>
+          <CardLinkRow theme={theme} cardArray={this.state.cards} />
+        </div>
+      </div>
+    )
+  }
+}
+
+ChooseShipment.propTypes = {
+  theme: PropTypes.theme,
+  messages: PropTypes.arrayOf(PropTypes.string),
+  selectLoadType: PropTypes.func.isRequired
+}
+
+ChooseShipment.defaultProps = {
+  theme: null,
+  messages: []
+}
+
+export default ChooseShipment
