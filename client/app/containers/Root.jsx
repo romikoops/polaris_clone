ebb556f33fa35107fe4a166684ac1ff28c8a9a49
Christopher König <criss@paars.de>
2018-02-14 19:30:39 +0100

    Root
    Former-commit-id: e20f9482b85942d12f1cc9e332bfcc1fd25c9224

--- client/app/containers/Root.jsx
+++ client/app/containers/Root.jsx
@@ -1,27 +1,28 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Route } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
+import storeShape from 'react-redux/lib/utils/storeShape'
+import PropTypes from '../prop-types'
 
 import App from '../containers/App/App'
 import DevTools from './DevTools'
 
 export default function Root ({ store, history }) {
-  const devtools = process.env.NODE_ENV === 'development' ? (<DevTools />) : null
+  const devtools = process.env.NODE_ENV === 'development' ? <DevTools /> : null
   return (
     <Provider store={store}>
       <div>
         <ConnectedRouter history={history}>
           <Route path="/" component={App} />
         </ConnectedRouter>
-        { devtools }
+        {devtools}
       </div>
     </Provider>
   )
 }
 
 Root.propTypes = {
-  store: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired
+  store: storeShape.isRequired,
+  history: PropTypes.history.isRequired
 }
