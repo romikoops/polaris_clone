3d3d9d47d890ad69bf57407104d43c927d330b9c
Christopher König <criss@paars.de>
2018-02-14 22:21:19 +0100

    NamedSelect
    Former-commit-id: e0946933860fe355ece78baee611248d029bbb5b

---
+++ client/app/components/NamedSelect/NamedSelect.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types';
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
+import styled from 'styled-components'
+import PropTypes from '../../prop-types'
+
+export class NamedSelect extends Component {
+  constructor (props) {
+    super(props)
+    this.onChangeFunc = this.onChangeFunc.bind(this)
+  }
+  onChangeFunc (optionSelected) {
+    const modifiedOptionSelected = Object.assign({}, optionSelected)
+    modifiedOptionSelected.name = this.props.name
+
+    this.props.onChange(modifiedOptionSelected)
+  }
+  render () {
+    const props = Object.assign({}, this.props)
+    delete props.onChange
+
+    const StyledSelect = styled(Select)`
+            .Select-control {
+                background-color: #F9F9F9;
+                box-shadow: 0 2px 3px 0 rgba(237,234,234,0.5);
+                border: 1px solid #F2F2F2 !important;
+            }
+            .Select-menu-outer {
+                box-shadow: 0 2px 3px 0 rgba(237,234,234,0.5);
+                border: 1px solid #F2F2F2;
+            }
+            .Select-value {
+                background-color: #F9F9F9;
+                border: 1px solid #F2F2F2;
+            }
+            .Select-option {
+                background-color: #F9F9F9;
+            }
+        `
+    return (
+      <StyledSelect
+        {...props}
+        onChange={this.onChangeFunc}
+      />
+    )
+  }
+}
+
+NamedSelect.propTypes = {
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  ...Select.propTypes
+}
+
+export default NamedSelect
