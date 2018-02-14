d6adcaabc4944a696d1fa14e3999dec7daff4290
Christopher König <criss@paars.de>
2018-02-14 18:31:08 +0100

    Admin/index
    Former-commit-id: 28a2e05b959b9a4cd74ec2430b284edbe0c4bc66

--- client/app/components/Admin/index.js
+++ client/app/components/Admin/index.js
@@ -1,62 +1,60 @@
+export * from './AdminDashboard'
 
-export * from './AdminDashboard';
+export { default as AdminHubs } from './AdminHubs'
+export * from './AdminHubsIndex'
+export * from './AdminHubTile'
+export * from './AdminHubView'
+export * from './AdminHubForm'
 
-export * from './AdminHubs';
-export * from './AdminHubsIndex';
-export * from './AdminHubTile';
-export * from './AdminHubView';
-export * from './AdminHubForm';
+export * from './AdminNav'
+export * from './AdminNavItem'
 
-export * from './AdminNav';
-export * from './AdminNavItem';
+export * from './AdminItineraryRow'
+export * from './AdminTripPanel'
+export * from './AdminLayoverTile'
+export * from './AdminLayoverRow'
 
-export * from './AdminItineraryRow';
-export * from './AdminTripPanel';
-export * from './AdminLayoverTile';
-export * from './AdminLayoverRow';
+export * from './AdminPricePanel'
+export { default as AdminPricings } from './AdminPricings'
+export * from './AdminPriceEditor'
+export * from './AdminPriceCreator'
+// export { default as AdminPricingView } from './AdminPricingView'
+export * from './AdminPricingsIndex'
+export * from './AdminPricingsRouteIndex'
+export * from './AdminPricingsClientIndex'
+export * from './AdminPricingClientView'
+export * from './AdminPricingRouteView'
 
-export * from './AdminPricePanel';
-export * from './AdminPricings';
-export * from './AdminPriceEditor';
-export * from './AdminPriceCreator';
-export * from './AdminPricingView';
-export * from './AdminPricingsIndex';
-export * from './AdminPricingsRouteIndex';
-export * from './AdminPricingsClientIndex';
-export * from './AdminPricingClientView';
-export * from './AdminPricingRouteView';
+export { default as AdminTrucking } from './AdminTrucking'
+export * from './AdminTruckingCreator'
+export * from './AdminTruckingView'
+export * from './AdminTruckingIndex'
 
-export * from './AdminTrucking';
-export * from './AdminTruckingCreator';
-export * from './AdminTruckingView';
-export * from './AdminTruckingIndex';
+export * from './AdminRouteForm'
+export * from './AdminRouteTile'
+export * from './AdminRouteView'
+export * from './AdminRoutesIndex'
 
-export * from './AdminRouteForm';
-export * from './AdminRouteTile';
-export * from './AdminRouteView';
-export * from './AdminRoutesIndex';
+export * from './AdminClientTile'
+export { default as AdminClients } from './AdminClients'
+export * from './AdminClientsIndex'
+export * from './AdminClientView'
 
-export * from './AdminClientTile';
-export * from './AdminClients';
-export * from './AdminClientsIndex';
-export * from './AdminClientView';
+export * from './AdminSchedules'
 
-export * from './AdminSchedules';
+export { default as AdminShipments } from './AdminShipments'
+export * from './AdminShipmentsIndex'
+export * from './AdminShipmentRow'
+export * from './AdminShipmentView'
 
-export * from './AdminShipments';
-export * from './AdminShipmentsIndex';
-export * from './AdminShipmentRow';
-export * from './AdminShipmentView';
+export { default as AdminScheduleGenerator } from './AdminScheduleGenerator'
+export * from './AdminScheduleLine'
+export * from './AdminChargeSection'
+export * from './AdminServiceCharges'
+export * from './AdminChargePanel'
+export * from './AdminImportChargePanel'
+export * from './AdminExportChargePanel'
 
-export * from './AdminScheduleGenerator';
-export * from './AdminScheduleLine';
-export * from './AdminChargeSection';
-export * from './AdminServiceCharges';
-export * from './AdminChargePanel';
-export * from './AdminImportChargePanel';
-export * from './AdminExportChargePanel';
+export * from './AdminAddressTile'
 
-export * from './AdminAddressTile';
-
-export * from './SuperAdmin';
-
+export * from './SuperAdmin'
