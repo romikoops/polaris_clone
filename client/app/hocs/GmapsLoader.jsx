9ee188a76b525eb7a53e851c9629707c0e47743a
Christopher König <criss@paars.de>
2018-02-14 21:59:25 +0100

    GmapsLoader
    Former-commit-id: 783c4aefcad59b6c338bece22d04c23b705bce16

---
+++ client/app/hocs/GmapsLoader.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactGoogleMapLoader from 'react-google-maps-loader'
+import PropTypes from '../prop-types'
+import { API_KEY } from '../constants'
+
+export default function GmapsLoader (props) {
+  const apiKey = API_KEY
+  const params = {
+    key: apiKey, // Define your api key here
+    libraries: 'places' // To request multiple libraries, separate them with a comma
+  }
+  const ParamComponent = props.component
+  return (
+    <ReactGoogleMapLoader
+      params={params}
+      render={googleMaps =>
+        googleMaps && (
+          <ParamComponent
+            prevRequest={props.prevRequest}
+            allNexuses={props.allNexuses}
+            setTargetAddress={props.setTargetAddress}
+            theme={props.theme}
+            gMaps={googleMaps}
+            setCarriage={props.toggleCarriage}
+            origin={props.origin}
+            destination={props.destination}
+            shipment={props.shipment}
+            nextStageAttempt={props.nextStageAttempt}
+            handleAddressChange={props.handleAddressChange}
+            routeIds={props.routeIds}
+            nexusDispatch={props.nexusDispatch}
+            availableDestinations={props.availableDestinations}
+            handleSelectLocation={props.handleSelectLocation}
+          />
+        )
+      }
+    />
+  )
+}
+
+GmapsLoader.propTypes = {
+  theme: PropTypes.theme,
+  component: PropTypes.node.isRequired,
+  allNexuses: PropTypes.arrayOf(PropTypes.object),
+  origin: PropTypes.location.isRequired,
+  destination: PropTypes.location.isRequired,
+  prevRequest: PropTypes.shape({
+    shipment: PropTypes.object
+  }),
+  setTargetAddress: PropTypes.func.isRequired,
+  toggleCarriage: PropTypes.func.isRequired,
+  shipment: PropTypes.shipment,
+  nextStageAttempt: PropTypes.func.isRequired,
+  handleAddressChange: PropTypes.func.isRequired,
+  routeIds: PropTypes.arrayOf(PropTypes.object),
+  nexusDispatch: PropTypes.func.isRequired,
+  availableDestinations: PropTypes.arrayOf(PropTypes.object),
+  handleSelectLocation: PropTypes.func.isRequired
+}
+
+GmapsLoader.defaultProps = {
+  theme: null,
+  allNexuses: [],
+  availableDestinations: [],
+  routeIds: [],
+  prevRequest: null,
+  shipment: null
+}
