b69ceadeb109612ff3c18faf590f606a807c62f9
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-02-14 15:15:58 +0100

    changed the order of inputs and refactored how they are generated
    Former-commit-id: 26c378989da4ea5358c8254e3a652373b683ece1

--- client/app/components/ShipmentCargoItems/ShipmentCargoItems.js
+++ client/app/components/ShipmentCargoItems/ShipmentCargoItems.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import { ValidatedInput } from '../ValidatedInput/ValidatedInput';
 import PropTypes from 'prop-types';
-import { Checkbox } from '../Checkbox/Checkbox';
 import styles from './ShipmentCargoItems.scss';
 import defs from '../../styles/default_classes.scss';
-import { NamedSelect } from '../NamedSelect/NamedSelect';
 import '../../styles/select-css-custom.css';
 import { v4 } from 'node-uuid';
-import { Tooltip } from '../Tooltip/Tooltip';
-import ReactTooltip from 'react-tooltip';
 import { TextHeading } from '../TextHeading/TextHeading';
+import getInputs from './inputs';
 
 export class ShipmentCargoItems extends Component {
     constructor(props) {
@@ -85,7 +81,7 @@ export class ShipmentCargoItems extends Component {
         this.props.deleteItem('cargoItems', index);
     }
     render() {
-        const { cargoItems, handleDelta, theme, scope } = this.props;
+        const { cargoItems, theme } = this.props;
         const { cargoItemTypes } = this.state;
         const cargosAdded = [];
         const availableCargoItemTypes = this.props.availableCargoItemTypes ? (
@@ -105,232 +101,51 @@ export class ShipmentCargoItems extends Component {
         const textStyle = {
             background: theme && theme.colors ? '-webkit-linear-gradient(left, ' + theme.colors.primary + ',' + theme.colors.secondary + ')' : 'black'
         };
-        const placeholderInput = (
-            <input
-                className="flex-80"
-                type="number"
-            />
-        );
+
         const generateSeparator = () => (
             <div key={v4()} className={`${styles.separator} flex-100`}>
                 <hr/>
             </div>
         );
-        const generateCargoItem = (cargoItem, i) => (
-            <div
-                key={i}
-                className="layout-row flex-100 layout-wrap layout-align-start-center"
-                style={{ position: 'relative' }}
-            >
-                <div className="layout-row flex-100 layout-wrap layout-align-start-center" >
-                    <div className="layout-row flex-50 layout-wrap layout-align-start-center" >
-                        <div style={{ width: '97.75%' }}>
-                            <p className={`${styles.input_label} flex-100`}> Colli Type </p>
-                            <NamedSelect
-                                placeholder=""
-                                className={styles.select_100}
-                                name={`${i}-colliType`}
-                                value={cargoItemTypes[i]}
-                                options={availableCargoItemTypes}
-                                onChange={this.handleCargoItemType}
-                            />
-                        </div>
+        const generateCargoItem = (cargoItem, i) => {
+            const inputs = getInputs.call(
+                this,
+                cargoItem,
+                i,
+                theme,
+                cargoItemTypes,
+                availableCargoItemTypes,
+                numberOptions
+            );
+            return (
+                <div
+                    key={i}
+                    className="layout-row flex-100 layout-wrap layout-align-start-center"
+                    style={{ position: 'relative' }}
+                >
+                    <div className="layout-row flex-100 layout-wrap layout-align-start-center" >
+                        {inputs.colliType}
+                        {inputs.quantity}
+                        {inputs.grossWeight}
                     </div>
-                    <div className="layout-row flex layout-wrap layout-align-start-center" >
-                        <div className="layout-row flex-100 layout-wrap layout-align-start-center" >
-                            <p className={`${styles.input_label} flex-none`}> Gross Weight </p>
-                            <Tooltip theme={theme} icon="fa-info-circle" text="payload_in_kg" />
-                        </div>
-                        <div className={`flex-95 layout-row ${styles.input_box}`}>
-                            {
-                                cargoItem ? (
-                                    <ValidatedInput
-                                        className="flex-80"
-                                        name={`${i}-payload_in_kg`}
-                                        value={cargoItem.payload_in_kg}
-                                        type="number"
-                                        onChange={handleDelta}
-                                        firstRenderInputs={this.state.firstRenderInputs}
-                                        setFirstRenderInputs={this.setFirstRenderInputs}
-                                        nextStageAttempt={this.props.nextStageAttempt}
-                                        validations={{
-                                            nonNegative: (values, value) => value > 0
-                                        }}
-                                        validationErrors={{
-                                            nonNegative: 'Must be greater than 0',
-                                            isDefaultRequiredValue: 'Must be greater than 0'
-                                        }}
-                                        required
-                                    />
-                                ) : placeholderInput
-                            }
-                            <div className="flex-20 layout-row layout-align-center-center">
-                                kg
-                            </div>
-                        </div>
+                    <div className="layout-row flex-100 layout-wrap layout-align-start-center" >
+                        {inputs.length}
+                        {inputs.height}
+                        {inputs.width}
+                        {inputs.dangerousGoods}
                     </div>
-                    <div className="layout-row flex layout-wrap layout-align-start-center" >
-                        <p className={`${styles.input_label} flex-100`}> Height </p>
-                        <div className={`flex-95 layout-row ${styles.input_box}`}>
-                            {
-                                cargoItem ? (
-                                    <ValidatedInput
-                                        className="flex-80"
-                                        name={`${i}-dimension_z`}
-                                        value={cargoItem.dimension_z}
-                                        type="number"
-                                        min="0"
-                                        step="any"
-                                        onChange={handleDelta}
-                                        firstRenderInputs={this.state.firstRenderInputs}
-                                        setFirstRenderInputs={this.setFirstRenderInputs}
-                                        nextStageAttempt={this.props.nextStageAttempt}
-                                        validations={{
-                                            nonNegative: (values, value) => value > 0,
-                                            maxDimention: (values, value) => value < 1000
-                                        }}
-                                        validationErrors={{
-                                            isDefaultRequiredValue: 'Must be greater than 0',
-                                            nonNegative: 'Must be greater than 0',
-                                            maxDimention: 'Maximum height is 1000'
-                                        }}
-                                        required
-                                    />
-                                ) : placeholderInput
-                            }
-                            <div className="flex-20 layout-row layout-align-center-center">
-                                cm
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="layout-row flex-100 layout-wrap layout-align-start-center" >
-                    <div className="layout-row flex layout-wrap layout-align-start-center" >
-                        <p className={`${styles.input_label} flex-100`}> Length </p>
-                        <ReactTooltip />
-                        <div
-                            className={`flex-95 layout-row ${styles.input_box}`}
-                            data-tip={
-                                cargoItem && !!cargoItemTypes[i].dimension_x ? (
-                                    'Length is automatically set by \'Collie Type\''
-                                ) : ''
-                            }
-                        >
-                            {
-                                cargoItem ? (
-                                    <ValidatedInput
-                                        className="flex-80"
-                                        name={`${i}-dimension_x`}
-                                        value={cargoItem.dimension_x}
-                                        type="number"
-                                        min="0"
-                                        step="any"
-                                        onChange={handleDelta}
-                                        firstRenderInputs={this.state.firstRenderInputs}
-                                        setFirstRenderInputs={this.setFirstRenderInputs}
-                                        nextStageAttempt={this.props.nextStageAttempt}
-                                        validations={{
-                                            nonNegative: (values, value) => value > 0,
-                                            maxDimention: (values, value) => value < 1000
-                                        }}
-                                        validationErrors={{
-                                            isDefaultRequiredValue: 'Must be greater than 0',
-                                            nonNegative: 'Must be greater than 0',
-                                            maxDimention: 'Maximum length is 1000'
-                                        }}
-                                        required
-                                        disabled={!!cargoItemTypes[i].dimension_x}
-                                    />
-                                ) : placeholderInput
-                            }
-                            <div className="flex-20 layout-row layout-align-center-center">
-                                cm
-                            </div>
-                        </div>
-                    </div>
-                    <div className="layout-row flex layout-wrap layout-align-start-center" >
-                        <p className={`${styles.input_label} flex-100`}> Width </p>
-                        <ReactTooltip />
-                        <div
-                            className={`flex-95 layout-row ${styles.input_box}`}
-                            data-tip={
-                                cargoItem && !!cargoItemTypes[i].dimension_y ? (
-                                    'Width is automatically set by \'Collie Type\''
-                                ) : ''
-                            }
-                        >
-                            {
-                                cargoItem ? (
-                                    <ValidatedInput
-                                        className="flex-80"
-                                        name={`${i}-dimension_y`}
-                                        value={cargoItem.dimension_y}
-                                        type="number"
-                                        min="0"
-                                        step="any"
-                                        onChange={handleDelta}
-                                        firstRenderInputs={this.state.firstRenderInputs}
-                                        setFirstRenderInputs={this.setFirstRenderInputs}
-                                        nextStageAttempt={this.props.nextStageAttempt}
-                                        validations={{
-                                            nonNegative: (values, value) => value > 0,
-                                            maxDimention: (values, value) => value < 1000
-                                        }}
-                                        validationErrors={{
-                                            isDefaultRequiredValue: 'Must be greater than 0',
-                                            nonNegative: 'Must be greater than 0',
-                                            maxDimention: 'Maximum width is 1000'
-                                        }}
-                                        disabled={!!cargoItemTypes[i].dimension_y}
-                                        required
-                                    />
-                                ) : placeholderInput
-                            }
-                            <div className="flex-20 layout-row layout-align-center-center">
-                                cm
-                            </div>
-                        </div>
-                    </div>
-                    <div className="layout-row flex layout-wrap layout-align-start-center" >
-                        <p className={`${styles.input_label} flex-100`}> No. of Cargo Items </p>
-                        <NamedSelect
-                            placeholder={cargoItem ? cargoItem.quantity : ''}
-                            className={`${styles.select} flex-95`}
-                            name={`${i}-quantity`}
-                            value={cargoItem ? cargoItem.quantity : ''}
-                            options={cargoItem ? numberOptions : ''}
-                            onChange={this.handleCargoItemQ}
-                        />
-                    </div>
-                    <div
-                        className="layout-row flex layout-wrap layout-align-start-center"
-                    >
-                        <div className="layout-row flex-100 layout-wrap layout-align-start-center">
-                            <p className={`${styles.input_label} flex-none`}> Dangerous Goods </p>
-                            <Tooltip theme={theme} icon="fa-info-circle" text="dangerous_goods" />
-                        </div>
-                        <Checkbox
-                            name={`${i}-dangerous_goods`}
-                            onChange={() => this.toggleDangerousGoods(i)}
-                            checked={cargoItem ? cargoItem.dangerousGoods : false}
-                            theme={theme}
-                            size="34px"
-                            disabled={!scope.dangerous_goods}
-                            onClick={scope.dangerous_goods ? '' : this.props.showAlertModal}
-                        />
-                    </div>
-                </div>
 
-                {
-                    cargoItem ? (
-                        <i
-                            className={`fa fa-trash ${styles.delete_icon}`}
-                            onClick={() => this.deleteCargo(i)}
-                        ></i>
-                    ) : ''
-                }
-            </div>
-        );
+                    {
+                        cargoItem ? (
+                            <i
+                                className={`fa fa-trash ${styles.delete_icon}`}
+                                onClick={() => this.deleteCargo(i)}
+                            ></i>
+                        ) : ''
+                    }
+                </div>
+            );
+        };
 
         if (cargoItems) {
             cargoItems.forEach((cargoItem, i) => {
