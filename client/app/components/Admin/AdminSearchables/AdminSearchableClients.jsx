63b38f030964ad4f04e78021fa2b51fd8ebe5f75
Christopher König <criss@paars.de>
2018-02-14 18:34:56 +0100

    AdminSearchableClients
    Former-commit-id: e4c36bdde8656d441c33664b36a780e39b465488

---
+++ client/app/components/Admin/AdminSearchables/AdminSearchableClients.jsx
@@ -0,0 +1,152 @@
+import React, { Component } from 'react'
+import { v4 } from 'node-uuid'
+import Fuse from 'fuse.js'
+import PropTypes from '../../../prop-types'
+import styles from '../Admin.scss'
+import { AdminClientTile } from '../'
+import { TextHeading } from '../../TextHeadings/TextHeading'
+
+export class AdminSearchableClients extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      clients: props.clients
+    }
+    this.handleSearchChange = this.handleSearchChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.seeAll = this.seeAll.bind(this)
+  }
+  componentDidUpdate (prevProps) {
+    if (prevProps.clients !== this.props.clients) {
+      this.handleSearchChange({ target: { value: '' } })
+    }
+  }
+  handleClick (client) {
+    const { handleClick, adminDispatch } = this.props
+    if (handleClick) {
+      handleClick(client)
+    } else {
+      adminDispatch.getClient(client.id, true)
+    }
+  }
+  seeAll () {
+    const { seeAll, adminDispatch } = this.props
+    if (seeAll) {
+      seeAll()
+    } else {
+      adminDispatch.goTo('/clients')
+    }
+  }
+  handleSearchChange (event) {
+    console.log(this.props.clients)
+    if (event.target.value === '') {
+      this.setState({
+        clients: this.props.clients
+      })
+      return
+    }
+    const search = (keys) => {
+      const options = {
+        shouldSort: true,
+        tokenize: true,
+        threshold: 0.2,
+        location: 0,
+        distance: 50,
+        maxPatternLength: 32,
+        minMatchCharLength: 5,
+        keys
+      }
+      const fuse = new Fuse(this.props.clients, options)
+      return fuse.search(event.target.value)
+    }
+    const filteredClients = search(['first_name', 'last_name', 'company_name', 'phone', 'email'])
+
+    this.setState({
+      clients: filteredClients
+    })
+  }
+  render () {
+    const {
+      theme, title, seeAll, placeholder
+    } = this.props
+
+    const { clients } = this.state
+    let clientsArr
+    if (clients) {
+      clientsArr = clients.map(client => (
+        <AdminClientTile key={v4()} client={client} theme={theme} handleClick={this.handleClick} />
+      ))
+    }
+    const viewType = this.props.sideScroll ? (
+      <div className={`layout-row flex-100 layout-align-start-center ${styles.slider_container}`}>
+        <div className={`layout-row flex-none layout-align-start-center ${styles.slider_inner}`}>
+          {clientsArr}
+        </div>
+      </div>
+    ) : (
+      <div className="layout-row flex-100 layout-align-start-center ">
+        <div className="layout-row flex-none layout-align-start-center layout-wrap">
+          {clientsArr}
+        </div>
+      </div>
+    )
+    return (
+      <div
+        className={`layout-row flex-100 layout-wrap layout-align-start-center ${styles.searchable}`}
+      >
+        <div
+          className={`flex-100 layout-row layout-align-space-between-center ${
+            styles.searchable_header
+          }`}
+        >
+          <div className="flex-60 layoput-row layout-align-start-center">
+            <TextHeading theme={theme} size={1} text={title || 'Clients'} />
+          </div>
+
+          <div className={`${styles.input_box} flex-40 layout-row layout-align-start-center`}>
+            <input
+              type="text"
+              name="search"
+              placeholder={placeholder || 'Search clients'}
+              onChange={this.handleSearchChange}
+            />
+          </div>
+        </div>
+        <div className="flex-100 layout-row layout-align-center layout-align-space-between">
+          {viewType}
+        </div>
+        {seeAll !== false ? (
+          <div className="flex-100 layout-row layout-align-end-center">
+            <div className="flex-none layout-row layout-align-center-center" onClick={this.seeAll}>
+              <p className="flex-none">See all</p>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    )
+  }
+}
+AdminSearchableClients.propTypes = {
+  clients: PropTypes.arrayOf(PropTypes.client).isRequired,
+  handleClick: PropTypes.func,
+  adminDispatch: PropTypes.shape({
+    getClient: PropTypes.func,
+    goTo: PropTypes.func
+  }).isRequired,
+  seeAll: PropTypes.func,
+  title: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  sideScroll: PropTypes.bool,
+  theme: PropTypes.theme
+}
+
+AdminSearchableClients.defaultProps = {
+  handleClick: null,
+  seeAll: null,
+  sideScroll: false,
+  theme: null
+}
+
+export default AdminSearchableClients
