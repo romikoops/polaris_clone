0cb0bd992ca494c826a221687026276af81067e3
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-02-15 18:57:33 +0100

    adapted validation on location box to work both directions
    Former-commit-id: 3be072bfd73489797f5d2f318bbf7f05040aad52

--- app/controllers/nexuses_controller.rb
+++ app/controllers/nexuses_controller.rb
@@ -4,7 +4,7 @@ class NexusesController < ApplicationController
   skip_before_action :require_non_guest_authentication!
 	def index
 		formatted_available_nexuses = format_for_select_box(find_available_nexuses)
-		response_handler("available_#{params[:target]}" => formatted_available_nexuses )
+		response_handler("available#{params[:target].capitalize}s" => formatted_available_nexuses )
 	end
 
 	def find_nexus
@@ -17,34 +17,29 @@ class NexusesController < ApplicationController
 
 	private
 
-	# def find_available_destinations 
-	# 	# itineraries = Itinerary.where(id: params[:itinerary_ids].split(","))
-	# 	ids = params[:itinerary_ids].split(",").map { |e| e.to_i }
-	# 	itineraries = retrieve_route_options(current_user.tenant_id, ids)
-	# 	# byebug
-	# 	return itineraries.map{ |itinerary| Location.find(itinerary["destination_nexus_id"])} if params[:origin].nil?
-
-	# 	origin = Location.geocoded_location params[:origin]
-	# 	origin_nexus_data = origin.closest_location_with_distance
-	# 	origin_nexus = origin_nexus_data.first if origin_nexus_data.last <= 200
-	# 	itineraries.reject {|itinerary| itinerary["origin_nexus_id"] != origin_nexus.id}.map { |itinerary2| Location.find(itinerary2["destination_nexus_id"])  }.uniq
-	# end
-
 	def find_available_nexuses
+		# Expects a target param which is either "origin" or "destination"
+		# Then the following variables are assigned:
+		#
+		#      target #=> "origin",      counterpart #=> "destination" 
+		#  or  target #=> "destination", counterpart #=> "origin" 
+
+		user_input 			= params[:user_input]
+		target 					= params[:target]
+		counterpart     = target == "destination" ? "origin" : "destination"
+
 		itinerary_ids = params[:itinerary_ids].split(",").map(&:to_i)
 		itineraries   = retrieve_route_options(current_user.tenant_id, itinerary_ids)
 
-		user_input = params[:user_input]
-		target = params[:target]
-		source = target == "destination" ? "origin" : "destination"
 
-		return itineraries.map { |itinerary| Location.find(itinerary["#{target}_nexus_id"])} if params[:user_input].nil?
+		return itineraries.map { |itinerary| Location.find(itinerary["#{target}_nexus_id"])} if user_input.blank?
 
-		user_input = Location.geocoded_location params[:user_input]
-		nexus_data = user_input.closest_location_with_distance
+		nexus = Location.geocoded_location user_input
+		nexus_data = nexus.closest_location_with_distance
 		nexus = nexus_data.first if nexus_data.last <= 200
-		itineraries.select { |itinerary| itinerary["#{source}_nexus_id"] == nexus.id }
-			.map { |itinerary| Location.find(itinerary["#{source}_nexus_id"]) }.uniq
+
+		itineraries.select { |itinerary| itinerary["#{counterpart}_nexus_id"] == nexus.id }
+			.map { |itinerary| Location.find(itinerary["#{target}_nexus_id"]) }.uniq
 	end
 
 	def format_for_select_box(nexuses)
