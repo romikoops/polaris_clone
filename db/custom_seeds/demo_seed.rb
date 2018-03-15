598bbb997d45c294df2fd6c7680d7f61327576c5
DaddyWozBucks <wkbeamish@gmail.com>
2018-03-15 19:31:02 +0100

    quick fix
    Former-commit-id: f28a8510c940535c054136907ff790f48fd6c052

--- db/custom_seeds/demo_seed.rb
+++ db/custom_seeds/demo_seed.rb
@@ -23,10 +23,10 @@ include MongoTools
   # overwrite_mongo_lcl_pricings(req, dedicated = true, shipper, true)
 
   # # # # # # Overwrite public pricings from excel sheet
-  puts "# Overwrite public pricings from excel sheet"
-  public_pricings = File.open("#{Rails.root}/db/dummydata/standard_sheet.xlsx")
-  req = {"xlsx" => public_pricings}
-  overwrite_freight_rates(req, shipper, false)
+  # puts "# Overwrite public pricings from excel sheet"
+  # public_pricings = File.open("#{Rails.root}/db/dummydata/standard_sheet.xlsx")
+  # req = {"xlsx" => public_pricings}
+  # overwrite_freight_rates(req, shipper, false)
   # # Overwrite public pricings from excel sheet
 
 
@@ -42,16 +42,16 @@ include MongoTools
 
 
   # # # # Overwrite trucking data from excel sheet
-  # puts "# Overwrite trucking data from excel sheet"
-  # ["import", "export"].each do |dir|
-  #   trucking = File.open("#{Rails.root}/db/dummydata/5_trucking_rates_per_city.xlsx")
-  #   req = {"xlsx" => trucking}
-  #   overwrite_zipcode_weight_trucking_rates(req, shipper, dir)
-  # end
-  # ["import", "export"].each do |dir|
-  #   trucking = File.open("#{Rails.root}/db/dummydata/shanghai_trucking.xlsx")
-  #   req = {"xlsx" => trucking}
-  #   overwrite_city_trucking_rates(req, shipper, dir)
-  # end
+  puts "# Overwrite trucking data from excel sheet"
+  ["import", "export"].each do |dir|
+    trucking = File.open("#{Rails.root}/db/dummydata/5_trucking_rates_per_city.xlsx")
+    req = {"xlsx" => trucking}
+    overwrite_zipcode_weight_trucking_rates(req, shipper, dir)
+  end
+  ["import", "export"].each do |dir|
+    trucking = File.open("#{Rails.root}/db/dummydata/shanghai_trucking.xlsx")
+    req = {"xlsx" => trucking}
+    overwrite_city_trucking_rates(req, shipper, dir)
+  end
   # tenant.update_route_details()
 end
