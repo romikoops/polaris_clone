f0c9b97ac208ae9ba7095a0e6a814c827af4e549
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-03-16 12:48:56 +0100

    added booking summary component
    Former-commit-id: 2109bf7e55dd63158f33419ec0240b6d16059133

---
+++ client/app/components/BookingSummary/BookingSummary.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import styles from './BookingSummary.scss'
+
+function BookingSummary (props) {
+  return (
+    <div className="flex-100 layout-row">
+      TEST
+    </div>
+  )
+}
+
+BookingSummary.propTypes = {
+
+}
+
+BookingSummary.defaulProps = {
+
+}
