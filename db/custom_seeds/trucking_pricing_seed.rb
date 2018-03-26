d775bfc74018f74f541b54a5b8eb6436384efff7
DaddyWozBucks <wkbeamish@gmail.com>
2018-03-26 15:46:21 +0200

    for seeding dev
    Former-commit-id: fad9ef68e8332e56de46afa3b0cdc3608cae89c8

--- db/custom_seeds/trucking_pricing_seed.rb
+++ db/custom_seeds/trucking_pricing_seed.rb
@@ -7,3 +7,17 @@
     req = {"xlsx" => trucking}
     overwrite_zipcode_trucking_rates_by_hub(req, shipper, hub.id, 'GC Trucking', dir)
   end
+
+  hub = tenant.hubs.find_by_name("Gothenburg Port")
+  ["import", "export"].each do |dir|
+    trucking = File.open("#{Rails.root}/db/dummydata/FTL_DISTANCE_SHEET.xlsx")
+    req = {"xlsx" => trucking}
+    overwrite_distance_trucking_rates_by_hub(req, shipper, hub.id, 'GC Trucking', dir, 'SE')
+  end
+
+  ["import", "export"].each do |dir|
+    hub = tenant.hubs.find_by_name("Shanghai Port")
+    trucking = File.open("#{Rails.root}/db/dummydata/shanghai_trucking.xlsx")
+    req = {"xlsx" => trucking}
+    overwrite_city_trucking_rates_by_hub(req, shipper,  hub.id, 'Globelink LTL', dir)
+  end
