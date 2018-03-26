adc1f60c357743825b03be9e919d0102306aaa8d
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-03-26 18:07:17 +0200

    added query find_by_hub_ids
    Former-commit-id: f786021fd84bb0ff9d9aafeccf4b3b41101931e1

--- app/models/trucking_pricing.rb
+++ app/models/trucking_pricing.rb
@@ -60,6 +60,43 @@ class TruckingPricing < ApplicationRecord
     TruckingPricing.where(id: ids)
   end
 
+
+  def self.find_by_hub_ids(args = {})
+    hub_ids = args[:hub_ids]
+    raise ArgumentError, "Must provide hub_ids" if hub_ids.nil?
+    raise ArgumentError, "Must provide tenant_id" if args[:tenant_id].nil?
+
+    result = ActiveRecord::Base.connection.exec_query("
+      SELECT trucking_pricings.id, (
+        CASE
+          WHEN MAX(trucking_destinations.zipcode) != '0'
+            THEN ('zipcode', MIN(trucking_destinations.zipcode), MAX(trucking_destinations.zipcode))
+          WHEN MAX(trucking_destinations.distance) != '0'
+            THEN ('distance', MIN(trucking_destinations.distance), MAX(trucking_destinations.distance))
+          ELSE
+            ('city', MAX(trucking_destinations.city_name))
+        END
+      ) AS filter
+      FROM  trucking_pricings
+      JOIN  hub_truckings         ON hub_truckings.trucking_pricing_id     = trucking_pricings.id
+      JOIN  trucking_destinations ON hub_truckings.trucking_destination_id = trucking_destinations.id
+      JOIN  hubs                  ON hub_truckings.hub_id                  = hubs.id
+      JOIN  locations             ON hubs.location_id                      = locations.id
+      JOIN  tenants               ON hubs.tenant_id                        = tenants.id
+      WHERE tenants.id = #{args[:tenant_id]}
+      AND   hubs.id IN #{hub_ids.sql_format}
+      GROUP BY trucking_pricings.id
+    ")
+
+    result.each_with_object({}) do |row, h|
+      filter = parse_sql_array(row["filter"])
+      h[row["id"]] = {
+        "trucking_pricing"  => find(row["id"]),
+        filter.first => filter[1..-1]
+      }
+    end
+  end
+
   # Instance Methods
   def nexus_id
     ActiveRecord::Base.connection.execute("
@@ -72,15 +109,15 @@ class TruckingPricing < ApplicationRecord
     ").values.first.first
   end
 
-  # def hub_id
-  #   ActiveRecord::Base.connection.execute("
-  #     SELECT hubs.id FROM hubs
-  #     JOIN hub_truckings ON hub_truckings.hub_id = hubs.id
-  #     JOIN trucking_pricings ON hub_truckings.trucking_pricing_id = trucking_pricings.id
-  #     WHERE trucking_pricings.id = #{self.id}
-  #     LIMIT 1
-  #   ").values.first.first
-  # end
+  def hub_id
+    ActiveRecord::Base.connection.execute("
+      SELECT hubs.id FROM hubs
+      JOIN hub_truckings ON hub_truckings.hub_id = hubs.id
+      JOIN trucking_pricings ON hub_truckings.trucking_pricing_id = trucking_pricings.id
+      WHERE trucking_pricings.id = #{self.id}
+      LIMIT 1
+    ").values.first.first
+  end
 
   private
 
@@ -99,4 +136,9 @@ class TruckingPricing < ApplicationRecord
   def self.nexuses_condition(args)
     args[:nexus_ids] ? "AND locations.id IN #{args[:nexus_ids].sql_format}" : ""
   end
+
+  def self.parse_sql_array(str)
+    str.gsub(/\(|\)|\"/, "").split(",")
+  end
+
 end
