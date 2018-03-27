8d969816f131663011706fe11f95ce14a6e5b97e
DaddyWozBucks <wkbeamish@gmail.com>
2018-03-27 09:46:01 +0200

    panic push
    Former-commit-id: 4b844ee89e671d5f3f2a00ed9e8b25001a8ca9ee

--- db/custom_seeds/demo_seed.rb
+++ db/custom_seeds/demo_seed.rb
@@ -1,6 +1,6 @@
 include ExcelTools
 include MongoTools
-['demo'].each do |sub|
+['greencarrier'].each do |sub|
 # # Tenant.all.each do |tenant|
   tenant = Tenant.find_by_subdomain(sub)
   shipper = tenant.users.where(role_id: 2).first
@@ -23,11 +23,11 @@ include MongoTools
 #   # overwrite_mongo_lcl_pricings(req, dedicated = true, shipper, true)
 
 #   # # # # # Overwrite public pricings from excel sheet
-#   # puts "# Overwrite public pricings from excel sheet"
-#   public_pricings = File.open("#{Rails.root}/db/dummydata/standard_sheet.xlsx")
-#   req = {"xlsx" => public_pricings}
-#   overwrite_freight_rates(req, shipper, false)
-#   # Overwrite public pricings from excel sheet
+  # puts "# Overwrite public pricings from excel sheet"
+  public_pricings = File.open("#{Rails.root}/db/dummydata/standard_sheet.xlsx")
+  req = {"xlsx" => public_pricings}
+  overwrite_freight_rates(req, shipper, false)
+  # Overwrite public pricings from excel sheet
 
 
   # # puts "# Overwrite MAERSK pricings from excel sheet"
@@ -42,16 +42,16 @@ include MongoTools
 
 
   # # # # # Overwrite trucking data from excel sheet
-  # puts "# Overwrite trucking data from excel sheet"
-  # ["import", "export"].each do |dir|
-  #   trucking = File.open("#{Rails.root}/db/dummydata/5_trucking_rates_per_city.xlsx")
-  #   req = {"xlsx" => trucking}
-  #   overwrite_zipcode_weight_trucking_rates(req, shipper, dir)
-  # end
-  # ["import", "export"].each do |dir|
-  #   trucking = File.open("#{Rails.root}/db/dummydata/shanghai_trucking.xlsx")
-  #   req = {"xlsx" => trucking}
-  #   overwrite_city_trucking_rates(req, shipper, dir)
-  # end
-  # tenant.update_route_details()
+  puts "# Overwrite trucking data from excel sheet"
+  ["import", "export"].each do |dir|
+    trucking = File.open("#{Rails.root}/db/dummydata/5_trucking_rates_per_city.xlsx")
+    req = {"xlsx" => trucking}
+    overwrite_zipcode_weight_trucking_rates(req, shipper, dir)
+  end
+  ["import", "export"].each do |dir|
+    trucking = File.open("#{Rails.root}/db/dummydata/shanghai_trucking.xlsx")
+    req = {"xlsx" => trucking}
+    overwrite_city_trucking_rates(req, shipper, dir)
+  end
+  tenant.update_route_details()
 end
