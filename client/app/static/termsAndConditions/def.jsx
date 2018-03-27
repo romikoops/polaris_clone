2ac27c7ae9f8e2c10fca1ef7caa73e571a026f8c
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-03-27 18:06:23 +0200

    added a static page folder and made greencarrier and demo page
    Former-commit-id: a80337c689b0d9e2595c3c34a626b84cbe133317

---
+++ client/app/static/termsAndConditions/def.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const def = (
+  <h3>
+    No Terms and Conditions Available
+  </h3>
+)
+
+export default def
---
+++ client/app/static/termsAndConditions/index.js
@@ -0,0 +1,11 @@
+import greencarrier from './greencarrier'
+import demo from './demo'
+import def from './def'
+
+const termsAndConditions = {
+  greencarrier,
+  demo,
+  def
+}
+
+export default termsAndConditions
