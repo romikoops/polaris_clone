b6bfcb87b72cb36a7addbb1956cc7adf444d40f8
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-03-28 13:58:27 +0200

    removed unecessary migrations
    Former-commit-id: bacc309d36139ffab725cb2f3276060a2217ece4

--- db/migrate/20180103084555_drop_vehicle_types.rb
+++
@@ -1,5 +0,0 @@
-class DropVehicleTypes < ActiveRecord::Migration[5.1]
-  def change
-  	drop_table :vehicle_types
-  end
-end
--- db/migrate/20180103084905_drop_transport_types.rb
+++
@@ -1,5 +0,0 @@
-class DropTransportTypes < ActiveRecord::Migration[5.1]
-  def change
-	  drop_table :transport_types
-  end
-end
