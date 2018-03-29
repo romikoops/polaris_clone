60e7354db82b87758a9ff1575070db0c96e7afb5
DaddyWozBucks <wkbeamish@gmail.com>
2018-03-29 18:49:28 +0200

    for angelica
    Former-commit-id: a628e7753f2141a2c3293411a46ade5f91ed3851

--- app/classes/excel_tools.rb
+++ app/classes/excel_tools.rb
@@ -689,7 +689,7 @@ module ExcelTools
         row_data = first_sheet.row(line)
         new_pricing = {}
         td = TruckingDestination.find_or_create_by!(city_name: Location.get_trucking_city("#{row_data[1]}, #{row_data[0]}"), country_code: 'CN')
-        hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id, courier_id: courier.id)
+        hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id)
         
         
         new_pricing[direction] = {"table" => []}
@@ -805,7 +805,7 @@ module ExcelTools
 
           td = TruckingDestination.find_or_create_by!(distance: range_values[0], country_code: country_code)
           trucking_destinations[range_key] << td
-          hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id, courier_id: courier.id)
+          hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id)
           hub_truckings[range_key] << hub_trucking
           if !aux_data[range_key]
             aux_data[range_key] = {}
@@ -817,7 +817,7 @@ module ExcelTools
             ((range_values[0] + 1)...range_values[1]).each do |dist|
               td = TruckingDestination.find_or_create_by!(distance: dist, country_code: country_code)
               trucking_destinations[range_key] << td
-              hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id, courier_id: courier.id, trucking_pricing_id: trucking_pricings[range_key])
+              hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id, trucking_pricing_id: trucking_pricings[range_key])
               hub_truckings[range_key] << hub_trucking
             end
           else
@@ -827,7 +827,7 @@ module ExcelTools
             (range_values[0]...range_values[1]).each do |dist|
               td = TruckingDestination.find_or_create_by!(distance: dist, country_code: country_code)
               trucking_destinations[range_key] << td
-              hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id, courier_id: courier.id, trucking_pricing_id: trucking_pricings[range_key])
+              hub_trucking = HubTrucking.find_or_initialize_by(trucking_destination_id: td.id, hub_id: hub.id, trucking_pricing_id: trucking_pricings[range_key])
               hub_truckings[range_key] << hub_trucking
             end
           end
--- db/custom_seeds/demo_seed.rb
+++ db/custom_seeds/demo_seed.rb
@@ -1,6 +1,6 @@
 include ExcelTools
 include MongoTools
-['greencarrier'].each do |sub|
+['demo'].each do |sub|
 # # Tenant.all.each do |tenant|
   tenant = Tenant.find_by_subdomain(sub)
   shipper = tenant.users.where(role_id: 2).first
@@ -46,25 +46,25 @@ include MongoTools
   # # # # # Overwrite trucking data from excel sheet
 
   # puts "# Overwrite trucking data from excel sheet" (params, user = current_user, hub_id, courier_name, direction, country_code)
-  # hub = tenant.hubs.find_by_name("Gothenburg Port")
-  # ["import", "export"].each do |dir|
-  #   trucking = File.open("#{Rails.root}/db/dummydata/FTL_DISTANCE_SHEET.xlsx")
-  #   req = {"xlsx" => trucking}
-  #   overwrite_distance_trucking_rates_by_hub(req, shipper, hub.id, 'GC Trucking', dir, "SE")
-  # end
-  # ["import", "export"].each do |dir|
-  #   hub = tenant.hubs.find_by_name("Shanghai Port")
-  #   trucking = File.open("#{Rails.root}/db/dummydata/shanghai_trucking.xlsx")
-  #   req = {"xlsx" => trucking}
-  #   overwrite_city_trucking_rates_by_hub(req, shipper,  hub.id, 'Globelink LTL', dir)
-  # end
   hub = tenant.hubs.find_by_name("Gothenburg Port")
   ["import", "export"].each do |dir|
-    trucking = File.open("#{Rails.root}/db/dummydata/5_trucking_rates_per_city.xlsx")
+    trucking = File.open("#{Rails.root}/db/dummydata/FTL_DISTANCE_SHEET.xlsx")
     req = {"xlsx" => trucking}
-    split_zip_code_sections(req, shipper, hub.id, 'GC Trucking', dir) 
-    # overwrite_zipcode_trucking_rates_by_hub(req, shipper, hub.id, 'GC Trucking', dir)
+    overwrite_distance_trucking_rates_by_hub(req, shipper, hub.id, 'GC Trucking', dir, "SE")
   end
+  ["import", "export"].each do |dir|
+    hub = tenant.hubs.find_by_name("Shanghai Port")
+    trucking = File.open("#{Rails.root}/db/dummydata/shanghai_trucking.xlsx")
+    req = {"xlsx" => trucking}
+    overwrite_city_trucking_rates_by_hub(req, shipper,  hub.id, 'Globelink LTL', dir)
+  end
+  # hub = tenant.hubs.find_by_name("Gothenburg Port")
+  # ["import", "export"].each do |dir|
+  #   trucking = File.open("#{Rails.root}/db/dummydata/5_trucking_rates_per_city.xlsx")
+  #   req = {"xlsx" => trucking}
+  #   split_zip_code_sections(req, shipper, hub.id, 'GC Trucking', dir) 
+  #   # overwrite_zipcode_trucking_rates_by_hub(req, shipper, hub.id, 'GC Trucking', dir)
+  # end
   
   
   # tenant.update_route_details()
