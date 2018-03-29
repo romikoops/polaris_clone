4c190b099307481afa9271896fdf611e6bc4b22b
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-03-29 14:39:16 +0200

    added shipment card component and implemented it in shipmentView
    Former-commit-id: 6dfce68216e11ffad741df55105b5da57799586b

---
+++ client/app/components/ShipmentCard/MainPanel/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Proptypes from '../../../prop-types'
+import styles from './ShipmentCardMainPanel.scss'
+
+export default class ShipmentCardMainPanel extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  componentDidMount () {
+    this.updateHeight()
+  }
+  updateHeight () {
+    this.setState({
+      panelHeight: this.panel.clientHeight
+    })
+  }
+  render () {
+    const { collapsed, content } = this.props
+    return (
+      <div
+        className={`${collapsed ? styles.collapsed : ''} ${styles.main_panel}`}
+        style={{
+          maxHeight: this.state.panelHeight,
+          transition: `max-height ${Math.log(1 + this.state.panelHeight) / 10}s linear`
+        }}
+        ref={(div) => { this.panel = div }}
+      >
+        <div className={
+          `${styles.inner_wrapper} flex-100 ` +
+          'layout-row layout-wrap layout-align-start-start'
+        }
+        >
+          { content }
+        </div>
+      </div>
+    )
+  }
+}
+
+ShipmentCardMainPanel.propTypes = {
+  collapsed: Proptypes.bool,
+  content: Proptypes.node
+}
+
+ShipmentCardMainPanel.defaultProps = {
+  collapsed: false,
+  content: ''
+}
