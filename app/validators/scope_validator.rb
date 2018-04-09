5af842e5f0a2affc4409f7fcdd3a31baed5e5fc7
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-04-09 10:29:25 +0200

    updated scope validator
    Former-commit-id: c8f28a26730c35e6691bb1f48dae0bd28eaa969a

--- app/validators/scope_validator.rb
+++ app/validators/scope_validator.rb
@@ -1,5 +1,8 @@
 class ScopeValidator < ActiveModel::EachValidator
-  SCOPES             = %w(cargo_info_level dangerous_goods detailed_billing has_customs has_insurance incoterm_info_level modes_of_transport terms ) 
+  SCOPES = %w(
+    cargo_info_level dangerous_goods detailed_billing has_customs has_insurance
+    incoterm_info_level modes_of_transport terms
+  ) 
   LOAD_TYPES         = %w(cargo_item container)
   MODES_OF_TRANSPORT = %w(ocean rail air)
 
@@ -8,19 +11,25 @@ class ScopeValidator < ActiveModel::EachValidator
     @attribute = attribute
     @value     = value
 
-    add_error('must be a Hash') unless value.is_a?(Hash)
-
+    unless value.is_a?(Hash)
+      add_error 'must be a Hash'
+      return
+    end
+    
     value.deep_stringify_keys!
     
     unless value.deep_values.all? { |value| is_a_boolean?(value) || value.is_a?(String) }
-      add_error('last level values must be Boolean or String')
+      add_error 'last level values must be Boolean or String'
     end
 
-  	add_error("must have the following scopes: #{SCOPES.log_format}") unless value.keys.sort == SCOPES
+    missing_scopes = SCOPES - value.keys
+  	unless missing_scopes.empty?
+      add_error "is missing the following keys: #{missing_scopes.log_format}"
+    end
 
   	MODES_OF_TRANSPORT.each do |mode_of_transport|
   		unless has_mode_of_transport?(mode_of_transport)
-  			add_error("must have '#{mode_of_transport}' mode of transport scope")
+  			add_error "must have '#{mode_of_transport}' mode of transport"
   		end
   	end 
   end
