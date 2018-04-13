7902fc5f5d316c7ae8554570d50c389c02ef1d99
Dejan Toteff <deyan8284@gmail.com>
2018-04-13 16:36:08 +0300

    add snapshot test in button
    Former-commit-id: 7177ebdf608d447f5d3ef510a2b1021584fb8e2b

--- client/app/components/Button/Button.spec.jsx
+++ client/app/components/Button/Button.spec.jsx
@@ -1,23 +1,25 @@
 import * as React from 'react'
-import { mount } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 
 import Button from './Button'
 
+const propsBase = {
+  text: 'FOO_TEXT'
+}
+
 test('it mounts only button HTML element', () => {
-  const props = {
-    text: 'foo'
-  }
-  const wrapper = mount(<Button {...props} />)
+  const wrapper = mount(<Button {...propsBase} />)
 
   expect(wrapper.find('button')).toHaveLength(1)
   expect(wrapper.find('div')).toHaveLength(0)
 })
 
 test('it renders with spaces around props.text', () => {
-  const props = {
-    text: 'foo'
-  }
-  const wrapper = mount(<Button {...props} />)
+  const wrapper = mount(<Button {...propsBase} />)
 
-  expect(wrapper.text()).toEqual(` ${props.text} `)
+  expect(wrapper.text()).toEqual(` ${propsBase.text} `)
+})
+
+test('shallow render', () => {
+  expect(shallow(<Button {...propsBase} />)).toMatchSnapshot()
 })
