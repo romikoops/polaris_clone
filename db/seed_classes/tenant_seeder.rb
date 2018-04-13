268d27e89e7cfd51569c18fbc11b52c7c9645c59
DaddyWozBucks <wkbeamish@gmail.com>
2018-04-13 16:34:51 +0200

    hartrodt and saco added
    Former-commit-id: cbd1d6ed763114fabc33eee416c56d306914e8e3

--- db/seed_classes/tenant_seeder.rb
+++ db/seed_classes/tenant_seeder.rb
@@ -690,6 +690,164 @@ class TenantSeeder
       other_data: {
         cargo_item_types: :all
       }
+    },
+    {
+      theme: {
+        colors: {
+          primary: "#DB0025",
+          secondary: "#008ACB",
+          brightPrimary: "#e0708c",
+          brightSecondary: "#4368b7"
+        },
+        logoLarge: "https://assets.itsmycargo.com/assets/images/logos/hartrodt_logo_wide.png",
+        logoSmall: "https://assets.itsmycargo.com/assets/images/logos/hartrodt_logo_small.png",
+        background: "https://assets.itsmycargo.com/assets/backgrounds/bg_nordic_consolidators.jpg"
+      },
+      addresses: {
+        main:"Hoegerdamm 35, 20097 Hamburg"
+      },
+      phones:{
+        main:"+49 40 23 90-0",
+        support: "+49 172 4203 1020"
+      },
+      emails: {
+        sales: "sales@hartrodt.com",
+        support: " ah_ham@hartrodt.com"
+      },
+      subdomain: "hartrodt",
+      name: "a.hartrodt",
+      currency: 'USD',
+      scope: {
+        modes_of_transport: {
+          ocean: {
+            container: true,
+            cargo_item: true
+          },
+          rail: {
+            container: true,
+            cargo_item: true
+          },
+          air: {
+            container: false,
+            cargo_item: false
+          }
+        },
+        dangerous_goods: false,
+        detailed_billing: false,
+        incoterm_info_level: 'text',
+        cargo_info_level: 'text',
+        has_insurance: true,
+        has_customs: true,
+        terms: [
+          "You verify that all the information provided above is true",
+          "You agree to the presented terms and conditions.",
+          "a.hartrodt is to discuss the validity of the presented prices with the product owners."
+
+        ],
+        carriage_options: {
+          on_carriage: {
+            import: 'mandatory',
+            export: 'optional'
+          },
+          pre_carriage: {
+            import: 'optional',
+            export: 'mandatory'      
+          }
+        }
+      },
+      # The following data is not a attribute of the Tenant model
+      # only being used for seeding purposes
+      other_data: {
+        cargo_item_types: [
+          "Pallet",
+          "Carton",
+          "Crate",
+          "Bottle",
+          "Stack",
+          "Drum",
+          "Skid",
+          "Barrel"
+        ]
+      }
+    },
+    {
+      theme: {
+        colors: {
+          primary: "#D5006A",
+          secondary: "#1C2F5D",
+          brightPrimary: "#D5009F",
+          brightSecondary: "#4984B4"
+        },
+        logoLarge: "https://assets.itsmycargo.com/assets/images/logos/saco_logo.png",
+        logoSmall: "https://assets.itsmycargo.com/assets/images/logos/saco_logo.png",
+        background: "https://assets.itsmycargo.com/assets/backgrounds/bg_nordic_consolidators.jpg"
+      },
+      addresses: {
+        main:"Wollkämmereistraße 1, 21107 Hamburg"
+      },
+      phones:{
+        main:"+49 40 311706-0",
+        support: "+49 173 4203 1020"
+      },
+      emails: {
+        sales: "sales@saco.de",
+        support: "support@saco.de"
+      },
+      subdomain: "saco",
+      name: "SACO Shipping GmbH",
+      currency: 'USD',
+      scope: {
+        modes_of_transport: {
+          ocean: {
+            container: true,
+            cargo_item: false
+          },
+          rail: {
+            container: false,
+            cargo_item: false
+          },
+          air: {
+            container: false,
+            cargo_item: false
+          }
+        },
+        dangerous_goods: false,
+        detailed_billing: false,
+        incoterm_info_level: 'text',
+        cargo_info_level: 'text',
+        has_insurance: true,
+        has_customs: true,
+        terms: [
+          "You verify that all the information provided above is true",
+          "You agree to the presented terms and conditions.",
+          "Saco Shipping is to discuss the validity of the presented prices with the product owners."
+
+        ],
+        carriage_options: {
+          on_carriage: {
+            import: 'mandatory',
+            export: 'optional'
+          },
+          pre_carriage: {
+            import: 'optional',
+            export: 'mandatory'      
+          }
+        }
+      },
+      # The following data is not a attribute of the Tenant model
+      # only being used for seeding purposes
+      other_data: {
+        cargo_item_types: [
+          "Pallet",
+          "Carton",
+          "Crate",
+          "Bottle",
+          "Stack",
+          "Drum",
+          "Skid",
+          "Barrel"
+        ]
+      }
     }
   ]
 
