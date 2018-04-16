55086ed63178c24a6a5fae22325d3b373fb152fb
vnnnlc <vnnnlc@gmail.com>
2018-04-16 11:35:05 +0200

    dynamic pricing progress
    Former-commit-id: 240f17e63e74a0ba16be3c5179006a77b12cfeb2

--- client/app/components/Admin/CardPricingIndex/SubComponents/CardRoutesPricing.jsx
+++ client/app/components/Admin/CardPricingIndex/SubComponents/CardRoutesPricing.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import styles from '../Card.scss'
+import AdminItineraryRow from './'
+import { v4 } from 'node-uuid'
+import PropTypes from 'prop-types'
 
 class CardRoutePricing extends Component {
   constructor (props) {
@@ -9,16 +12,26 @@ class CardRoutePricing extends Component {
 
     // clients: how many clients i have (active) in total for each route?
     // *OPT* fees: how many different fees?
+    this.selectItinerary = this.selectItinerary.bind(this)
   }
+
+  selectItinerary () {
+    const { itinerary, handleClick } = this.props
+    handleClick(itinerary)
+  }
+
   render () {
+    const {
+      itinerary
+    } = this.props
     return (
       // .card-route-pricing --> will be a flexbox
       // .top-routes --> border-bottom
       // TODO: calc number of clients and fees
       <div className="card-route-pricing">
         <div className="top-routes">
-          <div className="">
-            <p>From: <strong><span> Stockholm </span></strong></p>
+          <div>
+            <p>From: <strong><span> {itinerary.name} </span></strong></p>
             <p>To: <strong><span> Guthenberg </span></strong></p>
           </div>
           <i>icon</i>
@@ -30,6 +43,11 @@ class CardRoutePricing extends Component {
       </div>
       )
   }
+}
+
+CardRoutePricing.propTypes = {
+  itinerary: PropTypes.objectOf(PropTypes.any).isRequired,
+  handleClick: PropTypes.func.isRequired,
 }
 
 export default CardRoutePricing
