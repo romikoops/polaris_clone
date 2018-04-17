a9ce490f4cf8efc15b3a801aae54d0d218f8ff6b
DaddyWozBucks <wkbeamish@gmail.com>
2018-04-17 09:28:17 +0200

    tp seed
    Former-commit-id: 602a72924fecd3e601fa08ebe4488316a6c56975

--- db/custom_seeds/trucking_pricing.rb
+++ db/custom_seeds/trucking_pricing.rb
@@ -1,3 +1,5 @@
 require "#{Rails.root}/db/seed_classes/trucking_pricing_seeder.rb"
 
-TruckingPricingSeeder.exec(subdomain: 'demo')
+TruckingPricingSeeder.exec(subdomain: 'greencarrier')
+TruckingPricingSeeder.exec(subdomain: 'hartrodt')
+TruckingPricingSeeder.exec(subdomain: 'saco')
