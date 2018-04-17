19a0010c3dd241ee29acbfa8883a88bf9f46df34
Dejan Toteff <deyan8284@gmail.com>
2018-04-17 19:13:18 +0300

    switch to snapshot test in container.details
    Former-commit-id: b5e0490bcd3ffe3b4addb63e12e0210484dc9072

--- client/app/components/ContainerDetails/ContainerDetails.spec.jsx
+++ client/app/components/ContainerDetails/ContainerDetails.spec.jsx
@@ -1,13 +1,11 @@
 import * as React from 'react'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
+import { theme } from '../../mocks'
 
-jest.mock('../HsCodes/HsCodeViewer', () => {
+jest.mock('../HsCodes/HsCodeViewer', () => ({
   // eslint-disable-next-line react/prop-types
-  const HsCodeViewer = ({ children }) => <div>{children}</div>
-
-  return { HsCodeViewer }
-})
-
+  HsCodeViewer: ({ children }) => <div>{children}</div>
+}))
 // eslint-disable-next-line
 import ContainerDetails from './ContainerDetails'
 
@@ -19,47 +17,18 @@ const propsBase = {
   },
   index: 3,
   hsCodes: [],
-  theme: null,
+  theme,
   viewHSCodes: false
 }
 
-let wrapper
-
-const createWrapper = propsInput => mount(<ContainerDetails {...propsInput} />)
-
-beforeEach(() => {
-  wrapper = createWrapper(propsBase)
+test('shallow render', () => {
+  expect(shallow(<ContainerDetails {...propsBase} />)).toMatchSnapshot()
 })
 
-test('props.index', () => {
-  const selector = 'div[className="flex-100 layout-row"]'
-  const index = wrapper.find(selector).first().text()
-
-  expect(index.includes(propsBase.index + 1)).toBeTruthy()
-})
-
-test('props.item.payload_in_kg', () => {
-  const payload = wrapper.find('.layout-align-space-between').at(0).text()
-
-  expect(payload.includes(propsBase.item.payload_in_kg)).toBeTruthy()
-})
-
-test('props.item.quantity', () => {
-  const quantity = wrapper.find('.layout-align-space-between').at(2).text()
-
-  expect(quantity.includes(propsBase.item.quantity)).toBeTruthy()
-})
-
-test('props.viewHSCodes', () => {
-  const withCodes = createWrapper({
+test('props.viewHSCodes is true', () => {
+  const props = {
     ...propsBase,
     viewHSCodes: true
-  })
-
-  const selector = 'i.fa-eye'
-  const iconDefault = wrapper.find(selector)
-  const icon = withCodes.find(selector)
-
-  expect(iconDefault).toHaveLength(0)
-  expect(icon).toHaveLength(1)
+  }
+  expect(shallow(<ContainerDetails {...props} />)).toMatchSnapshot()
 })
