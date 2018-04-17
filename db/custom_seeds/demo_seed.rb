4b6aeaa84e48f82c2b2580eb921c21fcdc8049fe
DaddyWozBucks <wkbeamish@gmail.com>
2018-04-17 08:38:08 +0200

    deploy for dev v3
    Former-commit-id: 3467bf1b3469ac51aee90eae5713848f7251a31d

--- db/custom_seeds/demo_seed.rb
+++ db/custom_seeds/demo_seed.rb
@@ -4,6 +4,135 @@ include MongoTools
 ['hartrodt'].each do |sub|
 # # Tenant.all.each do |tenant|
   tenant = Tenant.find_by_subdomain(sub)
+  admin = tenant.users.new(
+    role: Role.find_by_name('admin'),
+
+    company_name: tenant.name,
+    first_name: "Admin",
+    last_name: "Admin",
+    phone: "123456789",
+
+    email: "admin@#{tenant.subdomain}.com",
+    password: "demo123456789",
+    password_confirmation: "demo123456789",
+
+    confirmed_at: DateTime.new(2017, 1, 20)
+  )
+  # admin.skip_confirmation!
+  admin.save!
+  sub_admin = tenant.users.new(
+    role: Role.find_by_name('sub_admin'),
+
+    company_name: tenant.name,
+    first_name: "Sub",
+    last_name: "Admin",
+    phone: "123456789",
+
+    email: "subadmin@#{tenant.subdomain}.com",
+    password: "demo123456789",
+    password_confirmation: "demo123456789",
+
+    confirmed_at: DateTime.new(2017, 1, 20)
+  )
+  sub_admin.save!
+  tld = tenant.web && tenant.web["tld"] ? tenant.web["tld"] : 'com'
+  shipper = tenant.users.new(
+    role: Role.find_by_name('shipper'),
+
+    company_name: "Example Shipper Company",
+    first_name: "John",
+    last_name: "Smith",
+    phone: "123456789",
+
+    email: "demo@#{tenant.subdomain}.#{tld}",
+    password: "demo123456789",
+    password_confirmation: "demo123456789",
+
+    confirmed_at: DateTime.new(2017, 1, 20)
+  )
+  # shipper.skip_confirmation!
+  shipper.save!
+  # Create dummy locations for shipper
+  dummy_locations = [
+    {
+      street: "Kehrwieder",
+      street_number: "2",
+      zip_code: "20457",
+      city: "Hamburg",
+      country:"Germany"
+    },
+    {
+      street: "Carer del Cid",
+      street_number: "13",
+      zip_code: "08001",
+      city: "Barcelona",
+      country:"Spain"
+    },
+    {
+      street: "College Rd",
+      street_number: "1",
+      zip_code: "PO1 3LX",
+      city: "Portsmouth",
+      country:"United Kingdom"
+    },
+    {
+      street: "Tuna St",
+      street_number: "64",
+      zip_code: "90731",
+      city: "San Pedro",
+      country:"USA"
+    }
+  ]
+
+  dummy_locations.each do |l|
+    loc = Location.create_and_geocode(l)
+    shipper.locations << loc
+  end
+
+  # Create dummy contacts for shipper address book
+  dummy_contacts = [
+    {
+      company_name: "Example Shipper Company",
+      first_name: "John",
+      last_name: "Smith",
+      phone: "123456789",
+      email: "demo@#{tenant.subdomain}.com",
+    },
+    {
+      company_name: "Another Example Shipper Company",
+      first_name: "Jane",
+      last_name: "Doe",
+      phone: "123456789",
+      email: "jane@doe.com"
+    },
+    {
+      company_name: "Yet Another Example Shipper Company",
+      first_name: "Javier",
+      last_name: "Garcia",
+      phone: "0034123456789",
+      email: "javi@shipping.com"
+    },
+    {
+      company_name: "Forwarder Company",
+      first_name: "Gertrude",
+      last_name: "Hummels",
+      phone: "0049123456789",
+      email: "gerti@fwd.com"
+    },
+    {
+      company_name: "Another Forwarder Company",
+      first_name: "Jerry",
+      last_name: "Lin",
+      phone: "001123456789",
+      email: "jerry@fwder2.com"
+    }
+  ]
+
+  dummy_contacts.each_with_index do |contact, i|
+    loc = Location.find_or_create_by(dummy_locations[i])
+    contact[:location_id] = loc.id
+    shipper.contacts.create(contact)
+  end
   shipper = tenant.users.where(role_id: 2).first
 #  	tenant.itineraries.destroy_all
 # 			tenant.stops.destroy_all
