992246154d69f1b7f9979d3aa7ab327dee7f5870
Dejan Toteff <deyan8284@gmail.com>
2018-04-19 15:24:42 +0300

    add file.uploader basic snapshot test
    Former-commit-id: 118735fe2d0cd0903cd1094a3e790cc6a7f98c65

---
+++ client/app/components/FileUploader/FileUploader.spec.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { theme, identity } from '../../mocks'
+
+jest.mock('isomorphic-fetch', () =>
+  () => Promise.resolve({ data: [] }))
+jest.mock('node-uuid', () => ({
+  v4: () => 'RANDOM_KEY'
+}))
+jest.mock('react-router', () => ({
+  // eslint-disable-next-line react/prop-types
+  Link: ({ props }) => <a {...props}>link</a>
+}))
+jest.mock('react-truncate', () =>
+  // eslint-disable-next-line react/prop-types
+  ({ children }) => <span>{children}</span>)
+jest.mock('../RoundButton/RoundButton', () => ({
+  // eslint-disable-next-line react/prop-types
+  RoundButton: ({ props }) => <button {...props}>click</button>
+}))
+jest.mock('react-tooltip', () =>
+  // eslint-disable-next-line react/prop-types
+  ({ children }) => <div>{children}</div>)
+jest.mock('../../helpers', () => ({
+  authHeader: x => x
+}))
+jest.mock('../../constants', () => {
+  const BASE_URL = 'BASE_URL'
+
+  return { BASE_URL }
+})
+// eslint-disable-next-line
+import FileUploader from './FileUploader'
+
+const propsBase = {
+  url: 'FOO_URL',
+  type: 'FOO_TYPE',
+  dispatchFn: identity,
+  uploadFn: identity,
+  tooltip: 'FOO_TOOLTIP',
+  theme
+}
+
+test('shallow render', () => {
+  expect(shallow(<FileUploader {...propsBase} />)).toMatchSnapshot()
+})
