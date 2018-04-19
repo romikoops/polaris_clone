9df623fc25dffeb63c34288a1e77cafd02cc9bcd
Dejan Toteff <deyan8284@gmail.com>
2018-04-19 10:03:21 +0300

    add documents.downloader snapshot test
    Former-commit-id: 4196af4b5b668d087e93eab810710df54631a13e

---
+++ client/app/components/Documents/Downloader/index.spec.jsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { theme, identity } from '../../../mocks'
+
+jest.mock('react-redux', () => ({
+  connect: (x, y) => Component => Component
+}))
+jest.mock('../../../helpers', () => ({
+  authHeader: x => x,
+  gradientTextGenerator: x => x
+}))
+jest.mock('node-uuid', () => ({
+  v4: () => 'RANDOM_KEY'
+}))
+jest.mock('../../../actions', () => ({
+  documentActions: x => x
+}))
+jest.mock('node-uuid', () => ({
+  v4: () => 'RANDOM_KEY'
+}))
+jest.mock('isomorphic-fetch', () =>
+  () => Promise.resolve({ data: [] }))
+jest.mock('react-router', () => ({
+  // eslint-disable-next-line react/prop-types
+  Link: ({ props }) => <a {...props}>link</a>
+}))
+jest.mock('../../RoundButton/RoundButton', () => ({
+  // eslint-disable-next-line react/prop-types
+  RoundButton: ({ props }) => <button {...props}>click</button>
+}))
+
+// eslint-disable-next-line
+import DocumentsDownloader from './'
+
+const propsBase = {
+  theme,
+  tooltip: 'FOO_TOOLTIP',
+  documentDispatch: identity,
+  downloadUrls: {},
+  target: 'FOO_TARGET',
+  loading: false,
+  options: {}
+}
+
+test('shallow render', () => {
+  expect(shallow(<DocumentsDownloader {...propsBase} />)).toMatchSnapshot()
+})
+
+test('props.loading is false, state.requested is true', () => {
+  const wrapper = shallow(<DocumentsDownloader {...propsBase} />)
+  wrapper.setState({ requested: true })
+
+  expect(wrapper).toMatchSnapshot()
+})
+
+test('props.loading is true, state.requested is true', () => {
+  const props = {
+    ...propsBase,
+    loading: true
+  }
+  const wrapper = shallow(<DocumentsDownloader {...props} />)
+  wrapper.setState({ requested: true })
+
+  expect(wrapper).toMatchSnapshot()
+})
