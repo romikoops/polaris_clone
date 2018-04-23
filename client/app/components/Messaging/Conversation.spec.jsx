b32023dea1c23029c52824841c0b2841ec92d615
Dejan Toteff <deyan8284@gmail.com>
2018-04-23 14:16:40 +0300

    add conversation test
    Former-commit-id: a7a9b4c2643130828052e1f99ff6145bf099b858

---
+++ client/app/components/Messaging/Conversation.spec.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { identity, shipment, user, tenant, client, theme } from '../../mocks'
+
+jest.mock('./index.js', () => ({
+  // eslint-disable-next-line react/prop-types
+  Message: ({ children }) => <div id="message">{children}</div>,
+  // eslint-disable-next-line react/prop-types
+  MessageShipmentData: ({ children }) => <div id="message.shipment.data">{children}</div>
+}))
+jest.mock('../../containers/RegistrationPage/RegistrationPage', () => ({
+  // eslint-disable-next-line react/prop-types
+  RegistrationPage: ({ children }) => <div>{children}</div>
+}))
+jest.mock('react-scroll', () => ({
+  // eslint-disable-next-line react/prop-types
+  Element: ({ children }) => <div id="scroll">{children}</div>,
+  scroller: { scrollTo: x => x }
+}))
+
+jest.mock('node-uuid', () => ({
+  // eslint-disable-next-line react/prop-types
+  v4: () => 'RANDOM_KEY'
+}))
+// eslint-disable-next-line
+import { Conversation } from './Conversation'
+
+const regularUser = {
+  ...user,
+  role_id: 2
+}
+
+const propsBase = {
+  sendMessage: identity,
+  conversation: {
+    messages: ['FOO_MESSAGE', 'BAR_MESSAGE']
+  },
+  theme,
+  shipment,
+  user: regularUser,
+  tenant,
+  clients: [client]
+}
+
+test('shallow render', () => {
+  expect(shallow(<Conversation {...propsBase} />)).toMatchSnapshot()
+})
+
+test('with admin user', () => {
+  const props = {
+    ...propsBase,
+    user: {
+      ...user,
+      role_id: 1
+    }
+  }
+  expect(shallow(<Conversation {...props} />)).toMatchSnapshot()
+})
