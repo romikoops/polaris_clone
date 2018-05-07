52d33ba6267ee36188fb5f3392051bf309ec833a
Dejan Toteff <deyan8284@gmail.com>
2018-05-07 11:07:59 +0300

    test: add booking.summary snapshot
    Former-commit-id: 2f5cc098bf6fb8cccad4ef009b60e6f59faafeb7

---
+++ client/app/components/BookingSummary/BookingSummary.spec.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { theme } from '../../mocks'
+
+jest.mock('react-redux', () => ({
+  connect: (mapStateToProps, mapDispatchToProps) => Component => Component
+}))
+jest.mock('react-router-dom', () => ({
+  withRouter: x => x
+}))
+jest.mock('node-uuid', () => ({
+  v4: () => 'RANDOM_KEY'
+}))
+// eslint-disable-next-line
+import BookingSummary from './BookingSummary'
+
+const propsBase = {
+  theme,
+  modeOfTransport: 'FOO_MODE_OF_TRANSPORT',
+  totalWeight: 100,
+  totalVolume: 200,
+  cities: {
+    origin: 'FOO_ORIGIN_CITY',
+    destination: 'FOO_DESTINATION_CITY'
+  },
+  hubs: {
+    origin: 'FOO_ORIGIN_HUB',
+    destination: 'FOO_DESTINATION_HUB'
+  },
+  trucking: {
+    on_carriage: { truck_type: 'FOO_ON_CARRIAGE' },
+    pre_carriage: { truck_type: 'FOO_PRE_CARRIAGE' }
+  }
+}
+
+test('shallow render', () => {
+  expect(shallow(<BookingSummary {...propsBase} />)).toMatchSnapshot()
+})
