1357e047b63cbce87136ce945ccbbf0c44f6c2f7
Unknown <vbalint9309@gmail.com>
2018-05-08 11:09:28 +0200

    greybox fix
    Former-commit-id: c427b2b89e0324a6ca34207766a0cf7eef029cbb

--- client/app/components/GreyBox/GreyBox.jsx
+++ client/app/components/GreyBox/GreyBox.jsx
@@ -1,50 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import defaults from '../../styles/default_classes.scss'
 import styles from './GreyBox.scss'
 
 export class GreyBox extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      title: props.title,
-      subtitle: props.subtitle,
       flex: props.flex,
       fullWidth: props.fullWidth,
       padding: props.padding
     }
   }
 
-  // <div className='layout-column flex-100 layout-wrap layout-align-start-center'>
-  //   <span className='title'>{this.state.title}</span><br/>
-  //   <span className='subtitle'>{this.state.subtitle}</span>
-  // </div>
-
   render () {
-    const Component = this.props.component
+    const InnerComponent = this.props.component
 
     return (
       <div className={`layout-row flex-${this.state.flex} layout-wrap layout-align-center-stretch ${styles.greyboxborder}
         ${this.state.padding ? styles.boxpadding : ''}
-        ${this.state.fullWidth ? styles.fullWidth : ''}`}>
-        {Component}
+        ${this.state.fullWidth ? styles.fullWidth : ''}`}
+      >
+        {InnerComponent}
       </div>
     )
   }
 }
 
 GreyBox.propTypes = {
-  // component: PropTypes.instanceOf(Component),
-  title: PropTypes.string,
-  subtitle: PropTypes.string,
+  component: PropTypes.element,
   flex: PropTypes.number,
   fullWidth: PropTypes.bool,
   padding: PropTypes.bool
 }
 
 GreyBox.defaultProps = {
-  title: '',
-  subtitle: '',
+  component: React.createElement('div'),
   flex: 50,
   fullWidth: false,
   padding: true
