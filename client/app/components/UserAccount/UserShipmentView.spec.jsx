fb9f67175c48017d924fee4c35a3848ff511c632
Dejan Toteff <deyan8284@gmail.com>
2018-05-09 15:46:14 +0300

    increase test coverage in user.shipment.view
    Former-commit-id: 49cec2212de2be6c00473724f0af62c4c604d039

--- client/app/components/UserAccount/UserShipmentView.spec.jsx
+++ client/app/components/UserAccount/UserShipmentView.spec.jsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { shallow, mount } from 'enzyme'
-import { theme, identity, shipmentData, tenant, user, match } from '../../mocks'
+import { theme, identity, shipmentData, tenant, user, match, location } from '../../mocks'
+
+/**
+ * ISSUE
+ * static sumCargoFees is not used while it is declared
+ */
 
 jest.mock('react-select', () =>
   // eslint-disable-next-line react/prop-types
@@ -97,4 +102,89 @@ test('props.setNav is called', () => {
 
   createWrapper(props)
   expect(props.setNav).toHaveBeenCalled()
+})
+
+test('props.hubs is false', () => {
+  const props = {
+    ...propsBase,
+    hubs: false
+  }
+
+  expect(shallow(<UserShipmentView {...props} />)).toMatchSnapshot()
+})
+
+test('props.shipmentData.documents is present', () => {
+  const documents = [
+    { id: 0, doc_type: 'foo' },
+    { id: 1, doc_type: 'bar' }
+  ]
+
+  const editedShipmentData = {
+    ...shipmentData,
+    documents
+  }
+  const props = {
+    ...propsBase,
+    shipmentData: editedShipmentData
+  }
+
+  expect(shallow(<UserShipmentView {...props} />)).toMatchSnapshot()
+})
+
+test('props.shipmentData.cargoItems is present', () => {
+  const cargoItems = [
+    { id: 0, cargo_item_type_id: 'foo' },
+    { id: 1, cargo_item_type_id: 'bar' }
+  ]
+  const cargoItemTypes = { foo: 'FOO_CARGO_TYPE', bar: 'BAR_CARGO_TYPE' }
+
+  const editedShipmentData = {
+    ...shipmentData,
+    cargoItems,
+    cargoItemTypes
+  }
+  const props = {
+    ...propsBase,
+    shipmentData: editedShipmentData
+  }
+
+  expect(shallow(<UserShipmentView {...props} />)).toMatchSnapshot()
+})
+
+test('props.shipmentData.contacts is present', () => {
+  const contacts = [
+    {
+      type: 'notifyee',
+      contact: {
+        first_name: 'FOO_FIRST_NAME',
+        last_name: 'FOO_LAST_NAME'
+      }
+    },
+    {
+      type: 'shipper',
+      location,
+      contact: {
+        first_name: 'BAR_FIRST_NAME',
+        last_name: 'BAR_LAST_NAME'
+      }
+    },
+    {
+      type: 'consignee',
+      location,
+      contact: {
+        first_name: 'BAZ_FIRST_NAME',
+        last_name: 'BAZ_LAST_NAME'
+      }
+    }
+  ]
+  const editedShipmentData = {
+    ...shipmentData,
+    contacts
+  }
+  const props = {
+    ...propsBase,
+    shipmentData: editedShipmentData
+  }
+
+  expect(shallow(<UserShipmentView {...props} />)).toMatchSnapshot()
 })
