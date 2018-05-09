d14c4d08f2e52648087fc8e553e361738d533aa2
Dejan Toteff <deyan8284@gmail.com>
2018-05-09 17:53:50 +0300

    add user.shipments snapshot test
    Former-commit-id: f25a413cbe32dd1b9459fd8768ca13eb00367a45

---
+++ client/app/components/UserAccount/UserShipments.spec.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { theme, identity, shipments, user } from '../../mocks'
+
+jest.mock('node-uuid', () => ({
+  v4: () => 'RANDOM_KEY'
+}))
+jest.mock('../TextHeading/TextHeading', () => ({
+  // eslint-disable-next-line react/prop-types
+  TextHeading: ({ children }) => <div>{children}</div>
+}))
+// eslint-disable-next-line import/first
+import { UserShipments } from './UserShipments'
+
+const propsBase = {
+  theme,
+  setNav: identity,
+  userDispatch: {
+    getShipment: identity
+  },
+  loading: false,
+  user,
+  hubs: [],
+  shipments
+}
+
+test('shallow render', () => {
+  expect(shallow(<UserShipments {...propsBase} />)).toMatchSnapshot()
+})
