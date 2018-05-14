e9eee47102f45def9049e5d864af834a732b7074
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-05-14 15:38:57 +0200

    updated geometry seed and spec to have multi polygon
    Former-commit-id: f1dccbe7a013af9673c01ad7fc7c4d8091f99539

--- db/custom_seeds/geometries.rb
+++ db/custom_seeds/geometries.rb
@@ -1,28 +1,45 @@
+
+puts "Reading from kml..."
 geometry_hash = Hash.from_xml(File.open('db/dummydata/china.kml'))
 geometries = geometry_hash['kml']['Document']['Folder']['Placemark']
 
-geometries.each do |geo|
-	puts '-' * 50
+puts "Seeding Geometries..."
+total = geometries.size
+completion_percentage = 0
+new_completion_percentage = 0
+puts
+puts "PROGRESS BAR"
+puts "_" * 100
+
+geometries.each_with_index do |geo, i|
+	# puts '-' * 50
   names = geo['ExtendedData']['SchemaData']['SimpleData']
-	puts names.join(' | ')
+	# puts names.join(' | ')
+
   polygons_raw_data = [geo['MultiGeometry']['Polygon']].flatten
-  polygons_serialized_coordinate_pairs = polygons_raw_data.map do |polygon_raw_data|
-    polygon_raw_data['outerBoundaryIs']['LinearRing']['coordinates']
-  end
+    
+  polygons = polygons_raw_data.map do |polygon_raw_data|
+    serialized_coordinate_pairs = polygon_raw_data['outerBoundaryIs']['LinearRing']['coordinates'].split
 
-  serialized_coordinate_pairs = polygons_serialized_coordinate_pairs.join(" ")
-  coordinate_pairs = serialized_coordinate_pairs.split
-  points = coordinate_pairs.map do |serialized_coordinate_pair|
-    RGeo::Cartesian.factory.point(*serialized_coordinate_pair.split(","))
+    points = serialized_coordinate_pairs.map do |serialized_coordinate_pair|
+      RGeo::Cartesian.factory.point(*serialized_coordinate_pair.split(",").map(&:to_f))
+    end
+    line_string = RGeo::Cartesian.factory.line_string(points)
+    
+    RGeo::Cartesian.factory.polygon(line_string)
   end
-  line_string = RGeo::Cartesian.factory.line_string(points)
-  polygon     = RGeo::Cartesian.factory.polygon(line_string)
-
+  multi_polygon = RGeo::Cartesian.factory.multi_polygon(polygons)
 
   name_attributes = names.map.with_index { |name, i| ["name_#{i + 1}", name] }.to_h
 
   geometry = Geometry.find_or_initialize_by(name_attributes)
-  geometry.assign_attributes(data: polygon)
+  geometry.assign_attributes(data: multi_polygon)
 
   puts geometry.errors.full_messages unless geometry.save
+
+  new_completion_percentage = i * 100 / total
+  if new_completion_percentage > completion_percentage
+    completion_percentage = new_completion_percentage
+    print "-"
+  end
 end
