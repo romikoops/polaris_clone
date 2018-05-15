e33823db8741d203289e39391713ec65d772dd4c
DaddyWozBucks <wkbeamish@gmail.com>
2018-05-15 17:38:11 +0200

    no byebug
    Former-commit-id: a93bb05b49a40c51c7f79ca93b00467e16bb8a06

--- app/classes/excel_tools.rb
+++ app/classes/excel_tools.rb
@@ -608,7 +608,7 @@ module ExcelTools
           defaults[mod_key] = {} unless defaults[mod_key]
           min_max_arr = cell.split(" - ")
           if !min_max_arr[1]
-            byebug
+            
           end
           defaults[mod_key][i] = {"min_#{mod_key}": min_max_arr[0].to_d, "max_#{mod_key}": min_max_arr[1].to_d, min_value: nil}.symbolize_keys
         end
