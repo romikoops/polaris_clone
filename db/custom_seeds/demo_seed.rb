205a396ba5da051523da4fb27fa7cbc9439fec0f
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-05-15 16:08:43 +0200

    updated excel tools
    Former-commit-id: 41a0e24c1b09e0e738edddfd295025d0a5ab6a2a

--- db/custom_seeds/demo_seed.rb
+++ db/custom_seeds/demo_seed.rb
@@ -51,16 +51,16 @@ subdomains.each do |sub|
   # req = {"xlsx" => trucking}
   # overwrite_zonal_trucking_rates_by_hub(req, shipper, hub.id)
 
-			# hub = tenant.hubs.find_by_name("Shanghai Port")
-			# trucking = File.open("#{Rails.root}/db/dummydata/new_gc_trucking_shanghai_port.xlsx")
-			# req = {"xlsx" => trucking}
-			# overwrite_zonal_trucking_rates_by_hub(req, shipper, hub.id)
+			hub = tenant.hubs.find_by_name("Shanghai Port")
+			trucking = File.open("#{Rails.root}/db/dummydata/new_gc_trucking_shanghai_port.xlsx")
+			req = {"xlsx" => trucking}
+			overwrite_zonal_trucking_rates_by_hub(req, shipper, hub.id)
 
-			# hub = tenant.hubs.find_by_name("Shanghai Port")
-			# trucking = File.open("#{Rails.root}/db/dummydata/new_gc_trucking_shanghai_port_ftl.xlsx")
-			# req = {"xlsx" => trucking}
-			# overwrite_zonal_trucking_rates_by_hub(req, shipper, hub.id)
-			# awesome_print "City rates done"
+			hub = tenant.hubs.find_by_name("Shanghai Port")
+			trucking = File.open("#{Rails.root}/db/dummydata/new_gc_trucking_shanghai_port_ftl.xlsx")
+			req = {"xlsx" => trucking}
+			overwrite_zonal_trucking_rates_by_hub(req, shipper, hub.id)
+			awesome_print "City rates done"
 			
 			hub = tenant.hubs.find_by_name("Gothenburg Port")
 			trucking = File.open("#{Rails.root}/db/dummydata/new_gc_trucking_gothenburg_port.xlsx")
