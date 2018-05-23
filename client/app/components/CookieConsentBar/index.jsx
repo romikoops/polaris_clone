205ade9c8b15a6a11bb37a2c35fb2716a2f92693
Angelica Vanni <vnnnlc@gmail.com>
2018-05-23 18:14:19 +0200

    cookies decline button with onClick to imc page (no-verify)
    Former-commit-id: 3a77acc771b5770639bcbcb4f2d4166c486e4d65

--- client/app/components/CookieConsentBar/index.jsx
+++ client/app/components/CookieConsentBar/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from '../../prop-types'
 import styles from './CookieConsentBar.scss'
 import ConsentButton from "./ConsentButton"
+import { Modal } from '../Modal/Modal'
 import { moment } from '../../constants'
 
 function handleAccept (user, tenant, loggedIn, authDispatch) {
@@ -28,38 +29,87 @@ function handleAccept (user, tenant, loggedIn, authDispatch) {
   }
 }
 
-export default function CookieConsentBar ({
-  user,
-  theme,
-  tenant,
-  loggedIn,
-  authDispatch
- }) {
-  if (!tenant) return ''
-  const cookieBackground = theme && theme.colors ? theme.colors.secondary : '#aaa'
-  return (
-    <div
-      className={`${styles.cookie_flex} ${user && user.cookie_consent ? styles.hidden : ''}`}
-      style={{ background: cookieBackground, filter: 'grayscale(60%)' }}
-    >
-      <p className={styles.cookie_text}>
-        This website uses cookies to enhance your user experience.
-      </p>
 
-      <ConsentButton
-        theme={theme}
-        handleNext={() => handleAccept(user, tenant, loggedIn, authDispatch)}
-        text="accept"
-        active
+export default class CookieConsentBar extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showModal: false
+    }
+    this.toggleShowModal = this.toggleShowModal.bind(this)
+    this.handleDecline = this.handleDecline.bind(this)
+  }
+  handleDecline () {
+    this.setState({ showModal: true })
+  }
+
+  toggleShowModal () {
+    this.setState((prevState) => { return { showModal: !prevState.showModal } })
+  }
+
+  render () {
+    const {
+      user,
+      theme,
+      tenant,
+      loggedIn,
+      authDispatch
+    } = this.props
+
+    const modal = (
+      <Modal
+        component={
+          <div className={styles.cookie_modal} >
+            Are you sure you want to decline the usage of cookies? <br />
+            If so, unfortunately you may not continue using the website and this tab will be closed.
+            <ConsentButton
+              theme={theme}
+              handleNext={() => handleAccept(user, tenant, loggedIn, authDispatch)}
+              text="accept"
+              active
+            />
+            <ConsentButton
+              theme={theme}
+              handleNext={() => { window.location.replace('https://www.itsmycargo.com/') }}
+              text="decline"
+              active
+            />
+          </div>
+        }
+        verticalPadding="30px"
+        horizontalPadding="40px"
+        parentToggle={this.toggleShowModal}
       />
-      <ConsentButton
-        theme={theme}
-        handleNext={() => this.hello()}
-        text="decline"
-        active
-      />
-    </div>
-  )
+    )
+
+    if (!tenant) return ''
+
+    const cookieBackground = theme && theme.colors ? theme.colors.secondary : '#aaa'
+    return (
+      <div
+        className={`${styles.cookie_flex} ${user && user.cookie_consent ? styles.hidden : ''}`}
+        style={{ background: cookieBackground, filter: 'grayscale(60%)' }}
+      >
+        { this.state.showModal && modal}
+        <p className={styles.cookie_text}>
+          This website uses cookies to enhance your user experience.
+        </p>
+
+        <ConsentButton
+          theme={theme}
+          handleNext={() => handleAccept(user, tenant, loggedIn, authDispatch)}
+          text="accept"
+          active
+        />
+        <ConsentButton
+          theme={theme}
+          handleNext={this.handleDecline}
+          text="decline"
+          active
+        />
+      </div>
+    )
+  }
 }
 
 CookieConsentBar.propTypes = {
