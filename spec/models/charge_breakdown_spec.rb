b7a9a0fcd1f3e24ee9f78dfaa07758c0b286dece
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-05-29 09:55:04 +0200

    added spec to charge breakdown
    Former-commit-id: c83c961c7c7ebba2bdb0870194273fcc544e51d6

--- spec/models/charge_breakdown_spec.rb
+++ spec/models/charge_breakdown_spec.rb
@@ -1,5 +1,38 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe ChargeBreakdown, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe ChargeBreakdown, type: :model do
+  context 'instance methods' do
+    let(:charge_breakdown)          { create(:charge_breakdown) }
+    let(:charge_category)           { create(:charge_category) }
+    let(:children_charge_category)  { create(:charge_category, code: 'export', name: 'Export') }
+    let(:price)                     { create(:price) }
+
+    let!(:charge) {
+      create(:charge,
+        charge_breakdown: charge_breakdown,
+        charge_category: charge_category,
+        children_charge_category: children_charge_category,
+        price: price
+      )
+    }
+
+    context '.charges.from_category' do
+      it 'returns a collection of charges' do
+        result = charge_breakdown.charges.from_category(charge_category.code)
+        expect(result).not_to be_empty
+        expect(result).to all be_a Charge
+      end
+    end
+
+    context '.charge_categories.detail' do
+      it 'returns a collection of charge categories' do
+        result = charge_breakdown.charge_categories.detail(0)
+        expect(result).not_to be_empty
+        expect(result).to all be_a ChargeCategory
+      end
+    end
+  end
 end
