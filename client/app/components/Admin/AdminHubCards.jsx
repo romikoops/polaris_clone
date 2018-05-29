a932459580465ed53a041547c11beec69f013d0a
Unknown <vbalint9309@gmail.com>
2018-05-29 19:50:49 +0200

    hub cards
    Former-commit-id: b2373d7e26b6dcadc9cd0cbe36deb2e86dd1617d

---
+++ client/app/components/Admin/AdminHubCards.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { AdminHubCard as AHubCard } from './AdminHubCard'
+import styles from './AdminHubCards.scss'
+
+function listHubs (hubs) {
+  return Object.keys(hubs).map((hub) => {
+    const HubCard = (
+      <div className={`flex-22 ${styles.hub}`}>
+        <AHubCard
+          hub={hubs[hub]}
+        />
+      </div>
+    )
+
+    return HubCard
+  })
+}
+
+export class AdminHubCards extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render () {
+    const {
+      hubs
+    } = this.props
+
+    return (
+      <div className="layout-wrap layout-row flex-100 layout-align-space-between-stretch">
+        <div className={`layout-padding flex-100 layout-align-start-center ${styles.greyBg}`}>
+          <span><b>Hubs</b></span>
+        </div>
+        {listHubs(hubs)}
+      </div>
+    )
+  }
+}
+
+AdminHubCards.propTypes = {
+  hubs: PropTypes.objectOf(PropTypes.hub)
+}
+
+AdminHubCards.defaultProps = {
+  hubs: {}
+}
+
+export default AdminHubCards
