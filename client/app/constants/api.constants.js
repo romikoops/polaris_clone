746852986873e579e33e48c961d96a848e0c8925
Warwick Beamish <wkbeamish@gmail.com>
2018-06-04 18:05:30 +0200

    easyshipping seeder fixed
    Former-commit-id: b31ebafe7781bd07cdc59bb7845acd213a3201df

--- client/app/constants/api.constants.js
+++ client/app/constants/api.constants.js
@@ -4,12 +4,12 @@ let url
 
 const subdomainKey = getSubdomain()
 if (process.env.NODE_ENV === 'production') {
-  url = 'https://api2.itsmycargo.com'
-  // url = 'https://devapi.itsmycargo.com'
-} else {
-  url = 'http://localhost:3000'
   // url = 'https://api2.itsmycargo.com'
-  // url = 'https://devapi.itsmycargo.com'
+  url = 'https://devapi.itsmycargo.com'
+} else {
+  // url = 'http://localhost:3000'
+  // url = 'https://api2.itsmycargo.com'
+  url = 'https://devapi.itsmycargo.com'
   // url = 'http://192.168.178.43:3000'
   // url = 'https://api2.itsmycargo.com'
   // url = 'http://imcr-staging.edrmpdsn2j.eu-central-1.elasticbeanstalk.com';
