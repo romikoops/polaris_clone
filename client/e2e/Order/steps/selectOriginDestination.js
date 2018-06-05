4370601558ab201c7f3a1e845b359218749f2888
Dejan Toteff <deyan8284@gmail.com>
2018-06-05 17:15:58 +0300

    e2e: fix selection of fright origin
    Former-commit-id: 14dc071738cb8913c1384226a56c47ff79122340

--- client/e2e/Order/steps/selectOriginDestination.js
+++ client/e2e/Order/steps/selectOriginDestination.js
@@ -1,10 +1,29 @@
+import { delay } from '../../_modules/delay'
+
+const LONG_DELAY = 10000
+const SELECTOR = '#origin'
+const ORIGIN = 'Gothenburg'
+
 export default async function selectOriginDestination (puppeteer) {
   const {
+    $,
+    page,
     focus,
     selectWithTab
   } = puppeteer
 
   await focus('body')
   await selectWithTab(3)
-  await selectWithTab(7)
+
+  const coordinates = await $(
+    SELECTOR,
+    el => JSON.stringify(el.getBoundingClientRect())
+  )
+  const { x, y } = JSON.parse(coordinates)
+
+  await page.keyboard.type(ORIGIN, { delay: 50 })
+
+  await page.mouse.click(x + 10, y + 40)
+  await delay(LONG_DELAY)
+  await selectWithTab(12, undefined, true)
 }
