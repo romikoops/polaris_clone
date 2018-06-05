bef35dd8a38004205560c3f58aac64457359dca5
Dejan Toteff <deyan8284@gmail.com>
2018-06-05 17:32:35 +0300

    add testRegex in jest config
    Former-commit-id: 12d76ecfc4161529fa9fc77cec8caffc01a084a3

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // if true stops test after first failure
   bail: true,
+  testRegex: '.spec.jsx$',
   verbose: true,
   setupFiles: ['./jest.init.js'],
   setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
