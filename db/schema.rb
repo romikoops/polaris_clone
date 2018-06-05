6e012fa88eccedd9572e6a5a6ddd5ffe2358448e
Henry Perschk <henry@Pattyss-iMac.fritz.box>
2018-06-05 11:26:25 +0200

    migrate -> refresh schema verison
    Former-commit-id: d26e408aaad630d42558ded1d7f4534be8745ca5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525104346) do
+ActiveRecord::Schema.define(version: 20180601170411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -475,10 +475,10 @@ ActiveRecord::Schema.define(version: 20180525104346) do
     t.jsonb "customs"
     t.bigint "transport_category_id"
     t.integer "incoterm_id"
-    t.datetime "closing_date"
-    t.string "incoterm_text"
     t.integer "origin_nexus_id"
     t.integer "destination_nexus_id"
+    t.datetime "closing_date"
+    t.string "incoterm_text"
     t.datetime "planned_origin_drop_off_date"
     t.index ["transport_category_id"], name: "index_shipments_on_transport_category_id"
   end
