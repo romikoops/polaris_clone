fdb635a37da7bdfebaf4d56e5562687906d971e0
Dejan Toteff <deyan8284@gmail.com>
2018-06-06 12:48:24 +0300

    add collect.coverage.from in jest config
    Former-commit-id: 6e9fd6039d1acca4713d2b889d28f0d485a1fa6e

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // if true stops test after first failure
   bail: true,
   testRegex: '.spec.jsx$',
+  collectCoverageFrom: ['app/components/**/*.(jsx|js)'],
   verbose: true,
   setupFiles: ['./jest.init.js'],
   setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
