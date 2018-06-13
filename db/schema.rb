8583ce3905cde8003f1c24c5fbbca63be4d271ec
Dejan Toteff <deyan8284@gmail.com>
2018-06-13 13:49:14 +0300

    restore booking.confirmation and apply recent changes
    Former-commit-id: c0d7085d1b764edb79b25697cad14b5e80bfd17b

--- db/schema.rb
+++ db/schema.rb
@@ -649,7 +649,6 @@ ActiveRecord::Schema.define(version: 20180612090653) do
     t.string "currency", default: "EUR"
     t.string "vat_number"
     t.boolean "allow_password_change", default: false, null: false
-    t.jsonb "optin_status", default: {}
     t.integer "optin_status_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email"
