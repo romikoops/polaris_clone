fec20b7c1c2e89cbebb30ef9bd053e31f18a8d79
Dejan Toteff <deyan8284@gmail.com>
2018-06-15 13:16:18 +0300

    remove start.md
    Former-commit-id: 64d4391219f0d8e27d6e9b8040441691b38eb1a5

--- client/app/components/ActiveRoutes/ActiveRoutes.spec.jsx
+++ client/app/components/ActiveRoutes/ActiveRoutes.spec.jsx
@@ -1,12 +1,6 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
 import { theme } from '../../mocks'
-
-jest.mock('../RouteHubBox/RouteHubBox', () => ({
-  // eslint-disable-next-line react/prop-types
-  Carousel: ({ children }) => <div>{children}</div>
-}))
-// eslint-disable-next-line
 import { ActiveRoutes } from './ActiveRoutes'
 
 const propsBase = {
