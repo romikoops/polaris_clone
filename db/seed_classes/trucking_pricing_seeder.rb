8598fe08073e5f7152a4f5bcc68a52996ebe262c
Angelica Vanni <vnnnlc@gmail.com>
2018-06-19 18:16:16 +0200

    added trucking pricings for Shanghai Airport
    Former-commit-id: f67232a656539f01fd8f3dc732c8d14b39942493

--- db/seed_classes/trucking_pricing_seeder.rb
+++ db/seed_classes/trucking_pricing_seeder.rb
@@ -22,6 +22,12 @@ class TruckingPricingSeeder
       trucking = File.open("#{Rails.root}/db/dummydata/new_gc_trucking_shanghai_port_ftl.xlsx")
       req = { 'xlsx' => trucking }
       overwrite_zonal_trucking_rates_by_hub(req, shipper, hub.id)
+
+      hub = tenant.hubs.find_by_name('Shanghai Airport')
+      trucking = File.open("#{Rails.root}/db/dummydata/new_gc_trucking_shanghai_port.xlsx")
+      req = { 'xlsx' => trucking }
+      overwrite_zonal_trucking_rates_by_hub(req, shipper, hub.id)
+
       puts 'City rates done'
 
       hub = tenant.hubs.find_by_name('Gothenburg Port')
