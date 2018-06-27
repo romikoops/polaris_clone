bf88e9c7724ed62f5a1ffb5119a665ac92f1396a
Dejan Toteff <deyan8284@gmail.com>
2018-06-28 00:50:07 +0300

    add origin_hub to mocked shipment
    Former-commit-id: 400ccea56ac0979f20eb4b05f0b03400b0d0a509

--- client/app/components/ShipmentCardNew/UserShipmentCard.spec.jsx
+++ client/app/components/ShipmentCardNew/UserShipmentCard.spec.jsx
@@ -4,11 +4,29 @@ import { shipment } from '../../mocks'
 
 import { UserShipmentCard } from './UserShipmentCard'
 
+jest.mock('../../helpers', () => ({
+  gradientTextGenerator: x=>x,
+  gradientGenerator:x=>x,
+  gradientBorderGenerator:x=>x,
+  switchIcon:x=>x
+}))
+jest.mock('uuid', () => {
+  let counter = -1
+  const v4 = () => {
+    counter++
+
+    return `RANDOM_KEY_${counter}`
+  }
+
+  return { v4 }
+})
 jest.mock('moment', () => {
   const format = () => 19
+  const diff = () => 17
 
   return () => ({
-    format
+    format,
+    diff
   })
 })
 
@@ -19,3 +37,4 @@ const propsBase = {
 test('shallow rendering', () => {
   expect(shallow(<UserShipmentCard {...propsBase} />)).toMatchSnapshot()
 })
+
