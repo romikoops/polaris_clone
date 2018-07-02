f8b1d713a2ac0b0161ec9aabad96574f39713041
Obiora-Imah <obiora.imah@gmail.com>
2018-07-02 08:39:24 +0000

    Refactor admin shipmentController
    Former-commit-id: 9697fe5939e98e4d0f655ac360acb98f8afbdda6

--- app/controllers/admin/shipments_controller.rb
+++ app/controllers/admin/shipments_controller.rb
@@ -2,128 +2,50 @@
 
 class Admin::ShipmentsController < ApplicationController
   before_action :require_login_and_role_is_admin
+  before_action :do_for_show, only: :show
   include ShippingTools
   include NotificationTools
 
   def index
-    @documents = {}
-    requested_shipments = Shipment.where(
-      status:    %w[requested requested_by_unconfirmed_account],
-      tenant_id: current_user.tenant_id
-    )
-    @documents["requested_shipments"] = Document.get_documents_for_array(requested_shipments)
-    open_shipments = Shipment.where(
-      status:    %w[in_progress confirmed],
-      tenant_id: current_user.tenant_id
-    )
-    @documents["open_shipments"] = Document.get_documents_for_array(open_shipments)
-    finished_shipments = Shipment.where(status: "finished", tenant_id: current_user.tenant_id)
-    @documents["finished_shipments"] = Document.get_documents_for_array(finished_shipments)
-    @requested_shipments = requested_shipments.map{|shipment| shipment.with_address_options_json}
-    @open_shipments = open_shipments.map{|shipment| shipment.with_address_options_json}
-    @finished_shipments = finished_shipments.map{|shipment| shipment.with_address_options_json}
-    resp = {
-      requested: @requested_shipments,
-      open:      @open_shipments,
-      finished:  @finished_shipments,
-      documents: @documents
-    }
-    response_handler(resp)
+    response_handler({
+      requested: requested_shipments,
+      open:      open_shipments,
+      finished:  finished_shipments,
+      documents: documents
+    })
   end
 
   def show
-    @shipment = Shipment.find(params[:id])
-    @cargo_items = @shipment.cargo_items
-    @containers = @shipment.containers
-    hs_codes = []
-    cargo_item_types = {}
-
-    @cargo_items.each do |ci|
-      if ci&.hs_codes
-        ci.hs_codes.each do |hs|
-          hs_codes << hs
-        end
-      end
-      cargo_item_types[ci.cargo_item_type_id] = CargoItemType.find(ci.cargo_item_type_id)
-    end
-    @containers.each do |cn|
-      next unless cn&.hs_codes
-      cn.hs_codes.each do |hs|
-        hs_codes << hs
-      end
-    end
-    # hsCodes = get_hs_code_hash(hs_codes)
-    @shipment_contacts = @shipment.shipment_contacts
-    @contacts = []
-    @shipment_contacts.each do |sc|
-      @contacts.push(contact: sc.contact, type: sc.contact_type, location: sc.contact.location)
-    end
-    @schedules = @shipment.schedule_set
-    @documents = []
-    @shipment.documents.each do |doc|
-      tmp = doc.as_json
-      tmp["signed_url"] = doc.get_signed_url
-      @documents << tmp
-    end
-    locations = {
-      origin: @shipment.origin_nexus,
-      destination: @shipment.destination_nexus
-    }
-    account_holder = @shipment.user
-    options = {
-      methods: [:selected_offer, :mode_of_transport],
-      include:[ { destination_nexus: {}},{ origin_nexus: {}}, { destination_hub: {}}, { origin_hub: {}} ]
-    }
-    shipment_as_json = @shipment.with_address_options_json
-    resp = {
+    prepare_response
+    response_handler({
       shipment:        shipment_as_json,
       cargoItems:      @cargo_items,
       containers:      @containers,
       aggregatedCargo: @shipment.aggregated_cargo,
-      contacts:        @contacts,
+      contacts:        contacts,
       documents:       @documents,
       schedules:       @schedules,
-      # hsCodes: hsCodes,
       locations:       locations,
       cargoItemTypes:  cargo_item_types,
-      accountHolder:   account_holder
-    }
-    response_handler(resp)
+      accountHolder:   @shipment.user
+    })
   end
 
   def edit_price
-    shipment = Shipment.find(params[:id])
-    shipment.total_price = { value: params[:priceObj]["value"], currency: params[:priceObj]["currency"] }
-    shipment.save!
-    message = {
-      title:       "Shipment Price Change",
-      message:     "Your shipment #{shipment.imc_reference} has an updated price. Your new total is #{params[:priceObj]['currency']} #{params[:priceObj]['value']}. For any issues, please contact your support agent.",
-      shipmentRef: shipment.imc_reference
-    }
-    add_message_to_convo(shipment.user, message, true)
-    response_handler(shipment.with_address_options_json)
+    add_message_to_convo(update_shipment.user, price_message, true)
+    response_handler(update_shipment.with_address_options_json)
   end
 
   def edit_service_price
-    shipment = Shipment.find(params[:id])
-    charge_breakdown = shipment.charge_breakdowns.selected
-	  charge = charge_breakdown.charge(params["charge_category"])
+    @shipment = Shipment.find(params[:id])
     new_price = Price.new(price_params)
-    charge.edited_price = new_price
+    edit_service_charge.edited_price = new_price
 
-    if charge.save
-      unless charge.parent.nil?
-        charge.parent.update_edited_price!
-        charge.parent.save!
-      end
-      response_handler(shipment.as_options_json())
+    if edit_service_charge.save
+      update_charge_parent
+      response_handler(@shipment.as_options_json())
 	  else
-      error = ApplicationError.new(
-        http_code: 400,
-        code:      SecureRandom.uuid,
-        message:   shipments.errors.full_messages.join("\n")
-      )
-      response_handler(error)
+      response_handler(resp_error)
 	  end
   end
 
@@ -132,22 +54,8 @@ class Admin::ShipmentsController < ApplicationController
       methods: %i(selected_offer mode_of_transport),
       include: %i(destination_nexus origin_nexus destination_hub origin_hub)
     }
-
-    shipment = Shipment.find(params[:id])
-    new_etd = DateTime.parse(params[:timeObj]["newEtd"])
-    new_eta = DateTime.parse(params[:timeObj]["newEta"])
-    shipment.planned_eta = new_eta
-    shipment.planned_etd = new_etd
-    shipment.schedule_set[0]["eta"] = new_eta
-    shipment.schedule_set[0]["etd"] = new_etd
-    shipment.save!
-    message = {
-      title:       "Shipment Schedule Updated",
-      message:     "Your shipment #{shipment.imc_reference} has an updated schedule. Your new estimated departure is #{params[:timeObj]['newEtd']}, estimated to arrive at #{params[:timeObj]['newEta']}. For any issues, please contact your support agent.",
-      shipmentRef: shipment.imc_reference
-    }
-    add_message_to_convo(shipment.user, message, true)
-    response_handler(shipment.with_address_options_json)
+    add_message_to_convo(update_schedule_shipment.user, schedule_message, true)
+    response_handler(update_schedule_shipment.with_address_options_json)
   end
 
   def edit
@@ -159,77 +67,233 @@ class Admin::ShipmentsController < ApplicationController
 
   def update
     @shipment = Shipment.find(params[:id])
-    if params[:shipment_action] # This happens when accept or decline buttons are used
-      case params[:shipment_action]
-      when "accept"
-        @shipment.accept!
-        shipper_confirmation_email(@shipment.user, @shipment)
-        message = {
-          title:       "Booking Accepted",
-          message:     "Your booking has been accepted! If you have any further questions or edits to your booking please contact the support department.",
-          shipmentRef: @shipment.imc_reference
-        }
-        add_message_to_convo(@shipment.user, message, true)
-        response_handler(@shipment.with_address_options_json)
-      when "decline"
-        @shipment.decline!
-        message = {
-          title:       "Booking Declined",
-          message:     "Your booking has been declined! This could be due to a number of reasons including cargo size/weight and goods type. For more info contact us through the support channels.",
-          shipmentRef: @shipment.imc_reference
-        }
-        add_message_to_convo(@shipment.user, message, true)
-        response_handler(@shipment.with_address_options_json)
-      when "ignore"
-        @shipment.ignore!
-        response_handler({})
-      when "finished"
-        @shipment.finish!
-        response_handler(@shipment.with_address_options_json)
-      else
-        raise "Unknown action!"
-      end
-    else # This happens when shipment is edited with edit form
-      if @shipment.update(shipment_params)
-        # redirect_to admin_shipments_path
-      else
-        # render 'edit'
-      end
+    if params[:shipment_action]
+      shipment_action
     end
   end
 
   def document_action
     @document = Document.find(params[:id])
     @user = @document.user
-    type = params[:type]
-    text = params[:text]
-    case type
+    decide_document_action
+    tmp = @document.as_json
+    tmp["signed_url"] = @document.get_signed_url
+
+    response_handler(tmp)
+  end
+
+  
+
+  private
+
+  def decide_document_action
+    case params[:type]
     when "approve"
       @document.approved = "approved"
       @document.save!
-      message = {
-        title:       "Document Approved",
-        message:     "Your document #{@document.text} was approved",
-        shipmentRef: @document.shipment.imc_reference
-      }
+      approved_document_message
       add_message_to_convo(@user, message, true)
     when "reject"
       @document.approved = "rejected"
       @document.save!
-      message = {
-        title:       "Document Rejected",
-        message:     "Your document #{@document.text} was rejected: #{text}",
-        shipmentRef: @document.shipment.imc_reference
-      }
+      rejected_document_message
       add_message_to_convo(@user, message, true)
     end
-
-    tmp = @document.as_json
-    tmp["signed_url"] = @document.get_signed_url
-    response_handler(tmp)
   end
 
-  private
+  def resp_error
+    ApplicationError.new(
+      http_code: 400,
+      code:      SecureRandom.uuid,
+      message:   @shipments.errors.full_messages.join("\n")
+    )
+  end
+  
+  def update_charge_parent
+    unless edit_service_charge.parent.nil?
+      edit_service_charge.parent.update_edited_price!
+      edit_service_charge.parent.save!
+    end
+  end
+
+  def edit_service_charge_breakdown
+    @shipment.charge_breakdowns.selected
+  end
+
+  def edit_service_charge
+    edit_service_charge_breakdown.charge(params["charge_category"])
+  end
+
+  def shipment_action
+    case params[:shipment_action]
+    when "accept"
+      @shipment.accept!
+      shipper_confirmation_email(@shipment.user, @shipment)
+      add_message_to_convo(@shipment.user, booking_accepted_message, true)
+      response_handler(@shipment.with_address_options_json)
+    when "decline"
+      @shipment.decline!
+      add_message_to_convo(@shipment.user, booking_declined_message, true)
+      response_handler(@shipment.with_address_options_json)
+    when "ignore"
+      @shipment.ignore!
+      response_handler({})
+    when "finished"
+      @shipment.finish!
+      response_handler(@shipment.with_address_options_json)
+    else
+      raise "Unknown action!"
+    end
+  end
+
+  def update_schedule_shipment
+    if @shipment
+      @shipment
+    else
+      shipment = Shipment.find(params[:id])
+      shipment.planned_eta = new_eta
+      shipment.planned_etd = new_etd
+      shipment.schedule_set[0]["eta"] = new_eta
+      shipment.schedule_set[0]["etd"] = new_etd
+      shipment.save!
+      @shipment = shipment
+    end
+  end
+
+  def new_etd
+    DateTime.parse(params[:timeObj]["newEtd"])
+  end
+
+  def new_eta
+    DateTime.parse(params[:timeObj]["newEta"])
+  end
+
+  def update_shipment
+    if @shipment 
+      @shipment
+    else
+      shipment = Shipment.find(params[:id])
+      shipment.total_price = { value: params[:priceObj]["value"], currency: params[:priceObj]["currency"] }
+      shipment.save!
+      @shipment = shipment
+    end
+  end
+
+  def prepare_response
+    add_cargo_item_types
+    add_hs_code
+    populate_contacts
+    populate_documents
+    @schedules = @shipment.schedule_set
+  end
+
+  def shipment_as_json
+    @shipment.with_address_options_json
+  end
+
+  def locations
+    @locations ||= {
+      origin: @shipment.origin_nexus,
+      destination: @shipment.destination_nexus
+    }
+    
+  end
+
+  def options
+    @options ||= {
+      methods: [:selected_offer, :mode_of_transport],
+      include:[ { destination_nexus: {}},
+        { origin_nexus: {}},
+        { destination_hub: {}},
+        { origin_hub: {}} ]
+    }
+  end
+  
+  def populate_documents
+    @documents = []
+    @shipment.documents.each do |doc|
+      tmp = doc.as_json
+      tmp["signed_url"] = doc.get_signed_url
+      @documents << tmp
+    end
+  end
+
+  def do_for_show
+    @shipment = Shipment.find(params[:id])
+    @cargo_items = @shipment.cargo_items
+    @containers = @shipment.containers
+  end
+
+  def populate_contacts
+    @shipment_contacts = @shipment.shipment_contacts
+    @shipment_contacts.each do |sc|
+      contacts.push(contact: sc.contact,
+        type: sc.contact_type,
+        location: sc.contact.location)
+    end
+  end
+
+  def cargo_item_types
+    @cargo_item_types ||= {}
+  end
+
+  def hs_codes
+    @hs_codes ||= []
+  end
+
+  def add_cargo_item_types
+    @cargo_items.each do |ci|
+      if ci&.hs_codes
+        ci.hs_codes.each do |hs|
+          hs_codes << hs
+        end
+      end
+      cargo_item_types[ci.cargo_item_type_id] = CargoItemType.find(ci.cargo_item_type_id)
+    end
+  end
+
+  def add_hs_code
+    @containers.each do |cn|
+      next unless cn&.hs_codes
+      cn.hs_codes.each do |hs|
+        hs_codes << hs
+      end
+    end
+  end
+
+  def contacts
+    @contacts ||= []
+  end
+
+  def tenant_shipment
+     @shipment ||= Shipment.where(tenant_id: current_user.tenant_id)
+  end
+
+  def requested_shipments
+    @requested_shipments ||= tenant_shipment.requested.map do |shipment|
+      shipment.with_address_options_json
+    end
+  end
+
+  def open_shipments
+    @open_shipments ||= tenant_shipment.open.map do |shipment|
+      shipment.with_address_options_json
+    end
+  end
+
+  def finished_shipments
+    @finished_shipments ||= tenant_shipment.finished.map do |shipment|
+      shipment.with_address_options_json
+    end
+  end
+
+  def documents
+    @documents ||= {
+      "requested_shipments" => Document.get_documents_for_array(tenant_shipment.requested),
+      "open_shipments" => Document.get_documents_for_array(tenant_shipment.open),
+      "finished_shipments" => Document.get_documents_for_array(tenant_shipment.finished)
+    }
+  end
 
   def require_login_and_role_is_admin
     unless user_signed_in? && current_user.role.name.include?("admin") && current_user.tenant_id === Tenant.find_by_subdomain(params[:subdomain_id]).id
@@ -244,5 +308,61 @@ class Admin::ShipmentsController < ApplicationController
 
   def price_params
     params.require(:price).permit(:value, :currency)
+  end
+
+  def approved_document_message
+    {
+      title: "Document Approved",
+      message: "Your document #{@document.text} was approved",
+      shipmentRef: @document.shipment.imc_reference
+    }
+  end
+  
+  def rejected_document_message
+    {
+      title: "Document Rejected",
+      message: "Your document #{@document.text} was rejected: #{params[:text]}",
+      shipmentRef: @document.shipment.imc_reference
+    }
+  end
+
+  def price_message
+    {
+      title: "Shipment Price Change",
+      message: "Your shipment #{update_shipment.imc_reference} has an updated price. \
+        Your new total is #{params[:priceObj]['currency']} #{params[:priceObj]['value']}. \
+        For any issues, please contact your support agent.",
+      shipmentRef: update_shipment.imc_reference
+    }
+  end
+
+  def schedule_message
+    {
+      title: "Shipment Schedule Updated",
+      message: "Your shipment #{update_schedule_shipment.imc_reference} has an updated schedule. \
+        Your new estimated departure is #{params[:timeObj]['newEtd']}, estimated to \
+        arrive at #{params[:timeObj]['newEta']}. For any issues, please contact your \
+        support agent.",
+      shipmentRef: update_schedule_shipment.imc_reference
+    }
+  end
+
+  def booking_accepted_message
+    {
+      title: "Booking Accepted",
+      message: "Your booking has been accepted! If you have any further questions or \
+        edits to your booking please contact the support department.",
+      shipmentRef: @shipment.imc_reference
+    }
+  end
+
+  def booking_declined_message
+    {
+      title: "Booking Declined",
+      message: "Your booking has been declined! This could be due to a number of \
+        reasons including cargo size/weight and goods type. For more info contact \
+        us through the support channels.",
+      shipmentRef: @shipment.imc_reference
+    }
   end
 end
