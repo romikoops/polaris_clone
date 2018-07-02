188980bbeb8a40786a7c3feb868e1694a7d79166
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-07-02 17:41:18 +0200

    removed carriage from trucking pricings
    Former-commit-id: 78c73d97d4dd88729e57993ab4ae782e0b72689c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628130604) do
+ActiveRecord::Schema.define(version: 20180702132909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -618,7 +618,6 @@ ActiveRecord::Schema.define(version: 20180628130604) do
     t.integer "tenant_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "carriage"
     t.jsonb "rates"
     t.jsonb "fees"
     t.string "identifier_modifier"
