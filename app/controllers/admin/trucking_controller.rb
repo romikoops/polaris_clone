1ddf049d3601b6eed4a2686a1cc00c9e38bcf2b9
Obiora-Imah <obiora.imah@gmail.com>
2018-07-03 09:24:31 +0000

    Refactor trucking_controller admin
    Former-commit-id: 825f159d874630bda6b9abc5783a214538bb9020

--- app/controllers/admin/trucking_controller.rb
+++ app/controllers/admin/trucking_controller.rb
@@ -17,97 +17,28 @@ class Admin::TruckingController < Admin::AdminBaseController
   def edit
     tp = TruckingPricing.find(params[:id])
     ntp = params[:pricing].as_json
-    ntp.delete("id")
-    tp.update_attributes(ntp)
+    tp.update_attributes(ntp.except("id", "cargo_class", "load_type", "courier_id", "truck_type", "carriage"))
     response_handler(tp)
   end
 
   def create
-    data = params[:obj][:data].as_json
-    meta = params[:obj][:meta].as_json
-    global = params[:obj][:global].as_json
-    query_holder = {}
-    truckingQueries = []
-    truckingPricings = []
-    directions = meta["direction"] == "either" ? %w[import export] : [meta["direction"]]
-    truckingHubId = "#{meta['nexus_id']}_#{meta['loadType']}_#{current_user.tenant_id}"
-    directions.each do |dir|
-      query_holder[dir] = {} unless query_holder[dir]
-      data.each do |d|
-        d.each do |dk, dv|
-          query = {}
-          query_holder[dir][dk] = [] unless query_holder[dir][dk]
-
-          dv.each do |k, v|
-            query[k] = if k.include?("upper") || k.include?("lower")
-                         v.clone.to_f
-                       else
-                         v.clone
-                       end
-          end
-          query.delete("table")
-          # query[:_id] = SecureRandom.uuid
-          query[:modifier] = meta["subModifier"]
-          query[:direction] = dir
-          query[:trucking_hub_id] = truckingHubId
-
-          query_holder[dir][dk] << query
-        end
-      end
-
-      query_holder[dir].each do |k, v|
-        p v.uniq
-        query_holder[dir][k] = v.uniq[0]
-        query_holder[dir][k][:_id] = SecureRandom.uuid
-        truckingQueries << query_holder[dir][k]
-      end
-
-      data.each do |d|
-        d.each do |dk, dv|
-          query = query_holder[dir][dk]
-
-          dv["table"].each_with_index do |dt, _i|
-            tmp = {}
-            dt.each do |k, v|
-              tmp[k] = if k.include?("min") || k.include?("max")
-                         v.clone.to_f
-                       else
-                         v.clone
-                       end
-            end
-            tmp[:_id] = SecureRandom.uuid
-            tmp["type"] = dk
-            tmp["trucking_hub_id"] = truckingHubId
-            tmp["trucking_query_id"] = query[:_id]
-            tmp["tenant_id"] = current_user.tenant_id
-            truckingPricings << tmp
-          end
-        end
-      end
-    end
-    truckingPricings.each do |k|
-      update_item("truckingPricings", { _id: k[:_id] }, k)
-    end
-    truckingQueries.each do |k|
-      update_item("truckingQueries", { _id: k[:_id] }, k)
-    end
-    update_item("truckingHubs", { _id: truckingHubId }, type: (meta["type"]).to_s, load_type: meta["loadType"], modifier: (meta["modifier"]).to_s, tenant_id: current_user.tenant_id, nexus_id: meta["nexus_id"])
-
+    do_for_create
     response_handler(truckingHubId: truckingHubId)
   end
-
- 
 
   def overwrite_zonal_trucking_by_hub
     if params[:file]
       req = { "xlsx" => params[:file] }
-      resp = ExcelTool::OverrideTruckingRateByHub.new(params: req, _user: current_user, hub_id: params[:id]).perform
+      resp = ExcelTool::OverrideTruckingRateByHub.new(
+          params: req,
+          _user: current_user,
+          hub_id: params[:id]
+        ).perform
       response_handler(resp)
     else
       response_handler(false)
     end
   end
-
 
   def download
     options = params[:options].as_json.symbolize_keys
@@ -116,4 +47,145 @@ class Admin::TruckingController < Admin::AdminBaseController
     response_handler(url: url, key: "trucking")
   end
 
+  private
+
+    def clone_dv(k, v, keys = ["upper", "lower"])
+      if k.include?(keys[0]) || k.include?(keys[1])
+       v.clone.to_f
+      else
+       v.clone
+      end
+    end
+
+    def create_query(dv)
+      query = {}
+      dv.each do |k, v|
+        query[k] = clone_dv(k, v)
+      end
+      query
+    end
+
+    def populate_query_holder(dv, dir, dk)
+      query_holder[dir][dk] = [] unless query_holder[dir][dk]
+
+      query = create_query(dv)
+      query.delete("table")
+      query[:modifier] = meta["subModifier"]
+      query[:direction] = dir
+      query[:trucking_hub_id] = truckingHubId
+
+      query_holder[dir][dk] << query
+    end
+
+    def data
+      @data ||= params[:obj][:data].as_json
+    end
+
+    def meta
+      @meta ||= params[:obj][:meta].as_json
+    end
+
+    def global
+      @global ||= params[:obj][:global].as_json
+    end
+
+    def query_holder
+       @query_holder ||= {}
+    end
+
+    def truckingQueries
+      @truckingQueries ||= []
+    end
+
+    def truckingPricings
+      @truckingPricings ||= []
+    end
+
+    def directions
+      @directions ||= meta["direction"] == "either" ? %w[import export] : [meta["direction"]]
+    end
+
+    def truckingHubId
+      @truckingHubId = "#{meta['nexus_id']}_#{meta['loadType']}_#{current_user.tenant_id}"
+    end
+
+    def iterate_and_populate_query_holder
+      data.each do |d|
+        d.each do |dk, dv|
+          populate_query_holder(dv, dir, dk)
+        end
+      end
+    end
+
+    def update_query_holder_and_truckingqueries(dir)
+      query_holder[dir].each do |k, v|
+        p v.uniq
+        query_holder[dir][k] = v.uniq[0]
+        query_holder[dir][k][:_id] = SecureRandom.uuid
+        truckingQueries << query_holder[dir][k]
+      end
+    end
+
+    def temp_hash(dt)
+      tmp = {}
+      dt.each do |k, v|
+        tmp[k] = clone_dv(k, v, ["min", "max"])
+      end
+      tmp
+    end
+
+    def populate_trucking_pricing(dir, dk, dv)
+      query = query_holder[dir][dk]
+      dv["table"].each_with_index do |dt, _i|
+        tmp =  temp_hash(dt)
+        tmp[:_id] = SecureRandom.uuid
+        tmp["type"] = dk
+        tmp["trucking_hub_id"] = truckingHubId
+        tmp["trucking_query_id"] = query[:_id]
+        tmp["tenant_id"] = current_user.tenant_id
+        truckingPricings << tmp
+      end
+    end
+
+    def iterate_and_populate_trucking_pricings
+      data.each do |d|
+        d.each do |dk, dv|
+          populate_trucking_pricing(dir, dk, dv)
+        end
+      end
+    end
+
+    def update_item_truckingPricings
+      truckingPricings.each do |k|
+        update_item("truckingPricings", { _id: k[:_id] }, k)
+      end
+    end
+
+    def update_item_truckingQueries
+      truckingQueries.each do |k|
+        update_item("truckingQueries", { _id: k[:_id] }, k)
+      end
+    end
+
+    def update_item_truckingHubs
+      update_item("truckingHubs", 
+        { _id: truckingHubId },
+        type: (meta["type"]).to_s,
+        load_type: meta["loadType"],
+        modifier: (meta["modifier"]).to_s,
+        tenant_id: current_user.tenant_id,
+        nexus_id: meta["nexus_id"])
+    end
+
+    def do_for_create
+      directions.each do |dir|
+        query_holder[dir] = {} unless query_holder[dir]
+        iterate_and_populate_query_holder
+        update_query_holder_and_truckingqueries(dir)
+        iterate_and_populate_trucking_pricings
+      end
+      update_item_truckingPricings
+      update_item_truckingQueries
+      update_item_truckingHubs
+    end
 end
