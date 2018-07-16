b0948fb3d239f511534c05ffd7930177323ff334
Warwick Beamish <wkbeamish@gmail.com>
2018-07-16 18:57:19 +0200

    for beta server
    Former-commit-id: ced2743ecfd5339a0de267ce672a9e83dd336834

--- config/initializers/mjml.rb
+++ config/initializers/mjml.rb
@@ -1,3 +1,3 @@
-Mjml.setup do |config|
-  config.template_language = :erb # :erb (default), :slim, :haml, or any other you are using
-end
+# Mjml.setup do |config|
+#   config.template_language = :erb # :erb (default), :slim, :haml, or any other you are using
+# end
