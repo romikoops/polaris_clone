d582e61293ba4a01dee02b396ee25db1e3a30444
Obiora-Imah <obiora.imah@gmail.com>
2018-07-17 08:16:03 +0000

    Dynamic testing
    Former-commit-id: d5d475b924d26f88c804abf360cff7ae5c2e6708

--- spec/classes/offer_calculator_service/charge_calculator_spec.rb
+++ spec/classes/offer_calculator_service/charge_calculator_spec.rb
@@ -2,23 +2,28 @@ require 'rails_helper'
 
 RSpec.describe OfferCalculatorService::ChargeCalculator do
   describe "#perform", :vcr do
-    include SetupHelper
+    include SetupHelper 
+    json_test = test_cases_from_excel("#{Rails.root}/spec/test_sheets/spec_sheet.xlsx", "Sheet1")
+    # json_test = test_cases_from_json("#{Rails.root}/spec/test_sheets/spec_sheet.json")
 
-    it "should calculate correct shipping charge" do
-      setup = variables_setup(trucking: {"on_carriage"=>{"truck_type"=>""}, "pre_carriage"=>{"truck_type"=>""}})
-      request_stubber('6985c41eadad14bbab851c518745c236', 'EUR')
-      shipment = setup[:_shipment]
-      schedule = setup[:_schedule]
-      user = setup[:_user]
-      offer_charge = OfferCalculatorService::ChargeCalculator.new(
-        shipment:      shipment,
-        trucking_data: {},
-        schedule:      schedule,
-        user:          user
-      ).perform
+    json_test.each do |test|
+      test = test.deep_symbolize_keys
+      it "should have a total price of #{test[:target_price]} and currency is #{test[:target_currency]}" do
+        setup = variables_setup(test)
+        request_stubber('6985c41eadad14bbab851c518745c236', test[:target_currency])
+        shipment = setup[:_shipment]
+        schedule = setup[:_schedule]
+        user = setup[:_user]
+        offer_charge = OfferCalculatorService::ChargeCalculator.new(
+          shipment:      shipment,
+          trucking_data: {},
+          schedule:      schedule,
+          user:          user
 
-      shipment_charge = shipment.charge_breakdowns.selected.charge("grand_total").price.value
-      expect(offer_charge.price.value).to eq(shipment_charge)
+        ).perform
+        expect(offer_charge.price.value.to_f).to eq(test[:target_price])
+        expect(offer_charge.price.currency).to eq(test[:target_currency])
+      end
     end
   end
 end
