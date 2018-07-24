e533d521beff20a2912a0812639c2b90015dd43c
Warwick Beamish <wkbeamish@gmail.com>
2018-07-24 11:09:07 +0200

    swap for worker v7
    Former-commit-id: 06d247c6dbb5204a8438354e0389754a332db362

--- .ebextensions/hooks/setup_swap.sh
+++ .ebextensions/hooks/setup_swap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-SWAPFILE=/var/swapfile
-SWAP_MEGABYTES=1024
+set -o xtrace
+set -e
 
-if [ -f $SWAPFILE ]; then
-	echo "Swapfile $SWAPFILE found, assuming already setup"
-	exit;
+if grep -E 'SwapTotal:\s+0+\s+kB' /proc/meminfo; then
+    echo "Positively identified no swap space, creating some."
+    dd if=/dev/zero of=/var/swapfile bs=1M count=1024
+    /sbin/mkswap /var/swapfile
+    chmod 000 /var/swapfile
+    /sbin/swapon /var/swapfile
+else
+    echo "Did not confirm zero swap space, doing nothing."
 fi
-
-/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
-/bin/chmod 600 $SWAPFILE
-/sbin/mkswap $SWAPFILE
-/sbin/swapon $SWAPFILE
