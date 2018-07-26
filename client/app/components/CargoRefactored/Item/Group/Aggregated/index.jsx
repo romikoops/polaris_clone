685b5a21894c92d95b859390772c3caa68cc33c8
Dejan Toteff <deyan8284@gmail.com>
2018-07-26 18:28:02 +0300

    refactor cargo.item.group.aggregated component
    Former-commit-id: f8a00182e8b7731e060d6fe478b04c37fbc781b9

--- client/app/components/CargoRefactored/Item/Group/Aggregated/index.jsx
+++ client/app/components/CargoRefactored/Item/Group/Aggregated/index.jsx
@@ -1,51 +1,55 @@
 import React from 'react'
 import styles from '../CargoItemGroup.scss'
 import PropTypes from '../../../../../prop-types'
+import { trim, ROW, WRAP_ROW, ALIGN_CENTER } from '../../../../../classNames'
 
 export default function CargoItemGroupAggregated ({ group }) {
+  const chargableWeight = group.size_class ? '' : +(group.chargeable_weight).toFixed(3)
+
   return (
-    <div className={
-      `${styles.panel} ${styles.open_panel} flex-100 ` +
-      'layout-row layout-wrap layout-align-start-center'
-    }
+    <div className={trim(`
+      ${styles.panel} 
+      ${styles.open_panel} 
+      ${WRAP_ROW(100)}
+      layout-align-start-center
+    `)}
     >
-      {/* <div className={
-        `${styles.detailed_row} flex-100 layout-row layout-wrap layout-align-none-center`
-      }
+      <div className={trim(`
+          ${styles.detailed_row_aggregated}
+          ${WRAP_ROW(100)} 
+          layout-align-none-center
+        `)}
       >
-        <h4 className="flex-none"> Aggregate Values:</h4>
-      </div> */}
-      <div className={
-        `${styles.detailed_row_aggregated} flex-100 ` +
-        'layout-row layout-wrap layout-align-none-center'
-      }
-      >
-        <div className="flex-33 layout-row layout-align-space-around">
+        <div className={`${ROW(33)} layout-align-space-around`}>
           <div className="layout-column">
-            <p className="flex-none layout-row layout-align-center-center"><span className={styles.cargo_type}>{group.payload_in_kg || group.weight}</span> &nbsp;kg </p>
-            <p className="flex-none layout-row layout-align-center-center">Gross Weight</p>
+            <p className={`${ROW('none')} ${ALIGN_CENTER}`}>
+              <span className={styles.cargo_type}>
+                {group.payload_in_kg || group.weight}
+              </span> &nbsp;kg </p>
+            <p className={`${ROW('none')} ${ALIGN_CENTER}`}>Gross Weight</p>
           </div>
         </div>
 
-        <div className="flex-33 layout-row layout-align-space-around">
+        <div className={`${ROW(33)} layout-align-space-around`}>
           <div className="layout-column">
-            <p className="flex-none layout-row layout-align-center-center">
+            <p className={`${ROW('none')} ${ALIGN_CENTER}`}>
               <span className={styles.cargo_type}>
                 {(+group.volume).toFixed(3)}
               </span> &nbsp;m<sup>3</sup>
             </p>
-            <p className="flex-none layout-row layout-align-center-center">Volume</p>
+            <p className={`${ROW('none')} ${ALIGN_CENTER}`}>Volume</p>
           </div>
         </div>
-        <div className="flex-33 layout-row layout-align-space-around">
+
+        <div className={`${ROW(33)} layout-align-space-around`}>
           <div className="layout-column">
-            <p className="flex-none layout-row layout-align-center-center">
+            <p className={`${ROW('none')} ${ALIGN_CENTER}`}>
               <span className={styles.cargo_type}>
-                {!group.size_class ? +(group.chargeable_weight).toFixed(3) : ''}
+                {chargableWeight}
               </span>
               &nbsp;kg
             </p>
-            <p className="flex-none layout-row layout-align-center-center">Chargeable Weight</p>
+            <p className={`${ROW('none')} ${ALIGN_CENTER}`}>Chargeable Weight</p>
           </div>
         </div>
       </div>
