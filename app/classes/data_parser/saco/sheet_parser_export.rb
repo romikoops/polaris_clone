a0c94b1f9ace77143a3218a99c813adcd23f2fc0
Warwick Beamish <wkbeamish@gmail.com>
2018-08-14 14:00:16 +0200

    tenant checker working
    Former-commit-id: 1517953388972b15b7ceefbe2ba1b2af98449779

--- app/classes/data_parser/saco/sheet_parser_export.rb
+++ app/classes/data_parser/saco/sheet_parser_export.rb
@@ -4,7 +4,7 @@ module DataParser
       attr_reader :path, :user, :port_object, :counterpart_hub_name, :load_type, :hub_type, :input_language
 
       def post_initialize(args)
-        @sheet = @xlsx.sheet(@xlsx.sheets.first)
+        @sheets = @xlsx.sheets
         @counterpart_hub_name = args[:counterpart_hub_name]
         @load_type = args[:load_type]
         @hub_type = args[:hub_type]
@@ -88,46 +88,106 @@ module DataParser
           string.split(', ').map{|s| split_and_capitalise(s)}
         end
       
-        def row_to_hash(row_index, country)
+        def row_to_hash
           {
-            port:     @sheet.cell("B", row_index),
-            code:     @sheet.cell("D", row_index),
-            rate:     @sheet.cell("F", row_index),
-            currency: @sheet.cell("I", row_index),
-            min:      @sheet.cell("J", row_index),
-            basis:    @sheet.cell("K", row_index),
-            notes:    @sheet.cell("M", row_index),
-            country:  country
+            ports_of_loading:     @row[:origins],
+            carrier:     @row[:carrier],
+            rates:     {
+              fcl_20: @row[:fcl_20_rate],
+              fcl_40: @row[:fcl_40_rate],
+              fcl_40_hq: @row[:fcl_40_hq_rate],
+            },
+            currency: @row[:currency],
+            fees:      {
+              thc: @row[:thc],
+              isps: @row[:isps],
+              ebs: @row[:ebs]
+            },
+            notes:    @row[:transshipments],
+            country:  @row[:country],
+            port_of_destination: @row[:destination],
+            effective_date: @row[:effective_date],
+            expiration_date: @row[:expiration_date],
+            transittime: @row[:transit_time]
           }
         end
 
-        def name_and_service_level(str)
-          if str.include?('(Economy)')
-            name = str.split(' (').first
-            return name, 'economy'
-          elsif str.include?(' - Express')
-            name = str.split(' - ').first
-            return name, 'express'
-          elsif str.include?('(')
-              name = str.split(' (').first
-              return name, 'standard'
+        def service_level_from_transshipment(str)
+          if str.downcase.include?('via')
+            service_level = str[/\(.*?\)/]
           else
-            return str, 'standard'
+            service_level = str
           end
+          return service_level.downcase.gsub(' ', '_')
         end
 
-        def parse_notes_for_rate(notes)
-          if notes && notes.include?('PLUS ONCARRIAGE')
-            rate, min = find_money_values(notes)
-            rate_currency, rate_value = rate ? rate.split(' ') : 'USD 0'.split(' ')
-            min_currency, min_value = min ? min.split(' ') : 'USD 0'.split(' ')
-            return { min: min_value.to_f, rate: rate_value.to_f, currency: rate_currency, code: 'XAS', rate_basis: 'PER_CBM'}
-          else
-            return false
+       def determine_routes(hash)
+        itineraries = {}
+        origins = hash[:ports_of_loading].split('/').map do |hub_code|
+          
+          hub_name = determine_hub_from_abbreviation(hub_code)
+          itineraries[hub_code] =  = "#{destination_port_name} - #{hub_name}"
+        end
+        itineraries
+       end
+      
+       def destination_port_name
+        port_name = @row[:destination]
+        if port_name.downcase.end_with?(' port')
+          return port_name
+        else
+          return "#{port_name} Port"
+       end
+
+      def determine_hub_from_abbreviation(abv)
+        case abv
+        when 'RTM'
+          return 'Rotterdam Port'  
+        when 'BRV'
+          return 'Bremerhaven Port'  
+        when 'ANR'
+          return 'Antwerp Port'  
+        when 'HAM'
+          return 'Hamburg Port'  
+        when 'WVN'
+          return 'Wilhelmhaven Port'  
+        when 'FXT'
+          return 'Felixstowe Port'  
+      end
+
+      def hub_abbreviations_from_country(country)
+        case abv
+        when 'DE'
+          return ['HAM', 'BRV', 'WVN']
+        when 'NL'
+          return ['RTM']
+        when 'BE'
+          return ['ANR'] 
+      end
+
+      def fees_with_slashes(existing_fees, fee_code, str)
+        if str.include?('/')
+          rates = str.split('/')
+          ['DE', 'NL', 'BE'].each_with_index do |code, ind|
+            hub_abvs = hub_abbreviations_from_country(code)
+            hub_abvs.each do |hub_code|
+              existing_fees[hub_code] = {} unless existing_fees[hub_code]
+              existing_fees[hub_code][fee_code] = rates[ind] || 0
+            end
+          end
+        else
+          ['DE', 'NL', 'BE'].each_with_index do |code, ind|
+            hub_abvs = hub_abbreviations_from_country(code)
+            hub_abvs.each do |hub_code|
+              existing_fees[hub_code] = {} unless existing_fees[hub_code]
+              existing_fees[hub_code][fee_code] = str
+            end
           end
         end
+      end
 
         def hash_to_rate(hash)
+          itineraries_hash = determine_routes(hash)
           rates = []
           main_rate = { 
             rate_basis: 'PER_WM',
@@ -141,10 +201,7 @@ module DataParser
           else
             main_rate[:min] = hash[:min]
           end
-          note_rate = parse_notes_for_rate(hash[:notes])
-          if note_rate
-            rates << note_rate
-          end
+
           if hash[:port]
             name, service_level = name_and_service_level(hash[:port])
             port_name, country_name = get_hub_name("#{name}, #{hash[:country]}")
@@ -166,39 +223,63 @@ module DataParser
             }
           }
         end
+        
+        def validate_row(row)
+          check1 = Float(row[:fcl_20_rate]) rescue nil
+          check2 = Float(row[:fcl_40_rate]) rescue nil
+          check3 = Float(row[:fcl_40_hq_rate]) rescue nil
+          check4 = row[:country] || nil
+         return ![check1, check2, check3, check4].include?(nil)
 
-        def find_money_values(str, values_only=false)
-          currencies = %w(USD EUR)
-          reg = /((#{currencies.join("|")})\s*(\d+([.,]{1}[\d-]+)?))/m
-          str.scan(reg).map { |el| el[values_only ? 2 : 0] } # always strings
         end
+
+        def parse_sheet_rows(sheet)
+          sheet.parse(
+            country:         "Country",
+            destination:     "Destination",
+            transshipment:   "Transshipment",
+            effective_date:  "Valid from",
+            expiration_date: "Expiry Date",
+            carrier:          "Carrier",
+            fcl_20_rate:     "20'DC",
+            fcl_40_rate:     "40'DC",
+            fcl_40_hq_rate:  "40'HC",
+            thc:              "THC(D/NL/B)",
+            isps:             "ISPS",
+            currency:         "Cur.",
+            ebs:              "EBS/TEU",
+            origins:          "POL",
+            transit_time:     "Transittime"
+          )
+        end
+
+       
 
         def parse_rates
           row_hashes = []
-          @sheet.each_with_index do |_row, i|
-            row_index = i + 1
-      
-            # "Hafen" is unique anchor that differentiates the data
-            # of the individual countries.
-            next unless @sheet.cell("B", row_index) == "Hafen"
-      
-            country = get_country(row_index)
-            
-            # Look one row after the current one for actual data rows.
-            # Stop iterating when no valid float value for the "Rate" column is found.
-            row_index += 1
-            row_hash = row_to_hash(row_index, country)
-            
-            while !row_hash[:code].nil?
-              if row_hash[:port].nil?
+          @sheets.each do |sheet|
+            @sheet = @xlsx.sheet(sheet)
+            @sheet_rows = parse_sheet_rows(@sheet)
+            @sheet_rows.each_with_index do |_row, i|
+              row_index = i + 1
+              
+              # "Hafen" is unique anchor that differentiates the data
+              # of the individual countries.
+              next unless validate_row(row)
+              @row = row
+              
+              row_hash = row_to_hash
+              
+              while !row_hash[:destination].nil?
+                if row_hash[:port].nil?
+                  row_index += 1
+                  row_hash = row_to_hash(row_index, country)
+                end
+                converted_hash = hash_to_rate(row_hash)
+                row_hashes << converted_hash
                 row_index += 1
                 row_hash = row_to_hash(row_index, country)
               end
-              converted_hash = hash_to_rate(row_hash)
-              row_hashes << converted_hash
-              row_index += 1
-              row_hash = row_to_hash(row_index, country)
-            end
             
           end
           awesome_print row_hashes
