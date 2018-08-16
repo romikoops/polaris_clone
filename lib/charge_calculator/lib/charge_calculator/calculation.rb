4daa28217390ffa83f45b6a99f314b199eb15288
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-08-16 19:08:55 +0200

    improved variable naming in calculation
    Former-commit-id: b117586e5aab87c5b8f506a63d91599d28f93695

--- lib/charge_calculator/lib/charge_calculator/calculation.rb
+++ lib/charge_calculator/lib/charge_calculator/calculation.rb
@@ -22,11 +22,11 @@ module ChargeCalculator
     def perform
       rates.map do |rate|
         reducer = Reducers.get(rate.fetch(:reducer, :first))
-        calculated_price = reducer.apply(calculated_prices(rate[:prices]))
-        calculated_price = Reducers::Max.new.apply [calculated_price, BigDecimal(rate[:min_price])]
+        calculated_price_value = reducer.apply(calculated_price_values(rate[:prices]))
+        calculated_price_value = Reducers::Max.new.apply [calculated_price_value, BigDecimal(rate[:min_price])]
 
         {
-          amount:      calculated_price,
+          amount:      calculated_price_value,
           currency:    rate[:currency],
           category:    rate[:category],
           description: rate[:category]
@@ -34,15 +34,20 @@ module ChargeCalculator
       end
     end
 
-    def calculated_prices(prices)
-      filtered_prices(prices).map do |price|
-        context[price[:basis].to_sym] * BigDecimal(price[:amount]) * context.fetch(:quantity, 1)
+    def calculated_price_values(prices_data)
+      filtered_prices_data(prices_data).map do |price_data|
+        [
+          context[price_data[:basis].to_sym],
+          BigDecimal(price_data[:amount]),
+          context.fetch(:quantity, 1)
+        ].reduce(:*)
       end
     end
 
-    def filtered_prices(prices)
-      prices.select do |price|
-        price[:rule].nil? || RuleEngine.match(context: context.to_h, rule: price[:rule]).result
+    def filtered_prices_data(prices_data)
+      prices_data.select do |price_data|
+        price_data[:rule].nil? ||
+          RuleEngine.match(context: context.to_h, rule: price_data[:rule]).result
       end
     end
   end
