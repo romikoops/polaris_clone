17e6e7cc8cabc49239ede9c25fbd2bb22e1fd79e
Angelica Vanni <angelicavanni@Angelicas-MBP.fritz.box>
2018-08-16 18:57:20 +0200

    removed contacts from dashboard home
    Former-commit-id: b3419bdcfc0275439f059feeec50745ba63a395b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,11 +24,6 @@ class UsersController < ApplicationController
     @finished_shipments = finished_shipments.map(&:with_address_options_json)
 
     @pricings = get_user_pricings(@shipper.id)
-    @contacts = @shipper.contacts.where(alias: false).map do |contact|
-      contact.as_json(
-        include: { location: { include: { country: { only: :name} },
-        except: %i(created_at updated_at country_id) } },
-        except: %i(created_at updated_at location_id))
     end
     @aliases = @shipper.contacts.where(alias: true)
     user_locs = @shipper.user_locations
