1dce3d5ea45ab51768dfcfb7413a96eb8eef9bb8
Dejan Toteff <deyan8284@gmail.com>
2018-08-23 16:10:21 +0300

    i18n complete for file.uploader
    Former-commit-id: 1bdacd28ae8aeec896a6fb2dd18f614e6f05cafa

---
+++ client/app/components/FileUploader/FileUploaderDone.jsx
@@ -0,0 +1,159 @@
+import React from 'react'
+import {translate} from 'react-i18next'
+import fetch from 'isomorphic-fetch'
+import { Promise } from 'es6-promise-promise'
+import ReactTooltip from 'react-tooltip'
+import { v4 } from 'uuid'
+import PropTypes from '../../prop-types'
+import { BASE_URL } from '../../constants'
+import { authHeader } from '../../helpers'
+import { RoundButton } from '../RoundButton/RoundButton'
+import SquareButton from '../SquareButton'
+import styles from './FileUploader.scss'
+
+class FileUploader extends React.Component {
+  static handleResponse (response) {
+    if (!response.ok) {
+      return Promise.reject(response.statusText)
+    }
+
+    return response.json()
+  }
+  constructor (props) {
+    super(props)
+    this.state = {
+      file: null,
+      error: false
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+  }
+  onFormSubmit (e) {
+    e.preventDefault()
+    if (this.state.file) {
+      this.fileUpload(this.state.file)
+    }
+  }
+  onChange (e) {
+    this.fileUpload(e.target.files[0])
+  }
+  fileUpload (baseFile) {
+    const file = baseFile
+    const {
+      dispatchFn, 
+      type, 
+      uploadFn, 
+      url, 
+    } = this.props
+
+    if (!file) return ''
+    if (dispatchFn) {
+      if (type) {
+        file.doc_type = type
+      }
+      dispatchFn(file)
+      if (this.uploaderInput.files.length) {
+        this.uploaderInput.value = ''
+      }
+      return null
+    }
+    if (uploadFn) return uploadFn(file, type, url)
+    
+    const formData = new window.FormData()
+    formData.append('file', file)
+    formData.append('type', type)
+    const requestOptions = {
+      method: 'POST',
+      headers: { ...authHeader() },
+      body: formData
+    }
+    const uploadUrl = BASE_URL + url
+    fetch(uploadUrl, requestOptions).then(FileUploader.handleResponse)
+    if (this.uploaderInput.files.length) {
+      this.uploaderInput.value = ''
+    }
+    return null
+  }
+  showFileTypeError () {
+    this.setState({ error: true })
+    this.alertTimeout = setTimeout(() => this.setState({ error: false }), 5000)
+  }
+  clickUploaderInput (e) {
+    e.preventDefault()
+    this.uploaderInput.click()
+  }
+  render () {
+    const {
+      size,
+      square, 
+      t,
+      theme, 
+      tooltip, 
+      type, 
+    } = this.props
+    const tooltipId = v4()
+    const errorStyle = this.state.error ? styles.error : ''
+
+    return (
+      <div
+        className={`flex-none layout-row ${styles.upload_btn_wrapper} `}
+        data-tip={tooltip}
+        data-for={tooltipId}
+      >
+        <form>
+          {square ? (
+            <SquareButton
+              text={t('common:upload')}
+              theme={theme}
+              size={size}
+              handleNext={e => this.clickUploaderInput(e)}
+              active
+              border
+            />
+          ) : (
+            <RoundButton
+              text={t('common:upload')}
+              theme={theme}
+              size={size}
+              handleNext={e => this.clickUploaderInput(e)}
+              active
+            />
+          )}
+
+          <ReactTooltip id={tooltipId} className={styles.tooltip} effect="solid" />
+          <input
+            type="file"
+            onChange={this.onChange}
+            name={type}
+            ref={(input) => {
+              this.uploaderInput = input
+            }}
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+FileUploader.propTypes = {
+  url: PropTypes.string.isRequired,
+  square: PropTypes.bool,
+  type: PropTypes.string.isRequired,
+  theme: PropTypes.theme,
+  dispatchFn: PropTypes.func,
+  uploadFn: PropTypes.func,
+  tooltip: PropTypes.string,
+  size: PropTypes.string
+}
+
+FileUploader.defaultProps = {
+  uploadFn: null,
+  square: false,
+  dispatchFn: null,
+  theme: null,
+  tooltip: '',
+  size: 'small'
+}
+
+export default translate('common')(FileUploader)
