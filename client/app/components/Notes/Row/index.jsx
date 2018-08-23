de7fbb23894ebfc0da30a09eae7f2a617f08a2a4
Angelica Vanni <angelica.vanni@itsmycargo.com>
2018-08-23 11:50:39 +0300

    editing polished
    Former-commit-id: 807cf8ab31e44cbcff68721aaaba8d16f747d4db

--- client/app/components/Notes/Row/index.jsx
+++ client/app/components/Notes/Row/index.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import PropTypes from '../../../prop-types'
 import NotesCard from '../Card'
 import styles from './index.scss'
+import adminStyles from '../../Admin/Admin.scss'
 
-const NotesRow = ({ notes, theme }) => {
+const NotesRow = ({
+  notes, theme, toggleNotesEdit, textStyle
+}) => {
   const noteCards = notes.map(n =>
     <NotesCard note={n} theme={theme} />)
+
   return (
-    <div className="layout-row flex-100 layout-wrap layout-align-center">
+    <div className="layout-row flex-100 layout-wrap layout-align-center-center">
       <div
-        className={`flex-100 layout-row layout-align-space-between-center ${styles.sec_header}`}
+        className={`flex-100 layout-row layout-start-center ${adminStyles.sec_header}`}
       >
-        <p className={` ${styles.sec_header_text} flex-none`}> Notes </p>
+        <p className={`${adminStyles.sec_header_text} flex-none`}> Notes </p>
+        <div
+          className="flex-10 offset-5 layout-row alyout-align-center-center pointy"
+          onClick={toggleNotesEdit}
+        >
+          <i className="fa fa-pencil clip pointy" style={textStyle} />
+        </div>
       </div>
+
       <div className="flex-100 layout-row layout-align-start-center layout-wrap">
         {noteCards}
       </div>
