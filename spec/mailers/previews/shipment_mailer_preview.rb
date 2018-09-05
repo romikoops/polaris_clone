1e7f706d66de3e974d179d34a62499b67c787fa0
Warwick Beamish <wkbeamish@gmail.com>
2018-09-05 09:33:34 +0200

    WIP - one sided swap
    Former-commit-id: d95e3e6ca08e2ca1d884add1b89fce3e75e330a0

--- spec/mailers/previews/shipment_mailer_preview.rb
+++ spec/mailers/previews/shipment_mailer_preview.rb
@@ -1,10 +1,12 @@
 class ShipmentMailerPreview < ActionMailer::Preview
   def tenant_notification
-    @shipment = Shipment.where(status: 'requested').last
+    @tenant = Tenant.saco
+    @shipment = @tenant.shipments.where(status: 'requested').last
     ShipmentMailer.tenant_notification(@shipment.user, @shipment)
   end
   def shipper_notification
-    @shipment = Shipment.where(status: 'requested').last
+    @tenant = Tenant.saco
+    @shipment = @tenant.shipments.where(status: 'requested').last
     ShipmentMailer.shipper_notification(@shipment.user, @shipment)
   end
 end
