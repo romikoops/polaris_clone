bf087b375a688375b1a7c6a12b1c070ebc557683
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2018-09-07 17:59:33 +0200

    Chore: Run global coverage
    Former-commit-id: fe97f3368c5464586e190513db7043806065ee51

--- lib/charge_calculator/spec/spec_helper.rb
+++ lib/charge_calculator/spec/spec_helper.rb
@@ -1,15 +1,21 @@
-require "bundler/setup"
+# frozen_string_literal: true
+
+require 'bundler/setup'
 
 require 'simplecov'
+if ENV['COVERAGE_DIR']
+  SimpleCov.coverage_dir(ENV['COVERAGE_DIR'])
+  SimpleCov.merge_timeout 3600
+  SimpleCov.command_name 'charge_calculator/rspec'
+end
 SimpleCov.start
 
-require "charge_calculator"
-
-require "support/json_schema_matcher"
+require 'charge_calculator'
+require 'support/json_schema_matcher'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
