24c73aa2eedbb0705dc6b2e34e04314917b1d0d8
Warwick Beamish <wkbeamish@gmail.com>
2018-09-10 12:39:20 +0200

    IMC-280 - changes made
    Former-commit-id: af0a81356db3d106b0a4f0031753883ad7387948

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -14,10 +14,15 @@ describe Country, type: :model do
           'address_components' => [{ 'long_name' => 'Sweden', 'short_name' => 'SE', 'types' => %w(country political) }],
           'formatted_address' => 'Sweden',
           'geometry' =>
-          { 'bounds' => { 'northeast' => { 'lat' => 69.0599709, 'lng' => 24.1773101 }, 'southwest' => { 'lat' => 55.0059799, 'lng' => 10.5798 } },
-            'location' => { 'lat' => 60.12816100000001, 'lng' => 18.643501 },
+          { 'bounds' => { 
+            'northeast' => { 'lat' => 69.0599709, 'lng' => 24.1773101 },
+            'southwest' => { 'lat' => 55.0059799, 'lng' => 10.5798 } },
+            'location' => { 
+              'lat' => 60.12816100000001, 'lng' => 18.643501 },
             'location_type' => 'APPROXIMATE',
-            'viewport' => { 'northeast' => { 'lat' => 69.0599709, 'lng' => 24.1773101 }, 'southwest' => { 'lat' => 55.0059799, 'lng' => 10.5798 } } },
+            'viewport' => { 
+              'northeast' => { 'lat' => 69.0599709, 'lng' => 24.1773101 },
+              'southwest' => { 'lat' => 55.0059799, 'lng' => 10.5798 } } },
           'partial_match' => true,
           'place_id' => 'ChIJ8fA1bTmyXEYRYm-tjaLruCI',
           'types' => %w(country political)
@@ -25,16 +30,15 @@ describe Country, type: :model do
       ]
     )
   end
-  context 'class methods' do
-    context '.geo_find_by_name' do
-      it 'returns nil if no country was found' do
-        expect(described_class.geo_find_by_name('asdfas')).to be_nil
-      end
+  
+  context '.geo_find_by_name' do
+    it 'returns nil if no country was found' do
+      expect(described_class.geo_find_by_name('asdfas')).to be_nil
+    end
 
-      it 'returns the correct country' do
-        country = create(:country)
-        expect(described_class.geo_find_by_name('sweden')).to eq(country)
-      end
+    it 'returns the correct country' do
+      country = create(:country)
+      expect(described_class.geo_find_by_name('sweden')).to eq(country)
     end
   end
 end
