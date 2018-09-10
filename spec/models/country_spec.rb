2fab98d17e48a1c6ae90870387f6115780fc3264
Warwick Beamish <wkbeamish@gmail.com>
2018-09-10 11:53:20 +0200

    IMC-280 - geocoder stubs working
    Former-commit-id: 581caefa57faf880458580a443a3d5142af5d0e4

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -2,42 +2,39 @@
 
 require 'rails_helper'
 
-# Allow outbound HTTP requests.
-# WebMock.allow_net_connect!
-
 describe Country, type: :model do
   before(:all) do
     Geocoder.configure(lookup: :test)
     Geocoder::Lookup::Test.add_stub(
-      {:country => "asdfas"}, []
+      { country: 'asdfas' }, []
     )
     Geocoder::Lookup::Test.add_stub(
-      {:country => "sweden"}, [
+      { country: 'sweden' }, [
         {
-          "address_components"=>[{"long_name"=>"Sweden", "short_name"=>"SE", "types"=>["country", "political"]}],
-          "formatted_address"=>"Sweden",
-          "geometry"=>
-          {"bounds"=>{"northeast"=>{"lat"=>69.0599709, "lng"=>24.1773101}, "southwest"=>{"lat"=>55.0059799, "lng"=>10.5798}},
-            "location"=>{"lat"=>60.12816100000001, "lng"=>18.643501},
-            "location_type"=>"APPROXIMATE",
-            "viewport"=>{"northeast"=>{"lat"=>69.0599709, "lng"=>24.1773101}, "southwest"=>{"lat"=>55.0059799, "lng"=>10.5798}}},
-          "partial_match"=>true,
-          "place_id"=>"ChIJ8fA1bTmyXEYRYm-tjaLruCI",
-          "types"=>["country", "political"]
+          'address_components' => [{ 'long_name' => 'Sweden', 'short_name' => 'SE', 'types' => %w(country political) }],
+          'formatted_address' => 'Sweden',
+          'geometry' =>
+          { 'bounds' => { 'northeast' => { 'lat' => 69.0599709, 'lng' => 24.1773101 }, 'southwest' => { 'lat' => 55.0059799, 'lng' => 10.5798 } },
+            'location' => { 'lat' => 60.12816100000001, 'lng' => 18.643501 },
+            'location_type' => 'APPROXIMATE',
+            'viewport' => { 'northeast' => { 'lat' => 69.0599709, 'lng' => 24.1773101 }, 'southwest' => { 'lat' => 55.0059799, 'lng' => 10.5798 } } },
+          'partial_match' => true,
+          'place_id' => 'ChIJ8fA1bTmyXEYRYm-tjaLruCI',
+          'types' => %w(country political)
         }
       ]
     )
   end
-	context 'class methods' do
-		context '.geo_find_by_name' do
-			it 'returns nil if no country was found' do      		
-		  	expect(described_class.geo_find_by_name("asdfas")).to be_nil
-			end
+  context 'class methods' do
+    context '.geo_find_by_name' do
+      it 'returns nil if no country was found' do
+        expect(described_class.geo_find_by_name('asdfas')).to be_nil
+      end
 
-			it 'returns the correct country' do
-				country = create(:country)
-		  	expect(described_class.geo_find_by_name("sweden")).to eq(country)
-			end
-		end
-	end
+      it 'returns the correct country' do
+        country = create(:country)
+        expect(described_class.geo_find_by_name('sweden')).to eq(country)
+      end
+    end
+  end
 end
