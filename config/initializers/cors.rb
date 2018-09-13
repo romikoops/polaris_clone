300121cd2e4f7a9002376ad26487926ee2bbbd5c
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2018-09-14 01:52:47 +0200

    Chore: Code Style et Cleanup
    Former-commit-id: deefc7da2e15c3df0c33ef23797abff13239ddda

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -21,6 +21,7 @@ cors_origins = (Settings.cors.origins ? Regexp.new("\\A#{Settings.cors.origins}\
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins cors_origins
-    resource '*', headers: :any, expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], methods: %i(get post patch put delete options)
+    resource '*', headers: :any, expose: %w(access-token expiry token-type uid client),
+                  methods: %i(get post patch put delete options)
   end
 end
