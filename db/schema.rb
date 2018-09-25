09daca71c43059031cd30565a048243fa838c77a
Warwick Beamish <wkbeamish@gmail.com>
2018-09-25 13:48:10 +0200

    IMC-255 - schema corrected
    Former-commit-id: c3dfc52fe8f67f4e53655b86504f737044ab2cd7

--- db/schema.rb
+++ db/schema.rb
@@ -13,19 +13,11 @@
 ActiveRecord::Schema.define(version: 2018_09_13_232620) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
   enable_extension "plpgsql"
   enable_extension "postgis"
-
-  create_table "addon_charges", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
-    t.jsonb "rate"
-    t.string "input_type"
-    t.jsonb "conditions"
-    t.integer "addon_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  enable_extension "postgis_tiger_geocoder"
+  enable_extension "postgis_topology"
 
   create_table "addons", force: :cascade do |t|
     t.string "title"
@@ -605,10 +597,10 @@ ActiveRecord::Schema.define(version: 2018_09_13_232620) do
     t.jsonb "customs"
     t.bigint "transport_category_id"
     t.integer "incoterm_id"
-    t.integer "origin_nexus_id"
-    t.integer "destination_nexus_id"
     t.datetime "closing_date"
     t.string "incoterm_text"
+    t.integer "origin_nexus_id"
+    t.integer "destination_nexus_id"
     t.datetime "planned_origin_drop_off_date"
     t.integer "quotation_id"
     t.index ["transport_category_id"], name: "index_shipments_on_transport_category_id"
@@ -789,6 +781,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_232620) do
     t.string "currency", default: "EUR"
     t.string "vat_number"
     t.boolean "allow_password_change", default: false, null: false
+    t.jsonb "optin_status", default: {}
     t.integer "optin_status_id"
     t.string "external_id"
     t.integer "agency_id"
