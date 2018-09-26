5422ead0246089caa54116d7bfc86287569f5ad5
Henry Perschk <henry.perschk@gmail.com>
2018-09-26 15:52:25 +0200

    change gc to speedtrans
    Former-commit-id: 489bf709263f9eaa7cb0d7f8bb1213fb8a08d498

--- db/custom_seeds/demo_seed.rb
+++ db/custom_seeds/demo_seed.rb
@@ -8,7 +8,7 @@ subdomains.each do |sub|
   tenant = Tenant.find_by_subdomain(sub)
 
   shipper = tenant.users.shipper.first
-  DataValidator::PricingValidator.new(tenant: tenant.id, user: shipper, key: 'data/speedtrans/pricing_expected_20180927_speedtrans.xlsx').perform
+  DataValidator::PricingValidator.new(tenant: tenant.id, user: shipper, key: 'data/speedtrans/speedtrans_pricing_test.xlsx').perform
   # tenant.itineraries.destroy_all
   # tenant.local_charges.destroy_all
   # tenant.customs_fees.destroy_all
