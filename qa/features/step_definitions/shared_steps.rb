099caa92cebae957dec5647ac637b0fdfbc1ca57
Warwick Beamish <wkbeamish@gmail.com>
2018-10-12 16:34:36 +0200

    IMC-282 - ready to gooooo
    Former-commit-id: b2d26337c9a0a3c92be9e1fe76ede2e6b8fe73ea

--- qa/features/step_definitions/shared_steps.rb
+++ qa/features/step_definitions/shared_steps.rb
@@ -1,11 +1,13 @@
-Given ('I am logged in successfully') do 
-  step "I am at the landing"
-  step "I am logged out"
-  step "I click the link to log in"
-  step "I expect to see the log in modal"
+# frozen_string_literal: true
 
-  step "I enter the correct email address"
-  step "I enter the correct password"
-  step "I click the sign in button"
-  step "I expect to be redirected to the account page"
+Given ('I am logged in successfully') do
+  step 'I am at the landing'
+  step 'I am logged out'
+  step 'I click the link to log in'
+  step 'I expect to see the log in modal'
+
+  step 'I enter the correct email address'
+  step 'I enter the correct password'
+  step 'I click the sign in button'
+  step 'I expect to be redirected to the account page'
 end
