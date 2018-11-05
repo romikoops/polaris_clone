52e06aad3f72067e37c2d508df5d103887b428c2
Warwick Beamish <wkbeamish@gmail.com>
2018-11-05 11:59:52 +0100

    IMC-718 - quote pdf adjusted - blocked by IMC-694
    Former-commit-id: 1f1c18d6a90a7055374c1ea91e76fbe3b20bd6c3

--- spec/mailers/previews/quote_mailer_preview.rb
+++ spec/mailers/previews/quote_mailer_preview.rb
@@ -1,7 +1,7 @@
 class QuoteMailerPreview < ActionMailer::Preview
   def quotation_email
     quotation = Quotation.last
-    tenant = Tenant.find_by_subdomain('gateway')
+    tenant = Tenant.find_by_subdomain('fivestar')
     @shipment = tenant.shipments.where(status: 'booking_process_started').last
     @shipments = tenant.shipments.where(quotation_id: quotation.id)
     @email = "demo@itsmycargo.com"
