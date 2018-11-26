eb28ca0b1bda480446b43cf422638d8fa19bf61c
Dejan Toteff <deyan8284@gmail.com>
2018-11-26 13:58:15 +0000

    Remove e2e folder
    Former-commit-id: 996ce9a2cef8115ed64e1c6729320ca8e2bb1e68

--- client/e2e/Order/steps/selectDate.js
+++
@@ -1,8 +0,0 @@
-const DATE_INPUT = 'input[placeholder="DD/MM/YYYY"]'
-
-export default async function selectDate (puppeteer) {
-  await puppeteer.saveStep('selectDate')
-  // await puppeteer.shouldMatchScreenshot('select.date')
-
-  expect(await puppeteer.selectFirstAvailableDay(DATE_INPUT)).toBeTruthy()
-}
