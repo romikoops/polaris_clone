a72fd8a596fcd810adb8a6469be8833881bb6557
Warwick Beamish <wkbeamish@gmail.com>
2018-11-27 16:28:12 +0100

    FIX - api url returned to config js
    Former-commit-id: bcd6dd2ee966390d3e6669b74dd53e1fd1277e95

--- client/app/config.js
+++ client/app/config.js
@@ -1,4 +1,5 @@
 window.keel = {
   environment: 'production',
-  sentryUrl: 'https://16d43a033af648da8b2f2933f6e62717@sentry.itsmycargo.tech/3'
+  sentryUrl: 'https://16d43a033af648da8b2f2933f6e62717@sentry.itsmycargo.tech/3',
+  apiUrl: 'https://api2.itsmycargo.com'
 }
