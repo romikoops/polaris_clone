327c8883aa8d56c2a6ab64db12cd43d180f29bfc
Francisco P Coutinho <francisco.lgpc@gmail.com>
2018-12-11 12:53:00 +0100

    IMC-952 - switched schedules to front end pagination in choose offer
    Former-commit-id: fbbe8c4e971032b02741edc9cafedfe6553386e9

--- client/app/helpers/hub.js
+++ client/app/helpers/hub.js
@@ -9,4 +9,26 @@ function splitName (fullName) {
   }
 }
 
+export function getHubType (hub) {
+  let hubType = ''
+  switch (hub.hub_type) {
+    case 'ocean':
+      hubType = 'Port'
+      break
+    case 'air':
+      hubType = 'Airport'
+      break
+    case 'rail':
+      hubType = 'Railyard'
+      break
+    case 'truck':
+      hubType = 'Depot'
+      break
+    default:
+      break
+  }
+
+  return hubType
+}
+
 export default splitName
