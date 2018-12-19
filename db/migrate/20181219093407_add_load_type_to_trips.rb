6aec663755a579cb6da8e8d421f0e77d599fe098
Warwick Beamish <wkbeamish@gmail.com>
2018-12-19 19:01:19 +0100

    IMC-997 - removed safe migration and schema reverted
    Former-commit-id: 61337f2b53cc3ded6ca3aab2e5173083cd5b695e

--- db/migrate/20181219093407_add_load_type_to_trips.rb
+++ db/migrate/20181219093407_add_load_type_to_trips.rb
@@ -2,8 +2,6 @@
 
 class AddLoadTypeToTrips < ActiveRecord::Migration[5.2]
   def change
-    safety_assured do
-      add_column :trips, :load_type, :string
-    end
+    add_column :trips, :load_type, :string
   end
 end
