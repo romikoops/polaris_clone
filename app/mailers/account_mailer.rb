e9f9e588dd184956dde7a8c84f2385e46fb4d2f5
Francisco P Coutinho <francisco.lgpc@gmail.com>
2019-01-03 14:32:57 +0100

    IMC-1097 - updated account mailers and added specs
    Former-commit-id: 8d24dc43dc8d0e4d14ceba3d5116b60b636ac3d1

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -27,7 +27,7 @@ class AccountMailer < Devise::Mailer
 
     attachments.inline['logo.png'] = URI.open(tenant.theme['logoLarge']).read
     opts[:from] = tenant.emails.dig('support', 'general')
-    opts[:subject] = 'ItsMyCargo Account Password Reset'
+    opts[:subject] = "#{tenant.name} Account Password Reset"
     redirect_url = base_url(tenant) + 'password_reset'
     @reset_url = "#{base_server_url}tenants/#{tenant.id}/auth/password/edit?redirect_url=#{redirect_url}&reset_password_token=#{token}"
 
---
+++ spec/mailers/account_mailer_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AccountMailer, type: :mailer do
+  let(:user) { create(:user) }
+
+  describe 'confirmation_instructions' do
+    let(:mail) do
+      described_class.confirmation_instructions(user, user.send_confirmation_instructions)
+    end
+
+    it 'renders the correct subject' do
+      expect(mail.subject).to eq('Demo Account Confirmation Email')
+    end
+
+    it 'renders the correct sender' do
+      expect(mail.from).to eq(['support@demo.com'])
+    end
+
+    it 'renders the correct receiver' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders a body with the correct text' do
+      expect(mail.body.encoded).to match('Thank you for registering')
+    end
+  end
+
+  describe 'reset_password_instructions' do
+    let(:mail) do
+      described_class.reset_password_instructions(user, user.send_reset_password_instructions)
+    end
+
+    it 'renders the correct subject' do
+      expect(mail.subject).to eq('Demo Account Password Reset')
+    end
+
+    it 'renders the correct sender' do
+      expect(mail.from).to eq(['support@demo.com'])
+    end
+
+    it 'renders the correct receiver' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders a body with the correct text' do
+      expect(mail.body.encoded).to match('Change my password')
+    end
+  end
+end
