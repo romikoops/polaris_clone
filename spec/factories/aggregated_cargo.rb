3e0f58df01fcc001e139d44c48f04561c7562c42
Warwick Beamish <wkbeamish@gmail.com>
2019-01-09 15:55:41 +0000

    CHORE - testing added for calc_aggregated_cargo_cbm_ratio
    Former-commit-id: 0525033d1fee7b69199680fc860021d8477eaefd

---
+++ spec/factories/aggregated_cargo.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :aggregated_cargo do
+    association :shipment
+
+    weight 200
+    volume 1.0
+    chargeable_weight 1000
+  end
+end
