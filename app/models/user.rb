94a568b335035f880ba1b91c8004ecf66d76a6e1
Warwick Beamish <wkbeamish@gmail.com>
2019-01-14 15:37:04 +0100

    IMC-1165 - agents excel tool fixed
    Former-commit-id: 35c4386b681e4138a690694c1ebbb6a878bd8269

--- app/models/user.rb
+++ app/models/user.rb
@@ -161,6 +161,10 @@ class User < ApplicationRecord
     as_json(include: :optin_status)
   end
 
+  def get_external_id
+    external_id || agency_manager&.external_id
+  end
+
   # Devise Token Auth override
   def token_validation_response
     as_json(
