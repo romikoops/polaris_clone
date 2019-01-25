1f0c0aae2761beec508c902c8c88a6cf4cfdda9d
Warwick Beamish <wkbeamish@gmail.com>
2019-01-25 11:32:03 +0100

    IMC-1212 - quote and shipment pdf updated
    Former-commit-id: f4ad1c4211ce72807241ca2a4d7e390fbe57a2ae

--- client/app/components/Cargo/Item/Group/index.jsx
+++ client/app/components/Cargo/Item/Group/index.jsx
@@ -67,53 +67,50 @@ class CargoItemGroup extends Component {
       >
 
         <div className={`${styles.unit_data_cell} flex-15 layout-row layout-align-center-center`}>
-          <img data-for={tooltipId} data-tip={t('common:length')} src={length} alt="length" border="0" />
-          {
-            showTooltip
-              ? <ReactTooltip className={styles.tooltip} id={tooltipId} effect="solid" />
-              : ''
-          }
-          <p className="flex-none">
-            <span>{group.items[0] ? group.items[0].dimension_x : ''}</span>
-            {' '}
+          <div className="">
+            <div className="layout-row">
+              <img data-for={tooltipId} data-tip={t('common:length')} src={length} alt="length" border="0" />
+              {
+                showTooltip
+                  ? <ReactTooltip className={styles.tooltip} id={tooltipId} effect="solid" />
+                  : ''
+              }
+              <p className="flex-none">
+                <span>{group.items[0] ? group.items[0].dimension_x : ''}</span>
+                {' '}
 cm
-          </p>
+              </p>
 
+            </div>
+            <p className={`flex-none layout-row layout-align-center-center ${styles.dims}`}>{t('common:length')}</p>
+          </div>
           <div className={`flex-none ${styles.operand}`}>x</div>
         </div>
         <div className={`${styles.unit_data_cell} flex-15 layout-row layout-align-center-center`}>
-          <img data-for={tooltipId} data-tip={t('common:width')} src={width} alt="width" border="0" />
-          {
-            showTooltip
-              ? <ReactTooltip className={styles.tooltip} id={tooltipId} effect="solid" />
-              : ''
-          }
-          <p className="flex-none">
-            <span>{group.items[0] ? group.items[0].dimension_y : ''}</span>
-            {' '}
-              cm
-          </p>
-
+          <div className="">
+            <div className="layout-row">
+              <img data-for={tooltipId} data-tip={t('common:width')} src={width} alt="width" border="0" />
+              {
+                showTooltip
+                  ? <ReactTooltip className={styles.tooltip} id={tooltipId} effect="solid" />
+                  : ''
+              }
+              <p className="flex-none">
+                <span>{group.items[0] ? group.items[0].dimension_y : ''}</span>
+                {' '}
+cm
+              </p>
+            </div>
+            <p className={`flex-none layout-row layout-align-center-center ${styles.dims}`}>{t('common:width')}</p>
+          </div>
           <div className={`flex-none ${styles.operand}`}>x</div>
         </div>
 
         <div className={`${styles.unit_data_cell} flex-15 layout-row layout-align-center-center`}>
-          <img data-for={tooltipId} data-tip={t('common:height')} src={height} alt="height" border="0" />
-          {
-            showTooltip
-              ? <ReactTooltip className={styles.tooltip} id={tooltipId} effect="solid" />
-              : ''
-          }
-          <p className="flex-none">
-            <span>{group.items[0] ? group.items[0].dimension_z : ''}</span>
-            {' '}
-              cm
-          </p>
-
+            <p className={`flex-none layout-row layout-align-center-center ${styles.dims}`}>{t('common:height')}</p>
+          </div>
           <div className={`flex-none ${styles.operand}`}>=</div>
         </div>
-
-       
 
         <div className={`${styles.unit_data_cell}  flex-15 layout-row layout-align-center-center`}>
           <div className="">
@@ -126,10 +123,10 @@ cm
               {' '}
 &nbsp;m
               <sup>3</sup>
-            </p>
             <p className="flex-none layout-row layout-align-center-center">{t('common:volume')}</p>
-            <div className={`flex-none ${styles.operand}`}>x</div>
+
           </div>
+          <div className={`flex-none ${styles.operand}`}>x</div>
         </div>
         <div className={`${styles.unit_data_cell}  flex-15 layout-row layout-align-center-center`}>
           <div className="">
@@ -194,7 +191,7 @@ cm
               </span>
               &nbsp;kg
             </p>
-            <p className="flex-none layout-row layout-align-center-center">{t('cargo:totalWeight')}</p>
+            <p className="flex-none layout-row layout-align-center-center">{t('cargo:totalGrossWeight')}</p>
           </div>
         </div>
 
@@ -231,66 +228,20 @@ cm
         </div>
       </div>
       )]
-    const aggStyle = unitView ? styles.closed_panel : styles.open_panel
     const imgLCL = { backgroundImage: `url(${LOAD_TYPES[0].img})` }
-    const aggViewer = (
-      <div
-        className={`${aggStyle} ${
-          styles.panel
-        } flex-100 layout-row layout-wrap layout-align-none-center layout-wrap`}
-      >
-        <CargoItemGroupAggregated group={group} hideUnits={hideUnits} />
-      </div>
-    )
-    const cargoCategory = group.cargoType ? group.cargoType.category : cargoGlossary[group.size_class]
 
     return (
       <div className={`${styles.info}`}>
-      <div className={`flex-100 layout-row layout-align-start-center  ${styles.title_bar}`}>
-        <div className={`flex-20 layout-row layout-align-center-center`}>
-          <div className={styles.icon_cargo_item_small} style={imgLCL} />
-          <p className="flex-none layout-row layout-align-center-center">{`${group.items.length} x ${group.cargoType.description}`}</p>
-          
+        <div className={`flex-100 layout-row layout-align-start-center  ${styles.title_bar}`}>
+          <div className="flex-20 layout-row layout-align-center-center">
+            <div className={styles.icon_cargo_item_small} style={imgLCL} />
+            <p className="flex-none layout-row layout-align-center-center">{`${group.items.length} x ${group.cargoType.description}`}</p>
+
+          </div>
         </div>
-      </div>
         <div className="flex-100 layout-row layout-align-end-start layout-wrap">
           {unitArr}
         </div>
-        {/* <div className={`flex-100 layout-row layout-align-center-center ${styles.height_box} ${collapsed ? styles.height_box : styles.height_box}`}>
-          <div className={`flex-5 layout-row layout-align-center-center ${styles.side_border}`}>
-            <p className={`flex-none layout-row layout-align-center-center ${styles.cargo_unit}`}>{group.groupAlias}</p>
-          </div>
-          <div className={`flex-20 layout-row layout-align-center-center ${styles.side_border}`}>
-            <p className="flex-none layout-row layout-align-center-center">{`x ${group.items.length}`}</p>
-            <div className={styles.icon_cargo_item} style={imgLCL} />
-          </div>
-          <div className={`flex-20 layout-row layout-align-center-center ${styles.side_border}`}>
-            <div className="">
-              <p className="flex-none layout-row layout-align-center-center"><span className={styles.cargo_type}>{cargoCategory}</span></p>
-              <p className="flex-none layout-row layout-align-center-center">{t('cargo:type')}</p>
-            </div>
-          </div>
-          <div className="flex-55 layout-row">
-            { aggViewer}
-          </div>
-          { hideUnits ? '' : (
-            <div
-              className="flex-5 layout-row layout-align-center-center"
-              onClick={this.handleCollapser}
-              onChange={e => this.handleViewToggle(e)}
-            >
-              <i className={`${collapsed ? styles.collapsed : ''} fa fa-chevron-down clip pointy`} style={gradientTextStyle} />
-            </div>
-          ) }
-        </div>
-        { hideUnits ? ''
-          : (
-            <div className={`${styles.unit_viewer} ${collapsed ? '' : styles.closed_panel}`}>
-              <div className="flex-100 layout-row layout-align-none-start layout-wrap">
-                {unitArr}
-              </div>
-            </div>
-          ) } */}
       </div>
     )
   }
