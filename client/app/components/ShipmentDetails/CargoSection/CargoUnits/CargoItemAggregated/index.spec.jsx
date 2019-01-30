cacd91f9c1659d331bcc7587c105f3cfa5ddeba4
Francisco P Coutinho <francisco.lgpc@gmail.com>
2019-01-30 19:12:08 +0100

    IMC-1125 - spec updates
    Former-commit-id: c08fc589bfa99b6979e794911fdaa02fe7831504

--- client/app/components/ShipmentDetails/CargoSection/CargoUnits/CargoItemAggregated/index.spec.jsx
+++ client/app/components/ShipmentDetails/CargoSection/CargoUnits/CargoItemAggregated/index.spec.jsx
@@ -3,6 +3,10 @@ import { shallow } from 'enzyme'
 import CargoItemAggregated from '.'
 import { cargoUnitProps, cargoItemAggregated } from '../../../mocks'
 
+jest.mock('react-redux', () => ({
+  connect: (mapStateToProps, mapDispatchToProps) => Component => Component
+}))
+
 test('with empty props', () => {
   expect(() => shallow(<CargoItemAggregated />)).toThrow()
 })
