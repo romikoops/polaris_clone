060719e92594b86c270ede1b2bdb91703bdcccf0
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2019-02-14 18:34:45 +0100

    Chore: Update Gems for Security

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,11 @@
 FactoryBot.define do
   factory :user do
     currency { 'USD' }
+    first_name { 'John' }
+    last_name { 'Doe' }
     sequence(:email) { |n| "demo#{n}@demo.com" }
     password { 'demo123456789' }
+
     association :tenant
     association :role
     association :optin_status
