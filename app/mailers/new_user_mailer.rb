2c2c8554f5d17bef04394d61b36a5a6def0266c5
Warwick Beamish <wkbeamish@gmail.com>
2019-02-19 17:18:27 +0100

    IMC-1300 - basic new user email

---
+++ app/mailers/new_user_mailer.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class NewUserMailer < ApplicationMailer
+  default from: 'ItsMyCargo Bookings <bookings@itsmycargo.com>'
+  layout 'mailer'
+  add_template_helper(ApplicationHelper)
+
+  def new_user_email(user_id:) # rubocop:disable Metrics/AbcSize
+    @user = User.find(user_id)
+    @tenant = Tenant.find(@user.tenant_id)
+    @theme = @tenant.theme
+
+    @mot_icon = URI.open(
+      "https://assets.itsmycargo.com/assets/icons/mail/mail_ocean.png"
+    ).read
+
+    attachments.inline['logo.png'] = URI.try(:open, @tenant.theme['emailLogo']).try(:read)
+    attachments.inline['icon.png'] = @mot_icon
+    email = @tenant.emails.dig('sales', 'general')
+
+    return if email.nil?
+
+    mail(
+      from: Mail::Address.new("no-reply@#{Settings.emails.domain}")
+                         .tap { |a| a.display_name = 'ItsMyCargo Service Request' }.format,
+      reply_to: 'support@itsmycargo.com',
+      to: mail_target_interceptor(@user, email),
+      subject: "A New User Has Registered!"
+    ) do |format|
+      format.html
+      format.mjml
+    end
+  end
+end
