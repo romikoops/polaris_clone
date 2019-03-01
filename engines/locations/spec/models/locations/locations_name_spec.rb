d2cc3e25dd6dd779f56ccb6818277b2853777537
Warwick Beamish <wkbeamish@gmail.com>
2019-03-01 16:26:44 +0100

    IMC-1136 - dead files removed

--- engines/locations/spec/models/locations/locations_name_spec.rb
+++ engines/locations/spec/models/locations/locations_name_spec.rb
@@ -6,12 +6,6 @@ RSpec.describe Locations::Name, type: :model do
     it 'is valid with valid attributes' do
       expect(FactoryBot.build(:locations_name)).to be_valid
     end
-
-    # it 'is unique' do
-    #   location_name = FactoryBot.create(:locations_name)
-
-    #   expect(FactoryBot.build(:locations_name, name: location_name.name)).not_to be_valid
-    # end
   end
 
 end
