91ba9bc75c4f5266f33ecdd50129fb99af02d762
Warwick Beamish <wkbeamish@gmail.com>
2019-03-05 10:33:21 +0100

    IMC-1136 - index suffix added to searchkick

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,4 @@
-ENV["ELASTICSEARCH_URL"] = Settings.elasticsearch.url
+# frozen_string_literal: true
+
+ENV['ELASTICSEARCH_URL'] = Settings.elasticsearch.url
+Searchkick.index_suffix = ENV['REVIEW_NAME']
