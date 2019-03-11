0aad2b7b22915270c938b97450336b94734df577
Warwick Beamish <wkbeamish@gmail.com>
2019-03-11 09:57:24 +0100

    IMC-1341 - ready for qa maybe

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,19 +7,9 @@ class LocationsController < ApplicationController
   def index
     input = params[:query]
 
-    experiment = Experiments::Trucking::Autocomplete.new(name: 'locations_search')
-    experiment.use do
-      countries = params[:countries].split(',').map { |code| Country.find_by_code(code.upcase)&.name }.compact
-      query = Location.all
-      query = query.where(country: countries) if countries.present?
-      query = query.where.not(city: nil, country: nil, postal_code: nil)
-      results = query.autocomplete(input)
-      results.map(&:as_result_json)
-    end
-    experiment.try do
       country_codes = params[:countries].split(',').map(&:downcase).compact
       raw_results = Locations::Autocomplete.search(term: input, country_codes: country_codes)
-      raw_results.map do |result|
+      results = raw_results.map do |result|
         {
           geojson: result.geojson,
           description: result.display_name,
@@ -29,8 +19,7 @@ class LocationsController < ApplicationController
           location: result.lat_lng
         }
       end
-    end
-    results = experiment.run
+
     response_handler(
       results: results
     )
