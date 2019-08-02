9970813e80170fad20cee0430c0566bdf198e5e7
Mikko Kokkonen <mikian@users.noreply.github.com>
2019-08-02 18:18:03 +0200

    fix: setup Config in correct engine (#315)

--- config/initializers/00-config.rb
+++
@@ -1,7 +0,0 @@
-Config.setup do |config|
-  config.use_env = true
-  config.env_prefix = 'SETTINGS'
-  config.env_separator = '__'
-  config.env_converter = :downcase
-  config.env_parse_values = true
-end
