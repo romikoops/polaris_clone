d9b0bccf1632e916d6b58a382659899656b8fa5f
Ivo Marcelo Leonardi Zaniolo <marcelo.leonardi@itsmycargo.com>
2019-11-13 10:00:47 +0100

    IMC-1745: Fix note that are now shown on pdf (#588)

--- app/classes/offer_calculator_service/quote_route_builder.rb
+++ app/classes/offer_calculator_service/quote_route_builder.rb
@@ -25,14 +25,17 @@ module OfferCalculatorService
     def attributes(route, itinerary, tenant_vehicle_id)
       origin_hub = ::Legacy::Stop.find_by(id: route.origin_stop_id, sandbox: @sandbox).hub
       destination_hub = ::Legacy::Stop.find_by(id: route.destination_stop_id, sandbox: @sandbox).hub
-      faux_trip = itinerary.trips.find_or_create_by!(tenant_vehicle_id: tenant_vehicle_id, sandbox: @sandbox)
+      faux_trip = itinerary.trips.find_or_create_by!(tenant_vehicle_id: tenant_vehicle_id,
+                                                     start_date: Legacy::Schedule::QUOTE_TRIP_END_DATE,
+                                                     end_date: Legacy::Schedule::QUOTE_TRIP_START_DATE,
+                                                     sandbox: @sandbox)
       {
         origin_hub_id: origin_hub.id,
         destination_hub_id: destination_hub.id,
         origin_hub_name: origin_hub.name,
         destination_hub_name: destination_hub.name,
-        eta: Legacy::Schedule::QUOTE_TRIP_END_DATE,
-        etd: Legacy::Schedule::QUOTE_TRIP_START_DATE,
+        eta: faux_trip.end_date,
+        etd: faux_trip.start_date,
         closing_date: Date.tomorrow,
         trip_id: faux_trip.id,
         mode_of_transport: route.mode_of_transport,
