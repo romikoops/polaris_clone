8db8e6fb55cc57a2a339114543537e0abe03eedc
Warwick Beamish <DaddyWozBucks@users.noreply.github.com>
2019-11-18 16:03:12 +0100

    IMC-1744 - coutnry codes in dropdown (#605)

--- client/app/components/ShipmentDetails/RouteSection/Form/Dropdown/routeOption.js
+++ client/app/components/ShipmentDetails/RouteSection/Form/Dropdown/routeOption.js
@@ -1,6 +1,6 @@
 export default function routeOption (route) {
   return {
-    label: route.nexusName,
+    label: route.country ? `${route.nexusName}, ${route.country}` : route.nexusName,
     value: {
       id: route.nexusId,
       latitude: route.latitude,
