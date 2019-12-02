0014b3b4190d85d1e94709ac9e03938da595baeb
Ivo Marcelo Leonardi Zaniolo <marcelo.leonardi@itsmycargo.com>
2019-12-02 11:41:04 +0100

    CHORE: Added gemhelper.rb to docker-prepare.sh (#641)
    * chore: added gemhelper to docker prepare script
    
    * chore: forcing the rsync sync only the lib/engines/gemhelper.rb

--- scripts/docker-prepare.sh
+++ scripts/docker-prepare.sh
@@ -10,7 +10,8 @@ mkdir -p .build/docker
 # Find all gemspecs
 if [ -n "$(command -v rsync)" ];
 then
-  rsync -zarvm --include='*/' --include='*.gemspec' --exclude='*' . .build/docker
+  rsync -zarvm --include='*/' --include='*.gemspec' --include='lib/engines/gemhelper.rb' --exclude='*' . .build/docker
 else
   find . -name "*.gemspec" -exec cp --parents {} .build/docker/ \;
+  find . -name "lib/engines/gemhelper.rb" -exec cp --parents {} .build/docker/ \;
 fi
