2ffc7ddc2a829cf63f372cc91de0b4d4b8af8543
Warwick Beamish <DaddyWozBucks@users.noreply.github.com>
2020-01-07 15:38:19 +0100

    IMC-1845 - updated scope call (#692)
    * IMC-1845 - updated scope call
    
    * Update app/classes/excel_data_services/inserters/base.rb
    
    Co-Authored-By: Mikko Kokkonen <mikian@users.noreply.github.com>
    
    Co-authored-by: Mikko Kokkonen <mikian@users.noreply.github.com>

--- app/classes/excel_data_services/inserters/base.rb
+++ app/classes/excel_data_services/inserters/base.rb
@@ -14,7 +14,10 @@ module ExcelDataServices
       def initialize(tenant:, data:, options:)
         @tenant = tenant
         @tenants_tenant = Tenants::Tenant.find_by(legacy_id: tenant&.id)
-        @scope = ::Tenants::ScopeService.new(tenant: tenants_tenant, target: options[:user]).fetch
+        @scope = ::Tenants::ScopeService.new(
+          tenant: tenants_tenant,
+          target: ::Tenants::User.find_by(legacy_id: options[:user]&.id)
+        ).fetch
         @data = data
         @klass_identifier = self.class.name.split('::').last
         @options = options
