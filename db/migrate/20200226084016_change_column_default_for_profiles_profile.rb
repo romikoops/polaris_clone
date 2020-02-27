7fb428bb6ac6f8dd45402a5da30fb2a18e5fc319
Warwick Beamish <DaddyWozBucks@users.noreply.github.com>
2020-02-27 10:21:17 +0100

    HOTFIX - safety assured added to broken migration pre deploy (#847)

--- db/migrate/20200226084016_change_column_default_for_profiles_profile.rb
+++ db/migrate/20200226084016_change_column_default_for_profiles_profile.rb
@@ -2,9 +2,11 @@
 
 class ChangeColumnDefaultForProfilesProfile < ActiveRecord::Migration[5.2]
   def change
-    change_column_null :profiles_profiles, :first_name, false
-    change_column_null :profiles_profiles, :last_name, false
-    change_column_default :profiles_profiles, :first_name, from: nil, to: ''
-    change_column_default :profiles_profiles, :last_name, from: nil, to: ''
+    safety_assured do
+      change_column_null :profiles_profiles, :first_name, false
+      change_column_null :profiles_profiles, :last_name, false
+      change_column_default :profiles_profiles, :first_name, from: nil, to: ''
+      change_column_default :profiles_profiles, :last_name, from: nil, to: ''
+    end
   end
 end
