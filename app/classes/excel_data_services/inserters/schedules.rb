97a7925f53f08afde88cf6ff26e5b02909bbb919
Henry Perschk <henry.perschk@gmail.com>
2020-03-12 03:29:45 +0100

    IMC-2169 - Schedule Uploader not accounting for tenant_id (#885)

--- app/classes/excel_data_services/inserters/schedules.rb
+++ app/classes/excel_data_services/inserters/schedules.rb
@@ -5,7 +5,13 @@ module ExcelDataServices
     class Schedules < ExcelDataServices::Inserters::Base
       def perform
         data.each do |params|
-          generate_schedules_for_import(params)
+          itinerary = find_itinerary(params)
+          next if itinerary.blank?
+
+          trip = init_trip_with_layovers(params, itinerary)
+          add_stats(trip)
+
+          trip.save
         end
 
         stats
@@ -13,11 +19,16 @@ module ExcelDataServices
 
       private
 
-      def generate_schedules_for_import(params)
-        itinerary = itinerary_from_params(params)
-        return if itinerary.blank?
+      def find_itinerary(params)
+        Itinerary.find_by(
+          tenant: tenant,
+          name: "#{params[:from]} - #{params[:to]}",
+          mode_of_transport: params[:mode_of_transport]
+        )
+      end
 
-        trip = Legacy::Trip.new(
+      def init_trip_with_layovers(params, itinerary)
+        Legacy::Trip.new(
           vessel: params[:vessel],
           voyage_code: params[:voyage_code],
           load_type: params[:load_type],
@@ -25,17 +36,20 @@ module ExcelDataServices
           closing_date: params[:closing_date],
           end_date: params[:eta],
           itinerary: itinerary,
-          tenant_vehicle_id: tenant_vehicle_id(params),
+          tenant_vehicle_id: find_tenant_vehicle_id(params),
           layovers: generate_layovers(itinerary, params)
         )
-
-        add_stats(trip)
-
-        trip.save
       end
 
-      def itinerary_from_params(params)
-        Itinerary.find_by(name: "#{params[:from]} - #{params[:to]}", mode_of_transport: params[:mode_of_transport])
+      def find_tenant_vehicle_id(params)
+        carrier = Carrier.find_by(name: params[:carrier]) if params[:carrier].present?
+
+        TenantVehicle.find_by(
+          tenant: tenant,
+          name: params[:service_level],
+          carrier: carrier,
+          mode_of_transport: params[:mode_of_transport]
+        ).id
       end
 
       def generate_layovers(itinerary, params)
@@ -50,11 +64,6 @@ module ExcelDataServices
             etd: stop_index_zero ? params[:etd] : nil
           )
         end
-      end
-
-      def tenant_vehicle_id(params)
-        carrier = Carrier.find_by(name: params[:carrier]) if params[:carrier].present?
-        TenantVehicle.find_by(name: params[:service_level], carrier: carrier, mode_of_transport: params[:mode_of_transport]).id
       end
     end
   end
