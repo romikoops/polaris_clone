33f6e823dfeee48713d1521f43b950e495e61a07
Joe Schafer <joeschafermedia@gmail.com>
2020-03-12 16:26:43 +0100

    IMC-2165 - Validations added to local charges (#882)

--- app/classes/excel_data_services/validators/type_validity/local_charges.rb
+++ app/classes/excel_data_services/validators/type_validity/local_charges.rb
@@ -4,6 +4,23 @@ module ExcelDataServices
   module Validators
     module TypeValidity
       class LocalCharges < ExcelDataServices::Validators::TypeValidity::Base
+        COLUMN_TO_CLASS_LOOKUP = {
+          'effective_date': :date,
+          'expiration_date': :date,
+          'hub': :required_string,
+          'country': :required_string,
+          'fee': :required_string,
+          'counterpart_hub': :optional_string,
+          'counterpart_country': :optional_string,
+          'service_level': :optional_string,
+          'carrier': :optional_string,
+          'fee_code': :optional_string,
+          'direction': :optional_string,
+          'rate_basis': :required_string,
+          'mot': :required_string,
+          'load_type': :required_string,
+          'currency': :required_string
+        }.freeze
       end
     end
   end
