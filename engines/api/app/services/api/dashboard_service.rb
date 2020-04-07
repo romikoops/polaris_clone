2525b09126b0312b8b3b5eb29f2f6947b900a250
Joe Schafer <joeschafermedia@gmail.com>
2020-04-07 10:56:44 +0200

    IMC-2336 - safe_constantize dashboard service (#957)
    * IMC-2336 - safe_constantize dashboard service
    
    * IMC-2336 - Simplify error

--- engines/api/app/services/api/dashboard_service.rb
+++ engines/api/app/services/api/dashboard_service.rb
@@ -2,6 +2,7 @@
 
 module Api
   class DashboardService
+    UnknownWidget = Class.new(StandardError)
     attr_reader :user, :widget_name, :start_date, :end_date
 
     def self.data(user:, widget_name:, start_date:, end_date:)
@@ -17,7 +18,9 @@ module Api
     end
 
     def data
-      widget_klass = Analytics::Dashboard.const_get(widget_name.camelize)
+      widget_klass = "Analytics::Dashboard::#{widget_name.camelize}".safe_constantize
+      raise UnknownWidget, 'Widget does not exist' if widget_klass.nil?
+
       widget_klass.data(user: user, start_date: start_date, end_date: end_date)
     end
   end
