dbf61736bc025d01d8df824725ed9e227c71cfaa
Warwick Beamish <DaddyWozBucks@users.noreply.github.com>
2020-05-12 16:32:12 +0100

    IMC-2593 - postal code trucking (#1161)
    * IMC-2593 - postal code trucking
    
    * IMC-2593 - corrected backfill

---
+++ db/migrate/20200512111459_backfill_german_postal_code_trucking.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class BackfillGermanPostalCodeTrucking < ActiveRecord::Migration[5.2]
+  def up
+    safety_assured do
+      hub_ids = Legacy::Hub.joins(address: :country)
+                           .where(countries: { code: 'DE' })
+                           .select(:id)
+      Trucking::TypeAvailability.joins(:hub_availabilities)
+                                .where(
+                                  trucking_hub_availabilities: {
+                                    hub_id: hub_ids
+                                  },
+                                  query_method: 2
+                                ).update_all(query_method: 3)
+      execute("
+        UPDATE trucking_locations
+        SET location_id = locations_locations.id
+        FROM locations_locations
+        WHERE trucking_locations.country_code = 'DE'
+        AND trucking_locations.zipcode IS NOT NULL
+        AND locations_locations.name = trucking_locations.zipcode
+        AND locations_locations.country_code = 'de'
+        ")
+    end
+  end
+end
