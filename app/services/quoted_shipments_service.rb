252a39f3c1e9a1eec491867225ed51142f29a8e5
Warwick Beamish <DaddyWozBucks@users.noreply.github.com>
2020-07-16 10:53:14 +0200

    IMC-2969: cleared up naming and check for destryoed charge-breakdowns (#1500)

--- app/services/quoted_shipments_service.rb
+++ app/services/quoted_shipments_service.rb
@@ -67,18 +67,20 @@ class QuotedShipmentsService
   end
 
   def clone_offer(charge_breakdown:)
-    shipment.dup.tap do |clone_shipment|
-      clone_cargo(clone_shipment: clone_shipment)
-      update_cloned_shipment(clone_shipment: clone_shipment, charge_breakdown: charge_breakdown)
-      clone_charge_breakdown(clone_shipment: clone_shipment, charge_breakdown: charge_breakdown)
-      clone_shipment.save!
+    return if charge_breakdown.destroyed?
+
+    shipment.dup.tap do |cloned_shipment|
+      clone_cargo(cloned_shipment: cloned_shipment)
+      update_cloned_shipment(cloned_shipment: cloned_shipment, charge_breakdown: charge_breakdown)
+      clone_charge_breakdown(cloned_shipment: cloned_shipment, charge_breakdown: charge_breakdown)
+      cloned_shipment.save!
     end
   end
 
-  def update_cloned_shipment(clone_shipment:, charge_breakdown:)
+  def update_cloned_shipment(cloned_shipment:, charge_breakdown:)
     tender = charge_breakdown.tender
     trip = charge_breakdown.trip
-    clone_shipment.assign_attributes(
+    cloned_shipment.assign_attributes(
       status: "quoted",
       origin_hub: tender.origin_hub,
       destination_hub: tender.destination_hub,
@@ -92,45 +94,45 @@ class QuotedShipmentsService
       itinerary_id: trip.itinerary_id,
       desired_start_date: shipment.desired_start_date
     )
-    handle_clone_dates(clone_shipment: clone_shipment, trip: trip)
+    handle_clone_dates(cloned_shipment: cloned_shipment, trip: trip)
   end
 
-  def clone_cargo(clone_shipment:)
+  def clone_cargo(cloned_shipment:)
     if shipment.aggregated_cargo.present?
-      clone_shipment.aggregated_cargo = shipment.aggregated_cargo.dup
-      clone_shipment.aggregated_cargo.set_chargeable_weight!
+      cloned_shipment.aggregated_cargo = shipment.aggregated_cargo.dup
+      cloned_shipment.aggregated_cargo.set_chargeable_weight!
     end
-    clone_shipment.cargo_units = shipment.cargo_units.map do |unit|
+    cloned_shipment.cargo_units = shipment.cargo_units.map do |unit|
       new_unit = unit.dup
-      new_unit.set_chargeable_weight! if clone_shipment.lcl?
+      new_unit.set_chargeable_weight! if cloned_shipment.lcl?
       charge_category_map[unit.id] = new_unit.id
       new_unit
     end
   end
 
-  def handle_clone_dates(clone_shipment:, trip:)
-    if clone_shipment.has_pre_carriage?
+  def handle_clone_dates(cloned_shipment:, trip:)
+    if cloned_shipment.has_pre_carriage?
       trucking_seconds = shipment.trucking.dig("pre_carriage", "trucking_time_in_seconds").to_i.seconds
-      clone_shipment.planned_pickup_date = trip.closing_date - 1.day - trucking_seconds
+      cloned_shipment.planned_pickup_date = trip.closing_date - 1.day - trucking_seconds
     else
-      clone_shipment.planned_origin_drop_off_date = trip.closing_date - 1.day
+      cloned_shipment.planned_origin_drop_off_date = trip.closing_date - 1.day
     end
   end
 
-  def clone_charge_breakdown(clone_shipment:, charge_breakdown:)
-    clone_charge_breakdown = charge_breakdown.dup
-    clone_charge_breakdown.update(shipment: clone_shipment)
-    clone_charge_breakdown.dup_charges(charge_breakdown: charge_breakdown)
-    clone_charges(clone_charge_breakdown: clone_charge_breakdown, original: charge_breakdown)
+  def clone_charge_breakdown(cloned_shipment:, charge_breakdown:)
+    cloned_charge_breakdown = charge_breakdown.dup
+    cloned_charge_breakdown.update(shipment: cloned_shipment)
+    cloned_charge_breakdown.dup_charges(charge_breakdown: charge_breakdown)
+    clone_charges(cloned_charge_breakdown: cloned_charge_breakdown, original: charge_breakdown)
   end
 
   def clone_pricing_metadatum(clone:, original:)
     metadatum = Pricings::Metadatum.find_by(charge_breakdown_id: original.id)
     return if metadatum.blank?
 
-    clone_metadatum = metadatum.dup.tap do |metadatum|
-      metadatum.charge_breakdown_id = clone.id
-      metadatum.save
+    clone_metadatum = metadatum.dup.tap do |tapped_metadatum|
+      tapped_metadatum.charge_breakdown_id = clone.id
+      tapped_metadatum.save
     end
 
     metadatum.breakdowns.each do |breakdown|
@@ -139,14 +141,14 @@ class QuotedShipmentsService
     clone_metadatum
   end
 
-  def clone_charges(clone_charge_breakdown:, original:)
+  def clone_charges(cloned_charge_breakdown:, original:)
     clone_metadatum = clone_pricing_metadatum(
-      clone: clone_charge_breakdown, original: original
+      clone: cloned_charge_breakdown, original: original
     )
     %w[import export cargo trucking_pre trucking_on].each do |charge_key|
-      next if clone_charge_breakdown.charge(charge_key).nil?
+      next if cloned_charge_breakdown.charge(charge_key).nil?
 
-      clone_charge_breakdown.charge(charge_key).children.each do |new_charge|
+      cloned_charge_breakdown.charge(charge_key).children.each do |new_charge|
         old_charge_category = new_charge&.children_charge_category
         next if old_charge_category.nil?
 
