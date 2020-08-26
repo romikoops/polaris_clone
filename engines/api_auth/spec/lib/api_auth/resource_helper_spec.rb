2d57f7f1f6ad62b6f94d8981d16a78960d3700fc
BassamAziz <bassam.aziz@itsmycargo.com>
2020-08-26 15:43:28 +0200

    IMC-3108 - Login Specs (#1644)
    * IMC-3108 - login specs

--- engines/api_auth/spec/lib/api_auth/resource_helper_spec.rb
+++ engines/api_auth/spec/lib/api_auth/resource_helper_spec.rb
@@ -4,38 +4,60 @@ require "rails_helper"
 
 RSpec.describe ApiAuth::ResourceHelper do
   let(:organization) { FactoryBot.create(:organizations_organization) }
+  let(:resource_for_login) { described_class.resource_for_login(client: client) }
 
   before do
     Organizations.current_id = organization.id
   end
 
   describe ".resource_for_login" do
-    context "when bridge is true" do
-      before do
-        allow(Authentication::User).to receive(:with_membership)
-        FactoryBot.create(:organizations_user, organization: organization)
-      end
-
+    describe "admin authentication for bridge" do
       let(:client) { double("client", name: "bridge") }
 
-      it "returns Authentication::User with the with_membership" do
-        described_class.resource_for_login(client: client)
+      context "when a user with membership exists" do
+        let!(:user) do
+          FactoryBot.create(:authentication_user, :users_user).tap do |user|
+            FactoryBot.create(:organizations_membership, organization: organization, user: user)
+          end
+        end
 
-        expect(Authentication::User).to have_received(:with_membership)
+        it "finds users with memberships" do
+          expect(resource_for_login.ids).to eq [user.id]
+        end
+      end
+
+      context "when user not found" do
+        before do
+          FactoryBot.create(:authentication_user, :users_user)
+        end
+
+        it "finds returns empty collection" do
+          expect(resource_for_login).to be_empty
+        end
       end
     end
 
-    context "when bridge is false" do
-      before do
-        allow(Authentication::User).to receive(:authentication_scope)
-      end
-
+    describe "non-admin authentication" do
       let(:client) { nil }
 
-      it "returns Authentication::User " do
-        described_class.resource_for_login(client: client)
+      context "when organization user exists" do
+        let!(:user) { FactoryBot.create(:authentication_user, :organizations_user, organization_id: organization.id) }
 
-        expect(Authentication::User).to have_received(:authentication_scope)
+        it "finds users for the current organization" do
+          expect(resource_for_login.ids).to eq [user.id]
+        end
+      end
+
+      context "when no users exist for the current organization" do
+        let(:another_org) { FactoryBot.create(:organizations_organization) }
+
+        before do
+          FactoryBot.create(:authentication_user, :organizations_user, organization_id: another_org.id)
+        end
+
+        it "finds no organization users" do
+          expect(resource_for_login).to be_empty
+        end
       end
     end
   end
