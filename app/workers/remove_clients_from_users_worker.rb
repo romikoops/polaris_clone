92ff48b5001866458244f5ee777ed30b5c3e0b88
BassamAziz <bassam.aziz@itsmycargo.com>
2021-02-08 08:53:35 +0100

    IMC-3512 - delete clients from users table (#1854)
    * IMC-3512 - fix syntax
    
    * IMC-3512 - remove profiles and settings

--- app/workers/remove_clients_from_users_worker.rb
+++ app/workers/remove_clients_from_users_worker.rb
@@ -4,9 +4,23 @@ class RemoveClientsFromUsersWorker
 
   def perform(*args)
     ActiveRecord::Base.connection.execute("
+      DELETE FROM users_settings
+      USING users_users
+      WHERE users_users.id = users_settings.user_id
+      AND users_users.type = 'Organizations::User'
+    ")
+
+    ActiveRecord::Base.connection.execute("
+      DELETE FROM users_profiles
+      USING users_users
+      WHERE users_users.id = users_profiles.user_id
+      AND users_users.type = 'Organizations::User'
+    ")
+
+    ActiveRecord::Base.connection.execute("
       DELETE FROM users_users
-      JOIN users_clients
-      ON users_clients.id = users_users.id
+      USING users_clients
+      WHERE users_clients.id = users_users.id
       AND users_users.type = 'Organizations::User'
     ")
   end
