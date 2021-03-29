1658943e6471425cd7260964ee50f53d90b9c222
Warwick Beamish <DaddyWozBucks@users.noreply.github.com>
2021-03-29 17:00:59 +0100

    POL-19: Handle 503 on all Cart calls (#1988)
    * POL-19: Handle 503 on all Cart calls
    
    * POL-19: rubocop fix
    
    Co-authored-by: kodiak-itsmycargo[bot] <80279588+kodiak-itsmycargo[bot]@users.noreply.github.com>

--- engines/carta/app/services/carta/client.rb
+++ engines/carta/app/services/carta/client.rb
@@ -1,22 +1,25 @@
 # frozen_string_literal: true
+
 module Carta
   class Client
     LocationNotFound = Class.new(StandardError)
 
     class << self
       def lookup(id:)
-        response = connection.get('lookup') do |req|
-          req.params['id'] = id
+        response = connection.get("lookup") do |req|
+          req.params["id"] = id
         end
 
-        result_from_carta(params: JSON.parse(response.body).dig("data"))
+        result_from_carta(params: JSON.parse(response.body)["data"])
       end
 
       def suggest(query:)
-        response = connection.get('suggest') do |req|
-          req.params['query'] = query
-          req.params['mode'] = 'internal'
+        response = connection.get("suggest") do |req|
+          req.params["query"] = query
+          req.params["mode"] = "internal"
         end
+
+        raise LocationNotFound unless response.success?
 
         id = JSON.parse(response.body).dig("data", 0, "id")
         lookup(id: id)
--- engines/carta/spec/services/carta/client_spec.rb
+++ engines/carta/spec/services/carta/client_spec.rb
@@ -1,74 +1,94 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 module Carta
   RSpec.describe Client do
-
     let(:stubs)  { Faraday::Adapter::Test::Stubs.new }
     let(:conn)   { Faraday.new { |b| b.adapter(:test, stubs) } }
-    let(:client) { described_class }
-
-    let(:geo_id) { '123' }
+    let(:result) { Carta::Result.new({ "id": "itsmycargo:locode:ABC123" }) }
     let(:lookup_resp) { '{"data": {"id": "itsmycargo:locode:ABC123"} }' }
-    let(:result) { Carta::Result.new({"id": "itsmycargo:locode:ABC123"}) }
 
     before do
-      allow(client).to receive(:connection).and_return(conn)
+      allow(described_class).to receive(:connection).and_return(conn)
     end
 
     describe "#lookup" do
-      it 'calls the carta /lookup endpoint with the arugment id as a param' do
-        stubs.get("/lookup?id=#{geo_id}") do |env|
-          expect(env.url.path).to eq('/lookup')
-          [
-            200,
-            { 'Content-Type': 'application/json' },
-            lookup_resp
-          ]
+      let(:geo_id) { "123" }
+
+      context "when carta responds with 200" do
+        before do
+          stubs.get("/lookup?id=#{geo_id}") do
+            [
+              200,
+              { 'Content-Type': "application/json" },
+              lookup_resp
+            ]
+          end
         end
 
-        expect(client.lookup(id: geo_id)).to eq(result)
-        stubs.verify_stubbed_calls
+        it "calls the carta /lookup endpoint with the argument id as a param", :aggregate_failures do
+          expect(described_class.lookup(id: geo_id)).to eq(result)
+          stubs.verify_stubbed_calls
+        end
       end
 
       context "when carta responds with 503" do
-        it 'retries after 1 second' do
-          stubs.get("/lookup?id=#{geo_id}") do |env|
-            expect(env.url.path).to eq('/lookup')
+        before do
+          stubs.get("/lookup?id=#{geo_id}") do
             [
               503,
-              { 'Content-Type': 'application/json' },
-              '{}'
+              { 'Content-Type': "application/json" },
+              "{}"
             ]
           end
+        end
 
-          expect { client.lookup(id: geo_id) }.to raise_error(Carta::Client::LocationNotFound)
+        it "retries after 1 second", :aggregate_failures do
+          expect { described_class.lookup(id: geo_id) }.to raise_error(Carta::Client::LocationNotFound)
           stubs.verify_stubbed_calls
         end
       end
     end
 
     describe "#suggest" do
-      let(:suggest_resp) { '{"data": [{"id": "itsmycargo:locode:ABC123"}] }' }
-      let(:locode) { 'DEHAM' }
+      let(:locode) { "DEHAM" }
 
-      before do
-        stubs.get("/lookup?id=itsmycargo:locode:ABC123") do |env|
-          [200, { 'Content-Type': 'application/json' }, lookup_resp]
+      context "when carta responds with 200" do
+        before do
+          stubs.get("/lookup?id=itsmycargo:locode:ABC123") do
+            [200, { 'Content-Type': "application/json" }, lookup_resp]
+          end
+          stubs.get("/suggest?query=#{locode}") do
+            [
+              200,
+              { 'Content-Type': "application/json" },
+              '{"data": [{"id": "itsmycargo:locode:ABC123"}] }'
+            ]
+          end
+        end
+
+        it "calls the carta /suggest endpoint with the argument query as a param", :aggregate_failures do
+          expect(described_class.suggest(query: locode)).to eq(result)
+          stubs.verify_stubbed_calls
         end
       end
 
-      it 'calls the carta /suggest endpoint with the arugment query as a param' do
-        stubs.get("/suggest?query=#{locode}") do |env|
-          expect(env.url.path).to eq("/suggest")
-          [
-            200,
-            { 'Content-Type': 'application/json' },
-            suggest_resp
-          ]
+      context "when carta responds with 503" do
+        before do
+          stubs.get("/suggest?query=#{locode}") do
+            [
+              503,
+              { 'Content-Type': "application/json" },
+              "{}"
+            ]
+          end
         end
 
-        expect(client.suggest(query: locode)).to eq(result)
-        stubs.verify_stubbed_calls
+        it "retries after 1 second", :aggregate_failures do
+          expect { described_class.suggest(query: locode) }.to raise_error(Carta::Client::LocationNotFound)
+          stubs.verify_stubbed_calls
+        end
       end
     end
   end
