f0b10551da4e4f385c429bcd21eabdd43ba553bd
Warwick Beamish <DaddyWozBucks@users.noreply.github.com>
2021-08-04 18:41:00 +0200

    POL-2728: Theme editing on Admiralty (#2180)
    * POL-2728: Theme editing on Admiralty
    
    * POL-2728: missed icon
    
    * POL-2728: attempting helper
    
    * POL-2728: dryed
    
    Co-authored-by: kodiak-itsmycargo[bot] <80279588+kodiak-itsmycargo[bot]@users.noreply.github.com>

--- engines/admiralty/app/admin/admiralty/organizations_admin.rb
+++ engines/admiralty/app/admin/admiralty/organizations_admin.rb
@@ -54,6 +54,21 @@ Trestle.resource(:organizations, model: Admiralty::Organization) do
           col(sm: 6) { text_field :bright_primary_color }
           col(sm: 6) { text_field :bright_secondary_color }
         end
+
+        %i[
+          background
+          small_logo
+          large_logo
+          email_logo
+          white_logo
+          wide_logo
+          booking_process_image
+        ].each do |attachment|
+          row do
+            col(sm: 4) { file_field attachment }
+            col(sm: 8) { organization.theme.send(attachment).attached? ? image_tag(Rails.application.routes.url_helpers.rails_blob_url(organization.theme.send(attachment)), width: "100px") : "No Content" }
+          end
+        end
       end
     end
 
--- engines/admiralty/app/admin/admiralty/themes_admin.rb
+++ engines/admiralty/app/admin/admiralty/themes_admin.rb
@@ -1,22 +1,9 @@
 # frozen_string_literal: true
+
 Trestle.resource(:themes, model: Organizations::Theme) do
-  active_storage_fields do
-    %i[background small_logo large_logo email_logo white_logo wide_logo
-      booking_process_image welcome_email_image]
-  end
+  menu :themes, icon: "fa fa-paint-brush", group: :organizations
 
-  # Customize the table columns shown on the index view.
-  #
-  # table do
-  #   column :name
-  #   column :created_at, align: :center
-  #   actions
-  # end
-
-  # Customize the form fields shown on the new/edit views.
-  #
   form do |theme|
-    # Form helper methods now dispatch to the product.category form scope
     text_field :name
 
     row do
@@ -29,26 +16,19 @@ Trestle.resource(:themes, model: Organizations::Theme) do
       col(sm: 6) { text_field :bright_secondary_color }
     end
 
-    row do
-      col(sm: 2) { active_storage_field :background }
-      col(sm: 2) { active_storage_field :small_logo }
-      col(sm: 2) { active_storage_field :large_logo }
-      col(sm: 2) { active_storage_field :email_logo }
-      col(sm: 2) { active_storage_field :white_logo }
-      col(sm: 2) { active_storage_field :wide_logo }
-      col(sm: 2) { active_storage_field :booking_process_image }
-      col(sm: 2) { active_storage_field :welcome_email_image }
+    %i[
+      background
+      small_logo
+      large_logo
+      email_logo
+      white_logo
+      wide_logo
+      booking_process_image
+    ].each do |attachment|
+      row do
+        col(sm: 4) { file_field attachment }
+        col(sm: 8) { organization.theme.send(attachment).attached? ? image_tag(Rails.application.routes.url_helpers.rails_blob_url(organization.theme.send(attachment)), width: "100px") : "No Content" }
+      end
     end
   end
-
-  # By default, all parameters passed to the update and create actions will be
-  # permitted. If you do not have full trust in your users, you should explicitly
-  # define the list of permitted parameters.
-  #
-  # For further information, see the Rails documentation on Strong Parameters:
-  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
-  #
-  # params do |params|
-  #   params.require(:theme).permit(:name, ...)
-  # end
 end
