7f6d291d59e90fecd23a0e468a1e44f75aaaabc8
Ajith Kumar <88225952+ajithatimc@users.noreply.github.com>
2021-09-03 16:26:39 +0530

    CHORE : Updating Gemfile.lock after adding user services engine (#2256)
    Updating Gemfile.lock after adding user services engine and fixing flaky tests

--- engines/offer_calculator/spec/services/offer_calculator/queries/full_attributes_from_itinerary_ids_spec.rb
+++ engines/offer_calculator/spec/services/offer_calculator/queries/full_attributes_from_itinerary_ids_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe OfferCalculator::Queries::FullAttributesFromItineraryIds do
   let(:organization) { FactoryBot.create(:organizations_organization) }
   let(:user) { FactoryBot.create(:users_client, organization: organization) }
-  let(:itinerary) { FactoryBot.create(:gothenburg_shanghai_itinerary, organization: organization) }
+  let(:itinerary) { FactoryBot.create(:gothenburg_shanghai_itinerary, rates: pricings, organization: organization) }
   let(:itinerary2) { FactoryBot.create(:shanghai_gothenburg_itinerary, organization: organization) }
   let(:origin_hub) { itinerary.origin_hub }
   let(:destination_hub) { itinerary.destination_hub }
@@ -17,14 +17,13 @@ RSpec.describe OfferCalculator::Queries::FullAttributesFromItineraryIds do
   end
   let(:result) { results.first }
 
-  before do
-    FactoryBot.create(:lcl_pricing, itinerary: itinerary, organization: organization)
-    FactoryBot.create(:fcl_20_pricing, itinerary: itinerary, organization: organization)
-    FactoryBot.create(:fcl_40_pricing, itinerary: itinerary, organization: organization)
-    FactoryBot.create(:fcl_40_hq_pricing, itinerary: itinerary, organization: organization)
+  let(:pricings) do
+    [FactoryBot.build(:lcl_pricing, organization: organization),
+      FactoryBot.build(:fcl_20_pricing,  organization: organization),
+      FactoryBot.build(:fcl_40_pricing,  organization: organization),
+      FactoryBot.build(:fcl_40_hq_pricing, organization: organization)]
   end
 
-  # rubocop: disable Performance/StringInclude
   describe ".perform", :vcr do
     context "when lcl" do
       it "return the route detail hashes for cargo_item", :aggregate_failures do
@@ -32,7 +31,7 @@ RSpec.describe OfferCalculator::Queries::FullAttributesFromItineraryIds do
         expect(result["itinerary_id"]).to eq(itinerary.id)
         expect(result["origin_hub_id"]).to eq(origin_hub.id)
         expect(result["destination_hub_id"]).to eq(destination_hub.id)
-        expect(results).not_to(be_any { |res| res["cargo_classes"].match?(/fcl/) })
+        expect(results).not_to(be_any { |res| res["cargo_classes"].include?("fcl_20") })
       end
     end
 
@@ -44,45 +43,36 @@ RSpec.describe OfferCalculator::Queries::FullAttributesFromItineraryIds do
         expect(result["itinerary_id"]).to eq(itinerary.id)
         expect(result["origin_hub_id"]).to eq(origin_hub.id)
         expect(result["destination_hub_id"]).to eq(destination_hub.id)
-        expect(results).not_to(be_any { |res| res["cargo_classes"].match?(/lcl/) })
+        expect(results).not_to(be_any { |res| res["cargo_classes"].include?("lcl") })
       end
     end
 
     context "with soft deleted pricing" do
-      let!(:first_rate) { itinerary.rates.first }
-
-      it "itinerary rates count is reduced by 1" do
-        expect { first_rate.destroy }.to change(itinerary.rates, :count).by(-1)
+      let(:load_type) { "container" }
+      let(:pricings) do
+        [
+          FactoryBot.build(:fcl_40_pricing, organization: organization),
+          FactoryBot.build(:fcl_20_pricing, organization: organization, deleted_at: 2.days.ago)
+        ]
       end
 
       it "cargo classes will not have the particular pricing" do
-        expect(results).not_to(be_any { |res| res["cargo_classes"].include?(first_rate.cargo_class) })
-      end
-    end
-
-    context "when deleted pricing is restored" do
-      let(:load_type) { "container" }
-      let(:first_rate) { itinerary.rates.first }
-
-      before do
-        first_rate.destroy
-      end
-
-      it "itinerary rates count is increased by 1" do
-        expect { first_rate.restore }.to change(itinerary.rates, :count).by(1)
+        expect(results).not_to(be_any { |res| res["cargo_classes"].include?("fcl_20") })
       end
     end
 
     context "when pricing is expired" do
       let(:load_type) { "container" }
+      let(:pricings) do
+        [
+          FactoryBot.build(:fcl_40_pricing, organization: organization),
+          FactoryBot.build(:fcl_20_pricing, organization: organization, effective_date: 1.year.ago, expiration_date: 2.days.ago)
+        ]
+      end
 
       it "cargo classes will not have the particular pricing" do
-        pricing = itinerary.rates.first
-        pricing.expiration_date = 2.days.ago
-        pricing.save!(validate: false)
-        expect(results).not_to(be_any { |res| res["cargo_classes"].include?(pricing.cargo_class) })
+        expect(results).not_to(be_any { |res| res["cargo_classes"].include?("fcl_20") })
       end
     end
   end
-  # rubocop: enable Performance/StringInclude
 end
